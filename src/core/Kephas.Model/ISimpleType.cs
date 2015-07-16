3545d83cba7082fbe57aed6cfc070a20232a972f
Ioan Crisan <ioan.crisan@quartz-software.com>
2015-07-17 01:25:18 +0300

    simple type, cmplex type

---
+++ src/core/Kephas.Model/ISimpleType.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ISimpleType.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   A simple type is a classifier which has as instances simple values.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Model
+{
+    /// <summary>
+    /// A simple type is a classifier which has as instances simple values.
+    /// </summary>
+    public interface ISimpleType : IClassifier
+    {
+    }
+}
