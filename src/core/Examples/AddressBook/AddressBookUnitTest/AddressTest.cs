b33ce5e126ab86fa50bb5b73249d25c32d3373cb
raimu <ortwang@googlemail.com>
2015-07-22 20:20:35 +0200

    start with a simple example-project

---
+++ src/core/Examples/AddressBook/AddressBookUnitTest/AddressTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using AddressBook;
+
+namespace AddressBookUnitTest
+{
+	[TestFixture]
+	public class AddressTest
+	{
+		[Test]
+		public void AddressToStringReturnsName()
+		{
+			var address = new Address ("John Doe", "john@example.com");
+			Assert.AreEqual ("John Doe", address.ToString ());
+		}
+	}
+}
+
