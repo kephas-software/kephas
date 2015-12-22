7b47f35c5f3adec5ab12cdd81bff73a4bac46433
Ioan Crisan <ioan.crisan@quartz-software.com>
2015-12-22 11:34:58 +0200

    ProjectAssistant app console starter

---
+++ Examples/ProjectAssistant.App.Console/ConsoleShell.cs
@@ -0,0 +1,42 @@
+﻿namespace ProjectAssistant.App.Console
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Kephas;
+    using Kephas.Application;
+    using Kephas.Diagnostics;
+    using Kephas.Hosting.Net45;
+    using Kephas.Logging.NLog;
+
+    /// <summary>
+    /// A console shell.
+    /// </summary>
+    public class ConsoleShell
+    {
+        /// <summary>
+        /// Starts the application asynchronously.
+        /// </summary>
+        /// <returns>
+        /// A task.
+        /// </returns>
+        public async Task StartAppAsync()
+        {
+            Console.WriteLine("Application starting...");
+
+            var elapsed = await Profiler.WithStopwatchAsync(
+                async () =>
+                    {
+                        var ambientServicesBuilder = await new AmbientServicesBuilder()
+                                .WithNLogManager()
+                                .WithNet45HostingEnvironment()
+                                .WithMefCompositionContainerAsync();
+
+                        var bootstrapper = ambientServicesBuilder.AmbientServices.CompositionContainer.GetExport<IAppBootstrapper>();
+                        await bootstrapper.StartAsync();
+                    });
+
+            Console.WriteLine($"Application started. Elapsed: {elapsed:c}.");
+        }
+    }
+}
---
+++ Examples/ProjectAssistant.App.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   The entry point class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ProjectAssistant.App.Console
+{
+    using System;
+
+    /// <summary>
+    /// The entry point class.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main entry-point for this application.
+        /// </summary>
+        /// <param name="args">Array of command-line argument strings.</param>
+        public static void Main(string[] args)
+        {
+            var shell = new ConsoleShell();
+            shell.StartAppAsync();
+
+            Console.ReadLine();
+        }
+    }
+}
