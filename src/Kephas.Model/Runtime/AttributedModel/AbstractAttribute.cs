5b24347c54e09a52ad6b6c7106d1b28e2f03a7d0
Ioan Crisan <ioan.crisan@quartz-software.com>
2015-12-23 13:45:50 +0200

    Abstract attribute

---
+++ src/Kephas.Model/Runtime/AttributedModel/AbstractAttribute.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AbstractAttribute.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Marks a classifier as being abstract (cannot be instantiated).
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Model.Runtime.AttributedModel
+{
+    using System;
+
+    /// <summary>
+    /// Marks a classifier as being abstract (cannot be instantiated).
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
+    public class AbstractAttribute : Attribute
+    {
+    }
+}
