54305a6d1a0c046dc6e76f2f35139a5a20e20953
Ioan Crisan <ioan.crisan@quartz-software.com>
2016-05-15 01:19:45 +0300

    Simple Chat App

---
+++ Samples/SimpleChatWebApp/SignalRChat.WebApp/Application/SignalRAppInitializer.cs
@@ -0,0 +1,29 @@
+﻿namespace SignalRChat.WebApp.Application
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Threading.Tasks;
+    using Kephas.Web.Owin.Application;
+
+    using Owin;
+
+    /// <summary>
+    /// A SignalR application initializer.
+    /// </summary>
+    public class SignalRAppInitializer : OwinAppInitializerBase
+    {
+        /// <summary>Initializes the application asynchronously.</summary>
+        /// <param name="appContext">Context for the application.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A Task.</returns>
+        protected override Task InitializeCoreAsync(IOwinAppContext appContext, CancellationToken cancellationToken)
+        {
+            // Any connection or hub wire up and configuration should go here
+            var app = appContext.AppBuilder;
+            app.MapSignalR();
+
+            return TaskHelper.CompletedTask;
+        }
+    }
+}
---
+++ Samples/SimpleChatWebApp/SignalRChat.WebApp/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin;
+
+using SignalRChat.WebApp;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace SignalRChat.WebApp
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Kephas;
+    using Kephas.Application;
+    using Kephas.Logging.NLog;
+    using Kephas.Platform.Net45;
+    using Kephas.Threading.Tasks;
+    using Kephas.Web.Owin.Application;
+
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var ambientServices = this.InitializeAmbientServicesAsync().GetResultNonLocking(TimeSpan.FromMinutes(5));
+
+            var appContext = new OwinAppContext(app);
+            var bootstrapper = ambientServices.CompositionContainer.GetExport<IAppBootstrapper>();
+            bootstrapper.StartAsync(appContext).WaitNonLocking(TimeSpan.FromMinutes(5));
+        }
+
+        private async Task<IAmbientServices> InitializeAmbientServicesAsync()
+        {
+            var ambientServicesBuilder = new AmbientServicesBuilder();
+            await ambientServicesBuilder
+                    .WithNLogManager()
+                    .WithNet45AppEnvironment()
+                    .WithMefCompositionContainerAsync();
+            return ambientServicesBuilder.AmbientServices;
+        }
+    }
+}
