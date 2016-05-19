ad256f3a84e5795b1c4838bb0470e87457740a28
Ioan Crisan <ioan.crisan@quartz-software.com>
2016-05-19 10:10:37 +0300

    MongoDB 2.2.4 in externals

--- externals/nugets/BuildAndPublishNugets.bat
+++ externals/nugets/BuildAndPublishNugets.bat
@@ -1,7 +1,9 @@
-nuget pack MongoDB.Bson.signed.2.2.3\MongoDB.Bson.signed.nuspec -BasePath MongoDB.Bson.signed.2.2.3
-nuget pack MongoDB.Driver.Core.signed.2.2.3\MongoDB.Driver.Core.signed.nuspec -BasePath MongoDB.Driver.Core.signed.2.2.3
-nuget pack MongoDB.Driver.signed.2.2.3\MongoDB.Driver.signed.nuspec -BasePath MongoDB.Driver.signed.2.2.3
+nuget pack MongoDB.Bson.signed.2.2.4\MongoDB.Bson.signed.nuspec -BasePath MongoDB.Bson.signed.2.2.4
+nuget pack MongoDB.Driver.Core.signed.2.2.4\MongoDB.Driver.Core.signed.nuspec -BasePath MongoDB.Driver.Core.signed.2.2.4
+nuget pack MongoDB.Driver.signed.2.2.4\MongoDB.Driver.signed.nuspec -BasePath MongoDB.Driver.signed.2.2.4
+nuget pack MongoDB.Driver.GridFS.signed.2.2.4\MongoDB.Driver.GridFS.signed.nuspec -BasePath MongoDB.Driver.GridFS.signed.2.2.4
 
-nuget add MongoDB.Bson.signed.2.2.3.nupkg -source ..\local-package-source
-nuget add MongoDB.Driver.Core.signed.2.2.3.nupkg -source ..\local-package-source
-nuget add MongoDB.Driver.signed.2.2.3.nupkg -source ..\local-package-source
+nuget add MongoDB.Bson.signed.2.2.4.nupkg -source ..\local-package-source
+nuget add MongoDB.Driver.Core.signed.2.2.4.nupkg -source ..\local-package-source
+nuget add MongoDB.Driver.signed.2.2.4.nupkg -source ..\local-package-source
+nuget add MongoDB.Driver.GridFS.signed.2.2.4.nupkg -source ..\local-package-source
