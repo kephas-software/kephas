65e69c9dd3c1696ba82cbb10c0c158fecc39d1cb
Ioan Crisan <ioan.crisan@quartz-software.com>
2016-11-10 01:32:14 +0200

    Fixed a bug with TryGetEnumerableItemType.

---
+++ src/Tests/Kephas.Core.Tests/Reflection/TypeExtensionsTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Kephas.Core.Tests.Reflection
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using Kephas.Reflection;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TypeExtensionsTest
+    {
+        [Test]
+        public void TryGetEnumerableItemType_not_enumerable()
+        {
+            var itemType = typeof(int).TryGetEnumerableItemType();
+            Assert.IsNull(itemType);
+        }
+
+        [Test]
+        public void TryGetEnumerableItemType_closed_generic_collection()
+        {
+            var itemType = typeof(ICollection<string>).TryGetEnumerableItemType();
+            Assert.AreEqual(typeof(string), itemType);
+        }
+    }
+}
