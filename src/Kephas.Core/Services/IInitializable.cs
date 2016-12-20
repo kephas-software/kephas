e04c01ae0860f92fdc3884cf9504592b72b45c8f
Ioan Crisan <ioan.crisan@quartz-software.com>
2016-12-20 16:11:27 +0200

    Added synchronous IInitializable

---
+++ src/Kephas.Core/Services/IInitializable.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IInitializable.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Declares the IInitializable interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Services
+{
+    /// <summary>
+    /// Provides the <see cref="Initialize"/> method for service initialization.
+    /// </summary>
+    public interface IInitializable
+    {
+        /// <summary>
+        /// Initializes the service.
+        /// </summary>
+        /// <param name="context">An optional context for initialization.</param>
+        void Initialize(IContext context = null);
+    }
+}
