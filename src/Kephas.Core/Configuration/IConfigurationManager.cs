a20443081c8820af6f8988972d6ebf48f7dea648
Ioan Crisan <ioan.crisan@quartz-software.com>
2016-12-29 15:51:32 +0200

    Simplified the configuration manager

--- src/Kephas.Core/Configuration/IConfigurationManager.cs
+++ src/Kephas.Core/Configuration/IConfigurationManager.cs
@@ -9,7 +9,6 @@
 
 namespace Kephas.Configuration
 {
-    using System;
     using System.Diagnostics.Contracts;
 
     /// <summary>
@@ -25,21 +24,6 @@ namespace Kephas.Configuration
         /// <remarks>If the setting is not found, returns <c>null</c>.</remarks>
         /// <returns>The setting with the provided key.</returns>
         string GetSetting(string key);
-
-        /// <summary>
-        /// Gets the service settings.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service.</typeparam>
-        /// <typeparam name="TSettings">The type of the settings.</typeparam>
-        /// <returns>The settings for the provided service type.</returns>
-        TSettings GetServiceSettings<TService, TSettings>();
-
-        /// <summary>
-        /// Gets the service settings.
-        /// </summary>
-        /// <param name="serviceType">Type of the service.</param>
-        /// <returns>The settings for the provided service type.</returns>
-        object GetServiceSettings(Type serviceType);
     }
 
     /// <summary>
@@ -61,32 +45,6 @@ namespace Kephas.Configuration
         public string GetSetting(string key)
         {
             Contract.Requires(!string.IsNullOrEmpty(key));
-            return null;
-        }
-
-        /// <summary>
-        /// Gets the service settings.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service.</typeparam>
-        /// <typeparam name="TSettings">The type of the settings.</typeparam>
-        /// <returns>
-        /// The settings for the provided service type.
-        /// </returns>
-        public TSettings GetServiceSettings<TService, TSettings>()
-        {
-            return default(TSettings);
-        }
-
-        /// <summary>
-        /// Gets the service settings.
-        /// </summary>
-        /// <param name="serviceType">Type of the service.</param>
-        /// <returns>
-        /// The settings for the provided service type.
-        /// </returns>
-        public object GetServiceSettings(Type serviceType)
-        {
-            Contract.Requires(serviceType != null);
             return null;
         }
     }
--- src/Kephas.Core/Configuration/NullConfigurationManager.cs
+++ src/Kephas.Core/Configuration/NullConfigurationManager.cs
@@ -9,8 +9,6 @@
 
 namespace Kephas.Configuration
 {
-    using System;
-
     using Kephas.Services;
 
     /// <summary>
@@ -30,31 +28,6 @@ namespace Kephas.Configuration
         /// If the setting is not found, returns <c>null</c>.
         /// </remarks>
         public string GetSetting(string key)
-        {
-            return null;
-        }
-
-        /// <summary>
-        /// Gets the service settings.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service.</typeparam>
-        /// <typeparam name="TSettings">The type of the settings.</typeparam>
-        /// <returns>
-        /// The settings for the provided service type.
-        /// </returns>
-        public TSettings GetServiceSettings<TService, TSettings>()
-        {
-            return default(TSettings);
-        }
-
-        /// <summary>
-        /// Gets the service settings.
-        /// </summary>
-        /// <param name="serviceType">Type of the service.</param>
-        /// <returns>
-        /// The settings for the provided service type.
-        /// </returns>
-        public object GetServiceSettings(Type serviceType)
         {
             return null;
         }
