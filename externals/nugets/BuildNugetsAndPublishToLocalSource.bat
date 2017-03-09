ac17f72868ae55a1b7ce300b97198140f7a26660
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-03-09 11:13:23 +0200

    Parameterized the publish scripts.

---
+++ externals/nugets/BuildNugetsAndPublishToLocalSource.bat
@@ -0,0 +1,21 @@
+@echo off
+if [%1]==[] goto usage
+
+nuget pack MongoDB.Bson.signed.%1\MongoDB.Bson.signed.nuspec -BasePath MongoDB.Bson.signed.%1
+nuget pack MongoDB.Driver.Core.signed.%1\MongoDB.Driver.Core.signed.nuspec -BasePath MongoDB.Driver.Core.signed.%1
+nuget pack MongoDB.Driver.signed.%1\MongoDB.Driver.signed.nuspec -BasePath MongoDB.Driver.signed.%1
+nuget pack MongoDB.Driver.GridFS.signed.%1\MongoDB.Driver.GridFS.signed.nuspec -BasePath MongoDB.Driver.GridFS.signed.%1
+
+nuget add MongoDB.Bson.signed.%1.nupkg -source ..\local-package-source
+nuget add MongoDB.Driver.Core.signed.%1.nupkg -source ..\local-package-source
+nuget add MongoDB.Driver.signed.%1.nupkg -source ..\local-package-source
+nuget add MongoDB.Driver.GridFS.signed.%1.nupkg -source ..\local-package-source
+
+@echo Done.
+pause
+
+goto :eof
+:usage
+@echo Usage: %0 ^<version-number^>
+pause
+exit /B 1
