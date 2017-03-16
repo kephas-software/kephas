04ac45c90a854ac79a8572db430f6dbd62d1f830
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-03-16 18:29:10 +0200

    IClientMessageProcessor moved to Messaging.Client, also started the HttpClientMessageProcessor

--- src/Kephas.Messaging.Client/IClientMessageProcessor.cs
+++ src/Kephas.Messaging.Client/IClientMessageProcessor.cs
@@ -9,11 +9,9 @@
 
 namespace Kephas.Messaging.Client
 {
-    using System.Diagnostics.Contracts;
     using System.Threading;
     using System.Threading.Tasks;
 
-    using Kephas.Diagnostics.Contracts;
     using Kephas.Messaging;
     using Kephas.Services;
 
@@ -24,7 +22,6 @@ namespace Kephas.Messaging.Client
     /// The client message processor is defined as a shared service.
     /// </remarks>
     [SharedAppServiceContract]
-    [ContractClass(typeof(ClientMessageProcessorContractClass))]
     public interface IClientMessageProcessor
     {
         /// <summary>
@@ -42,37 +39,5 @@ namespace Kephas.Messaging.Client
         /// </summary>
         /// <param name="message">The message.</param>
         void ProcessOneWay(IMessage message);
-    }
-
-    /// <summary>
-    /// Contract class for <see cref="IClientMessageProcessor"/>.
-    /// </summary>
-    [ContractClassFor(typeof(IClientMessageProcessor))]
-    internal abstract class ClientMessageProcessorContractClass : IClientMessageProcessor
-    {
-        /// <summary>
-        /// Processes the specified message asynchronously.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="token">The cancellation token.</param>
-        /// <returns>
-        /// The response promise.
-        /// </returns>
-        public Task<IMessage> ProcessAsync(IMessage message, CancellationToken token)
-        {
-            Requires.NotNull(message, nameof(message));
-            Contract.Ensures(Contract.Result<Task<IMessage>>() != null);
-
-            return Contract.Result<Task<IMessage>>();
-        }
-
-        /// <summary>
-        /// Processes the specified message asynchronously by sending it to the server without waiting for a response.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        public void ProcessOneWay(IMessage message)
-        {
-            Requires.NotNull(message, nameof(message));
-        }
     }
 }
