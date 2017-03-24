39891fe289be2bc5bc0c010f63c572d7c8ab49d8
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-03-24 13:40:32 +0200

    Made the nuget packaging configurable (Debug/Release)

--- src/Deployment/CreateNugetPackages.cmd
+++ src/Deployment/CreateNugetPackages.cmd
@@ -1,18 +1,18 @@
-nuget pack ..\Kephas.Core\Package.nuspec -BasePath ..\Kephas.Core -Symbols
-nuget pack ..\Kephas.Logging.NLog\Package.nuspec -BasePath ..\Kephas.Logging.NLog -Symbols
-nuget pack ..\Kephas.Logging.Log4Net\Package.nuspec -BasePath ..\Kephas.Logging.Log4Net -Symbols
-nuget pack ..\Kephas.Composition.Mef\Package.nuspec -BasePath ..\Kephas.Composition.Mef -Symbols
-nuget pack ..\Kephas.Messaging\Package.nuspec -BasePath ..\Kephas.Messaging -Symbols
-nuget pack ..\Kephas.Messaging.Model\Package.nuspec -BasePath ..\Kephas.Messaging.Model -Symbols
-nuget pack ..\Kephas.Model\Package.nuspec -BasePath ..\Kephas.Model -Symbols
-nuget pack ..\Kephas.Data\Package.nuspec -BasePath ..\Kephas.Data -Symbols
-nuget pack ..\Kephas.Data.Client\Package.nuspec -BasePath ..\Kephas.Data.Client -Symbols
-nuget pack ..\Kephas.Data.Model\Package.nuspec -BasePath ..\Kephas.Data.Model -Symbols
-nuget pack ..\Kephas.Data.DocumentDB\Package.nuspec -BasePath ..\Kephas.Data.DocumentDB -Symbols
-nuget pack ..\Kephas.Data.MongoDB\Package.nuspec -BasePath ..\Kephas.Data.MongoDB -Symbols
-nuget pack ..\Kephas.Data.InMemory\Package.nuspec -BasePath ..\Kephas.Data.InMemory -Symbols
-nuget pack ..\Kephas.Serialization.Json\Package.nuspec -BasePath ..\Kephas.Serialization.Json -Symbols
-nuget pack ..\Kephas.Platform.Net\Package.nuspec -BasePath ..\Kephas.Platform.Net -Symbols
-nuget pack ..\Kephas.Mail\Package.nuspec -BasePath ..\Kephas.Mail -Symbols
-nuget pack ..\Kephas.Mail.Net46\Package.nuspec -BasePath ..\Kephas.Mail.Net46 -Symbols
-nuget pack ..\Kephas.Web.Owin\Package.nuspec -BasePath ..\Kephas.Web.Owin -Symbols
+nuget pack ..\Kephas.Core\Package.nuspec -BasePath ..\Kephas.Core -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Logging.NLog\Package.nuspec -BasePath ..\Kephas.Logging.NLog -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Logging.Log4Net\Package.nuspec -BasePath ..\Kephas.Logging.Log4Net -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Composition.Mef\Package.nuspec -BasePath ..\Kephas.Composition.Mef -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Messaging\Package.nuspec -BasePath ..\Kephas.Messaging -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Messaging.Model\Package.nuspec -BasePath ..\Kephas.Messaging.Model -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Model\Package.nuspec -BasePath ..\Kephas.Model -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Data\Package.nuspec -BasePath ..\Kephas.Data -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Data.Client\Package.nuspec -BasePath ..\Kephas.Data.Client -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Data.Model\Package.nuspec -BasePath ..\Kephas.Data.Model -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Data.DocumentDB\Package.nuspec -BasePath ..\Kephas.Data.DocumentDB -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Data.MongoDB\Package.nuspec -BasePath ..\Kephas.Data.MongoDB -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Data.InMemory\Package.nuspec -BasePath ..\Kephas.Data.InMemory -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Serialization.Json\Package.nuspec -BasePath ..\Kephas.Serialization.Json -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Platform.Net\Package.nuspec -BasePath ..\Kephas.Platform.Net -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Mail\Package.nuspec -BasePath ..\Kephas.Mail -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Mail.Net46\Package.nuspec -BasePath ..\Kephas.Mail.Net46 -Symbols -properties Configuration=Debug
+nuget pack ..\Kephas.Web.Owin\Package.nuspec -BasePath ..\Kephas.Web.Owin -Symbols -properties Configuration=Debug
