944f6ce323f2887c6fcfc704b22e6543a3d4395b
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-04-10 18:01:43 +0300

    Added the IniFormat

---
+++ src/Kephas.Core/Serialization/Ini/IniFormat.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IniFormat.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the initialize format class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Serialization.Ini
+{
+    /// <summary>
+    /// Marker class for the INI format.
+    /// </summary>
+    public class IniFormat : IFormat
+    {
+    }
+}
