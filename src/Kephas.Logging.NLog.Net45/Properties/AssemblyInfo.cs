d538e57995f297a03752027b5cf18d3df01ce9e1
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-04-11 14:47:07 +0300

    Kephas.Logging.NLog (1.3)

--- src/Kephas.Logging.NLog.Net45/Properties/AssemblyInfo.cs
+++ src/Kephas.Logging.NLog.Net45/Properties/AssemblyInfo.cs
@@ -1,4 +1,13 @@
-﻿using System.Reflection;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the assembly information class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
