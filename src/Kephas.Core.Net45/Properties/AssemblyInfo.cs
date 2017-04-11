8cc70a6aa5eb73c5fd7c0449a0f864e370d526f7
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-04-11 11:27:27 +0300

    Refactored Kephas.Core.PortableLegacy to Kephas.Core.Net45

--- src/Kephas.Core.Net45/Properties/AssemblyInfo.cs
+++ src/Kephas.Core.Net45/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@
 
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kephas.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides a common infrastructure for all the other Kephas Framework components: ambient services, dynamic reflection, composition, application management, and others.")]
