973f33e5d3e6ccbf5381aee43986f68ffb812ef6
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-04-21 02:34:20 +0300

    Added ToListAsync also to IQueryable.

--- src/Kephas.Data/Linq/QueryableExtensions.cs
+++ src/Kephas.Data/Linq/QueryableExtensions.cs
@@ -9,11 +9,15 @@
 
 namespace Kephas.Data.Linq
 {
+    using System.Collections;
     using System.Collections.Generic;
     using System.Diagnostics.Contracts;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
+
+    using Kephas.Diagnostics.Contracts;
+    using Kephas.Threading.Tasks;
 
     /// <summary>
     /// Extension methods for <see cref="IQueryable{T}"/>.
@@ -29,7 +33,7 @@ namespace Kephas.Data.Linq
         /// <returns>A list of items.</returns>
         public static Task<List<T>> ToListAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default(CancellationToken))
         {
-            Contract.Requires(query != null);
+            Requires.NotNull(query, nameof(query));
 
             var asyncProvider = query.Provider as IAsyncQueryProvider;
             if (asyncProvider != null)
@@ -38,6 +42,17 @@ namespace Kephas.Data.Linq
             }
 
             return Task.FromResult(query.ToList());
+        }
+
+        /// <summary>
+        /// Executes the query asynchronously and returns a list of items.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A list of items.</returns>
+        public static Task<List<object>> ToListAsync(this IQueryable query, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return ToListAsync<object>((IQueryable<object>)query, cancellationToken);
         }
     }
 }
