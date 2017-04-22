1cf991a2d299e5cfdcab850647fc97c75d7608b4
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-04-22 20:59:52 +0300

    Add missing default implementation of the IGraphOperationContext.

---
+++ src/Kephas.Data/Capabilities/GraphOperationContext.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GraphOperationContext.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the graph operation context class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Capabilities
+{
+    /// <summary>
+    /// A graph operation context.
+    /// </summary>
+    public class GraphOperationContext : DataOperationContext, IGraphOperationContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GraphOperationContext"/> class.
+        /// </summary>
+        /// <param name="dataContext">Context for the data.</param>
+        public GraphOperationContext(IDataContext dataContext)
+            : base(dataContext)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the loose parts should be loaded.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if loose parts should be loaded, <c>false</c> if not.
+        /// </value>
+        public bool LoadLooseParts { get; set; }
+    }
+}
