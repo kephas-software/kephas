c6e050c5a8e533fa11c82d7be6e299eef92c1731
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-04-25 14:27:27 +0300

    Added GetEntityInfo to IDataContextCache, to be able to optimize it.

--- src/Kephas.Data/Caching/IDataContextCache.cs
+++ src/Kephas.Data/Caching/IDataContextCache.cs
@@ -18,5 +18,13 @@ namespace Kephas.Data.Caching
     /// </summary>
     public interface IDataContextCache : IDictionary<Id, IEntityInfo>
     {
+        /// <summary>
+        /// Gets the entity information associated to the provided entity, or <c>null</c> if none could be found.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns>
+        /// The entity information or <c>null</c>.
+        /// </returns>
+        IEntityInfo GetEntityInfo(object entity);
     }
 }
