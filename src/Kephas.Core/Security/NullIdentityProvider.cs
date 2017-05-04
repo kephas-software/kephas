b98e157333d7341f143f0ef56fefc096677a5208
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-05-04 09:48:17 +0300

    Added an Identity provider service

---
+++ src/Kephas.Core/Security/NullIdentityProvider.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NullIdentityProvider.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the null identity provider class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Security
+{
+    using System.Security.Principal;
+
+    using Kephas.Services;
+
+    /// <summary>
+    /// A null identity provider.
+    /// </summary>
+    [OverridePriority(Priority.Lowest)]
+    public class NullIdentityProvider : IIdentityProvider
+    {
+        /// <summary>
+        /// Gets the current identity.
+        /// </summary>
+        /// <returns>
+        /// The current identity.
+        /// </returns>
+        public IIdentity GetCurrentIdentity()
+        {
+            return null;
+        }
+    }
+}
