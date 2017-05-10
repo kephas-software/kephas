2814136d5d62272b46cd7dfbbcf14bda07187983
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-05-10 10:53:45 +0300

    Added Base64Data class

---
+++ src/Kephas.Core/Text/Encoding/Base64Data.cs
@@ -0,0 +1,142 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Base64Data.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the base 64 class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Text.Encoding
+{
+    using System;
+    using System.Text;
+
+    using Kephas.Diagnostics.Contracts;
+
+    /// <summary>
+    /// A Base64 data.
+    /// </summary>
+    public class Base64Data
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Base64Data"/> class.
+        /// </summary>
+        /// <param name="base64String">The base64 string.</param>
+        public Base64Data(string base64String)
+        {
+            Requires.NotNullOrEmpty(base64String, nameof(base64String));
+
+            var indexOfComma = base64String.IndexOf(",");
+            if (indexOfComma > 0)
+            {
+                this.MimeType = base64String.Substring(0, indexOfComma);
+                this.PlainString = base64String.Substring(indexOfComma + 1);
+            }
+            else
+            {
+                this.PlainString = base64String;
+            }
+
+            this.Bytes = Convert.FromBase64String(this.PlainString);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Base64Data"/> class.
+        /// </summary>
+        /// <param name="bytes">The bytes.</param>
+        /// <param name="mimeType">The MIME type (optional).</param>
+        public Base64Data(byte[] bytes, string mimeType = null)
+        {
+            Requires.NotNull(bytes, nameof(bytes));
+
+            this.Bytes = bytes;
+            this.MimeType = mimeType;
+        }
+
+        /// <summary>
+        /// Gets the MIME type.
+        /// </summary>
+        /// <value>
+        /// The MIME type.
+        /// </value>
+        public string MimeType { get; }
+
+        /// <summary>
+        /// Gets the bytes.
+        /// </summary>
+        /// <value>
+        /// The bytes.
+        /// </value>
+        public byte[] Bytes { get; }
+
+        /// <summary>
+        /// Gets the plain string.
+        /// </summary>
+        /// <value>
+        /// The plain string.
+        /// </value>
+        public string PlainString { get; }
+
+        /// <summary>
+        /// Initializes a new <see cref="Base64Data"/> from the given Base64 string.
+        /// </summary>
+        /// <param name="base64String">The base64 string.</param>
+        /// <returns>
+        /// A <see cref="Base64Data"/> instance representing the provided string.
+        /// </returns>
+        public static Base64Data FromBase64String(string base64String)
+        {
+            return new Base64Data(base64String);
+        }
+
+        /// <summary>
+        /// Converts the provided bytes to a base 64 string.
+        /// </summary>
+        /// <param name="bytes">The bytes to convert.</param>
+        /// <param name="mimeType">The MIME type (optional).</param>
+        /// <returns>
+        /// The given data converted to a Base64 string.
+        /// </returns>
+        public static string ToBase64String(byte[] bytes, string mimeType = null)
+        {
+            return new Base64Data(bytes, mimeType).ToBase64String();
+        }
+
+        /// <summary>
+        /// Converts the provided plain string to a base 64 string.
+        /// </summary>
+        /// <param name="plainString">The plain string to convert.</param>
+        /// <param name="mimeType">The MIME type (optional).</param>
+        /// <returns>
+        /// The given string converted to a Base64 string.
+        /// </returns>
+        public static string ToBase64String(string plainString, string mimeType = null)
+        {
+            Requires.NotNull(plainString, nameof(plainString));
+
+            var bytes = Encoding.UTF8.GetBytes(plainString);
+
+            return ToBase64String(bytes, mimeType);
+        }
+
+        /// <summary>
+        /// Converts this object to a Base64 string.
+        /// </summary>
+        /// <returns>
+        /// This object as a Base64 string.
+        /// </returns>
+        public string ToBase64String()
+        {
+            var sb = new StringBuilder();
+            if (this.MimeType != null)
+            {
+                sb.Append(this.MimeType).Append(", ");
+            }
+
+            sb.Append(Convert.ToBase64String(this.Bytes));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/Tests/Kephas.Core.Tests/Text/Encoding/Base64DataTest.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Base64DataTest.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the base 64 data test class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Core.Tests.Text.Encoding
+{
+    using System;
+    using System.Text;
+
+    using Kephas.Text.Encoding;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Base64DataTest
+    {
+        [Test]
+        public void ToBase64String_no_mime_type()
+        {
+            var base64String = Base64Data.ToBase64String("Hi there");
+            Assert.AreEqual("SGkgdGhlcmU=", base64String);
+        }
+
+        [Test]
+        public void ToBase64String_with_mime_type()
+        {
+            var base64String = Base64Data.ToBase64String("Hi there", "application/json");
+            Assert.AreEqual("application/json, SGkgdGhlcmU=", base64String);
+        }
+
+        [Test]
+        public void FromBase64String_no_mime_type()
+        {
+            var base64Data = Base64Data.FromBase64String("SGkgdGhlcmU=");
+            Assert.AreEqual("Hi there", Encoding.UTF8.GetString(base64Data.Bytes));
+        }
+
+        [Test]
+        public void FromBase64String_with_mime_type()
+        {
+            var base64Data = Base64Data.FromBase64String("application/json, SGkgdGhlcmU=");
+            Assert.AreEqual("Hi there", Encoding.UTF8.GetString(base64Data.Bytes));
+            Assert.AreEqual("application/json", base64Data.MimeType);
+        }
+    }
+}
