e9ae431b0326b9ec2e668dcad8dc850e65b2bc6a
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-05-11 15:14:17 +0300

    Added the IDomainScopeDimensionElement

---
+++ src/Kephas.Data.Model/Dimensions/Scope/IDomainScopeDimensionElement.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IDomainScopeDimensionElement.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Declares the IDomainScopeDimensionElement interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Model.Dimensions.Scope
+{
+    using Kephas.Model.AttributedModel;
+    using Kephas.Model.Dimensions;
+
+    /// <summary>
+    /// The Domain element of the <see cref="IScopeDimension"/>.
+    /// </summary>
+    [ModelDimensionElement]
+    public interface IDomainScopeDimensionElement
+    {
+    }
+}
