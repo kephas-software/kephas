c937aeb2e78e7dc1cf9f84a896ecb45ebdfa0e1f
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-05-18 14:48:14 +0300

    Added tests for CodeAnalysis

---
+++ src/Tests/Kephas.CodeAnalysis.Tests/Generation/CodeFormatterExtensionsTest.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CodeFormatterExtensionsTest.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the code formatter extensions test class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.CodeAnalysis.Tests.Generation
+{
+    using Kephas.Generation;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CodeFormatterExtensionsTest
+    {
+        [Test]
+        public void CurrentIndent()
+        {
+            var formatter = new CodeFormatter();
+            Assert.AreEqual(string.Empty, formatter.CurrentIndent());
+
+            formatter.IncreaseIndent();
+            Assert.AreEqual("  ", formatter.CurrentIndent());
+        }
+    }
+}
