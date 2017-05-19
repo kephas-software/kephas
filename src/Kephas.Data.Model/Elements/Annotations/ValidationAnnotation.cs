f5f8c3ff8cba7af5cee9d8c54324ebbfe2599ca1
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-05-19 15:36:51 +0300

    Added validation annotations

---
+++ src/Kephas.Data.Model/Elements/Annotations/ValidationAnnotation.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ValidationAnnotation.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the validation annotation class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Model.Elements.Annotations
+{
+    using Kephas.Model.Construction;
+    using Kephas.Model.Elements;
+
+    /// <summary>
+    /// A validation annotation.
+    /// </summary>
+    public class ValidationAnnotation : Annotation
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidationAnnotation"/> class.
+        /// </summary>
+        /// <param name="constructionContext">Context for the construction.</param>
+        /// <param name="name">The model element name.</param>
+        public ValidationAnnotation(IModelConstructionContext constructionContext, string name)
+            : base(constructionContext, name)
+        {
+        }
+    }
+}
---
+++ src/Kephas.Data.Model/Runtime/Construction/Annotations/ValidationAnnotationConstructor.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ValidationAnnotationConstructor.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the validation annotation constructor class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Model.Runtime.Construction.Annotations
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Kephas.Data.Model.Elements.Annotations;
+    using Kephas.Model.Construction;
+    using Kephas.Model.Runtime.Construction;
+
+    /// <summary>
+    /// A validation annotation constructor.
+    /// </summary>
+    public class ValidationAnnotationConstructor : AnnotationConstructorBase<ValidationAnnotation, ValidationAttribute>
+    {
+        /// <summary>
+        /// The static instance of the <see cref="ValidationAnnotationConstructor"/>.
+        /// </summary>
+        public static readonly ValidationAnnotationConstructor Instance = new ValidationAnnotationConstructor();
+
+        /// <summary>
+        /// Core implementation of trying to get the element information.
+        /// </summary>
+        /// <param name="constructionContext">Context for the construction.</param>
+        /// <param name="runtimeElement">The runtime element.</param>
+        /// <returns>
+        /// A new element information based on the provided runtime element information, or <c>null</c>
+        /// if the runtime element information is not supported.
+        /// </returns>
+        protected override ValidationAnnotation TryCreateModelElementCore(IModelConstructionContext constructionContext, ValidationAttribute runtimeElement)
+        {
+            return new ValidationAnnotation(constructionContext, this.TryComputeName(constructionContext, runtimeElement));
+        }
+    }
+}
