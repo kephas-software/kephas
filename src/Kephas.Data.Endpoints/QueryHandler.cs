4dee445ce5fb5a4f6e674f25195fc2ab34109aff
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-06-13 14:50:16 +0300

    Added Kephas.Data.Endpoints for client-server communication.

---
+++ src/Kephas.Data.Endpoints/QueryHandler.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="QueryHandler.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the query handler class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Endpoints
+{
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Data.Client.Queries;
+    using Kephas.Diagnostics.Contracts;
+    using Kephas.Messaging.Server;
+    using Kephas.Threading.Tasks;
+
+    /// <summary>
+    /// Message handler for <see cref="QueryMessage"/>.
+    /// </summary>
+    public class QueryHandler : MessageHandlerBase<QueryMessage, QueryResponseMessage>
+    {
+        /// <summary>
+        /// The client query executor.
+        /// </summary>
+        private readonly IClientQueryExecutor clientQueryExecutor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueryHandler"/> class.
+        /// </summary>
+        /// <param name="clientQueryExecutor">The client query executor.</param>
+        public QueryHandler(IClientQueryExecutor clientQueryExecutor)
+        {
+            Requires.NotNull(clientQueryExecutor, nameof(clientQueryExecutor));
+
+            this.clientQueryExecutor = clientQueryExecutor;
+        }
+
+        /// <summary>
+        /// Processes the provided message asynchronously and returns a response promise.
+        /// </summary>
+        /// <param name="message">The message to be handled.</param>
+        /// <param name="context">The processing context.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// The response promise.
+        /// </returns>
+        public override async Task<QueryResponseMessage> ProcessAsync(
+            QueryMessage message,
+            IMessageProcessingContext context,
+            CancellationToken token)
+        {
+            var clientEntities = await this.clientQueryExecutor.ExecuteQueryAsync(message.Query, token)
+                                     .PreserveThreadContext();
+
+            return new QueryResponseMessage { Entities = clientEntities.ToArray() };
+        }
+    }
+}
---
+++ src/Kephas.Data.Endpoints/QueryMessage.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="QueryMessage.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the query message class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Endpoints
+{
+    using Kephas.Data.Client.Queries;
+    using Kephas.Messaging;
+
+    /// <summary>
+    /// A query message.
+    /// </summary>
+    public class QueryMessage : IMessage
+    {
+        /// <summary>
+        /// Gets or sets the query to be executed.
+        /// </summary>
+        /// <value>
+        /// The query to be executed.
+        /// </value>
+        public ClientQuery Query { get; set; }
+    }
+
+    /// <summary>
+    /// A query response message.
+    /// </summary>
+    public class QueryResponseMessage : IMessage
+    {
+        /// <summary>
+        /// Gets or sets the entities.
+        /// </summary>
+        /// <value>
+        /// The entities.
+        /// </value>
+        public object[] Entities { get; set; }
+    }
+}
