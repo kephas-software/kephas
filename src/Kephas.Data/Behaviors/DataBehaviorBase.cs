26b9fcbd2c0d91fcf306757c262767965a222535
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-06-13 13:31:57 +0300

    Made the Async methods in DataBehaviorBase typed.

--- src/Kephas.Data/Behaviors/DataBehaviorBase.cs
+++ src/Kephas.Data/Behaviors/DataBehaviorBase.cs
@@ -75,9 +75,24 @@ namespace Kephas.Data.Behaviors
         /// <returns>
         /// A Task.
         /// </returns>
-        public virtual Task BeforePersistAsync(object entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken = default(CancellationToken))
+        Task IOnPersistBehavior.BeforePersistAsync(object entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken)
         {
-            this.BeforePersist((TEntity)entity, entityInfo, operationContext);
+            return this.BeforePersistAsync((TEntity)entity, entityInfo, operationContext, cancellationToken);
+        }
+
+        /// <summary>
+        /// Callback invoked before an entity is being persisted.
+        /// </summary>
+        /// <param name="entity">The entity to be persisted.</param>
+        /// <param name="entityInfo">The entity information.</param>
+        /// <param name="operationContext">The operation context.</param>
+        /// <param name="cancellationToken">The cancellation token (optional).</param>
+        /// <returns>
+        /// A Task.
+        /// </returns>
+        public virtual Task BeforePersistAsync(TEntity entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            this.BeforePersist(entity, entityInfo, operationContext);
 
             return Task.FromResult(0);
         }
@@ -92,9 +107,24 @@ namespace Kephas.Data.Behaviors
         /// <returns>
         /// A Task.
         /// </returns>
-        public virtual Task AfterPersistAsync(object entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken = default(CancellationToken))
+        Task IOnPersistBehavior.AfterPersistAsync(object entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken)
         {
-            this.AfterPersist((TEntity)entity, entityInfo, operationContext);
+            return this.AfterPersistAsync((TEntity)entity, entityInfo, operationContext, cancellationToken);
+        }
+
+        /// <summary>
+        /// Callback invoked after an entity was persisted.
+        /// </summary>
+        /// <param name="entity">The entity to be persisted.</param>
+        /// <param name="entityInfo">The entity information.</param>
+        /// <param name="operationContext">The operation context.</param>
+        /// <param name="cancellationToken">The cancellation token (optional).</param>
+        /// <returns>
+        /// A Task.
+        /// </returns>
+        public virtual Task AfterPersistAsync(TEntity entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            this.AfterPersist(entity, entityInfo, operationContext);
 
             return Task.FromResult(0);
         }
@@ -109,9 +139,24 @@ namespace Kephas.Data.Behaviors
         /// <returns>
         /// A Task.
         /// </returns>
-        public virtual Task InitializeAsync(object entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken = default(CancellationToken))
+        Task IOnInitializeBehavior.InitializeAsync(object entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken)
         {
-            this.Initialize((TEntity)entity, entityInfo, operationContext);
+            return this.InitializeAsync((TEntity)entity, entityInfo, operationContext, cancellationToken);
+        }
+
+        /// <summary>
+        /// Initializes the entity asynchronously.
+        /// </summary>
+        /// <param name="entity">The entitiy to be initialized.</param>
+        /// <param name="entityInfo">The entity information.</param>
+        /// <param name="operationContext">The operation context.</param>
+        /// <param name="cancellationToken">The cancellation token (optional).</param>
+        /// <returns>
+        /// A Task.
+        /// </returns>
+        public virtual Task InitializeAsync(TEntity entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            this.Initialize(entity, entityInfo, operationContext);
 
             return Task.FromResult(0);
         }
@@ -126,9 +171,24 @@ namespace Kephas.Data.Behaviors
         /// <returns>
         /// A promise of a <see cref="IDataValidationResult"/>.
         /// </returns>
-        public virtual Task<IDataValidationResult> ValidateAsync(object entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken = default(CancellationToken))
+        Task<IDataValidationResult> IOnValidateBehavior.ValidateAsync(object entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken)
         {
-            return Task.FromResult(this.Validate((TEntity)entity, entityInfo, operationContext));
+            return this.ValidateAsync((TEntity)entity, entityInfo, operationContext, cancellationToken);
+        }
+
+        /// <summary>
+        /// Validates the provided instance asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to be validated.</param>
+        /// <param name="entityInfo">The entity information.</param>
+        /// <param name="operationContext">Context for the validation operation.</param>
+        /// <param name="cancellationToken">The cancellation token (optional).</param>
+        /// <returns>
+        /// A promise of a <see cref="IDataValidationResult"/>.
+        /// </returns>
+        public virtual Task<IDataValidationResult> ValidateAsync(TEntity entity, IEntityInfo entityInfo, IDataOperationContext operationContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult(this.Validate(entity, entityInfo, operationContext));
         }
     }
 }
