d4a4dfe88211c7a5ca59b56c0845db3241796282
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-09-18 11:42:23 +0300

    Initial support for ASP.NET Core

---
+++ src/Kephas.AspNetCore/Services/Composition/ServiceCollectionConventionsRegistrar.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ServiceCollectionConventionsRegistrar.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the service collection conventions registrar class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.AspNetCore.Services.Composition
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    using Kephas.Composition.Conventions;
+    using Kephas.Composition.Hosting;
+
+    /// <summary>
+    /// A conventions registrar for a collection of service definitions.
+    /// </summary>
+    public class ServiceCollectionConventionsRegistrar : IConventionsRegistrar
+    {
+        /// <summary>
+        /// Registers the conventions.
+        /// </summary>
+        /// <param name="builder">The registration builder.</param>
+        /// <param name="candidateTypes">The candidate types which can take part in the composition.</param>
+        /// <param name="registrationContext">Context for the registration.</param>
+        public void RegisterConventions(
+            IConventionsBuilder builder,
+            IEnumerable<TypeInfo> candidateTypes,
+            ICompositionRegistrationContext registrationContext)
+        {
+            // TODO get the services collection from the registation context expando
+            // put there by the WebStartup
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Kephas.AspNetCore/WebStartup.cs
@@ -0,0 +1,85 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WebStartupBase.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the web startup class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.AspNetCore
+{
+    using System;
+    using System.ComponentModel;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// The startup class.
+    /// </summary>
+    public abstract class WebStartupBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebStartup"/> class.
+        /// </summary>
+        /// <param name="env">The environment.</param>
+        protected WebStartupBase(IHostingEnvironment env)
+        {
+            //var builder = new ConfigurationBuilder()
+            //    .SetBasePath(env.ContentRootPath)
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            //    .AddEnvironmentVariables();
+            //this.Configuration = builder.Build();
+        }
+
+        public IContainer ApplicationContainer { get; private set; }
+
+        public IConfigurationRoot Configuration { get; private set; }
+
+        // ConfigureServices is where you register dependencies. This gets
+        // called by the runtime before the Configure method, below.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            // TODO
+
+            ////// Create the container builder.
+            ////var builder = new ContainerBuilder();
+
+            ////// Register dependencies, populate the services from
+            ////// the collection, and build the container. If you want
+            ////// to dispose of the container at the end of the app,
+            ////// be sure to keep a reference to it as a property or field.
+            ////builder.RegisterType<MyType>().As<IMyType>();
+            ////builder.Populate(services);
+            ////this.ApplicationContainer = builder.Build();
+
+            ////// Create the IServiceProvider based on the container.
+            ////return new AutofacServiceProvider(this.ApplicationContainer);
+
+            return null;
+        }
+
+        // Configure is where you add middleware. This is called after
+        // ConfigureServices. You can use IApplicationBuilder.ApplicationServices
+        // here if you need to resolve things from the container.
+        public void Configure(
+          IApplicationBuilder app,
+          ILoggerFactory loggerFactory,
+          IApplicationLifetime appLifetime)
+        {
+            ////loggerFactory.AddConsole(this.Configuration.GetSection("Logging"));
+            ////loggerFactory.AddDebug();
+
+            ////app.UseMvc();
+
+            // If you want to dispose of resources that have been resolved in the
+            // application container, register for the "ApplicationStopped" event.
+            appLifetime.ApplicationStopped.Register(() => this.ApplicationContainer.Dispose());
+        }
+    }
+}
