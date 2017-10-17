b9c21092633bd64e26b2999d28784ddd25652dd6
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-10-17 11:45:36 +0300

    Added reading the System  SMTP email settings from configuration

---
+++ src/Kephas.Mail.MailKit/Configuration/SystemEmailSenderSettings.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SystemEmailSenderSettings.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the system email sender settings class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Mail.Configuration
+{
+    /// <summary>
+    /// A system email sender settings.
+    /// </summary>
+    public class SystemEmailSenderSettings
+    {
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>
+        /// The name of the user.
+        /// </value>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the host.
+        /// </summary>
+        /// <value>
+        /// The host.
+        /// </value>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// Gets or sets the port.
+        /// </summary>
+        /// <value>
+        /// The port.
+        /// </value>
+        public string Port { get; set; }
+    }
+}
--- src/Kephas.Mail.MailKit/Services/MailKitSystemEmailSenderService.cs
+++ src/Kephas.Mail.MailKit/Services/MailKitSystemEmailSenderService.cs
@@ -9,13 +9,62 @@
 
 namespace Kephas.Mail.Services
 {
+    using System.Net;
+
+    using Kephas.Configuration;
+    using Kephas.Diagnostics.Contracts;
+    using Kephas.Mail.Configuration;
     using Kephas.Services;
 
     /// <summary>
     /// A MailKit system email sender service.
     /// </summary>
     [OverridePriority(Priority.Low)]
-    public class MailKitSystemEmailSenderService: MailKitEmailSenderService, ISystemEmailSenderService
+    public class MailKitSystemEmailSenderService : MailKitEmailSenderService, ISystemEmailSenderService
     {
+        /// <summary>
+        /// The system email sender configuration.
+        /// </summary>
+        private readonly IConfiguration<SystemEmailSenderSettings> systemEmailSenderConfig;
+
+        /// <summary>
+        /// Information describing the connection.
+        /// </summary>
+        private (ICredentials credentials, string host, int port)? connectionData;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MailKitSystemEmailSenderService"/> class.
+        /// </summary>
+        /// <param name="systemEmailSenderConfig">The system email sender configuration.</param>
+        public MailKitSystemEmailSenderService(IConfiguration<SystemEmailSenderSettings> systemEmailSenderConfig)
+        {
+            Requires.NotNull(systemEmailSenderConfig, nameof(systemEmailSenderConfig));
+
+            this.systemEmailSenderConfig = systemEmailSenderConfig;
+        }
+
+        /// <summary>
+        /// Gets the connection data.
+        /// </summary>
+        /// <param name="emailMessage">The email message.</param>
+        /// <returns>
+        /// The connection data.
+        /// </returns>
+        protected override (ICredentials credentials, string host, int port) GetConnectionData(IEmailMessage emailMessage)
+        {
+            return this.connectionData ?? (this.connectionData = this.ComputeConnectionData()).Value;
+        }
+
+        /// <summary>
+        /// Calculates the connection data.
+        /// </summary>
+        /// <returns>
+        /// The calculated connection data.
+        /// </returns>
+        private (NetworkCredential, string Host, int) ComputeConnectionData()
+        {
+            var settings = this.systemEmailSenderConfig.Settings;
+            return (new NetworkCredential(settings.UserName, settings.Password), settings.Host, int.Parse(settings.Port));
+        }
     }
 }
