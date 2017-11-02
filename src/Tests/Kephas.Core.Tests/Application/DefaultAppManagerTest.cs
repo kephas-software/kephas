408f355e437233e62eaee91f8bfdccc05e49f2c0
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-11-02 23:38:44 +0200

    Added some more DefaultAppManager tests

--- src/Tests/Kephas.Core.Tests/Application/DefaultAppManagerTest.cs
+++ src/Tests/Kephas.Core.Tests/Application/DefaultAppManagerTest.cs
@@ -12,6 +12,7 @@ namespace Kephas.Core.Tests.Application
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -20,6 +21,7 @@ namespace Kephas.Core.Tests.Application
     using Kephas.Composition;
     using Kephas.Composition.ExportFactories;
     using Kephas.Composition.ExportFactoryImporters;
+    using Kephas.Logging;
     using Kephas.Services;
     using Kephas.Services.Behavior;
     using Kephas.Services.Behavior.Composition;
@@ -40,7 +42,7 @@ namespace Kephas.Core.Tests.Application
             var featureManager2 = this.CreateFeatureManager();
             var featureManager3 = this.CreateFeatureManager();
 
-            Assert.Throws<InvalidOperationException>(() => 
+            Assert.Throws<InvalidOperationException>(() =>
                 new DefaultAppManager(
                     Substitute.For<IAppManifest>(),
                     this.GetAmbientServices(),
@@ -194,20 +196,9 @@ namespace Kephas.Core.Tests.Application
         {
             var order = new List<int>();
 
-            var featureManager1 = Substitute.For<IFeatureManager>();
-            featureManager1.InitializeAsync(Arg.Any<IAppContext>(), Arg.Any<CancellationToken>())
-                .Returns(Task.FromResult(0))
-                .AndDoes(_ => order.Add(1));
-
-            var featureManager2 = Substitute.For<IFeatureManager>();
-            featureManager2.InitializeAsync(Arg.Any<IAppContext>(), Arg.Any<CancellationToken>())
-                .Returns(Task.FromResult(0))
-                .AndDoes(_ => order.Add(2));
-
-            var featureManager3 = Substitute.For<IFeatureManager>();
-            featureManager3.InitializeAsync(Arg.Any<IAppContext>(), Arg.Any<CancellationToken>())
-                .Returns(Task.FromResult(0))
-                .AndDoes(_ => order.Add(3));
+            var featureManager1 = this.CreateFeatureManager(initialization: _ => order.Add(1));
+            var featureManager2 = this.CreateFeatureManager(initialization: _ => order.Add(2));
+            var featureManager3 = this.CreateFeatureManager(initialization: _ => order.Add(3));
 
             var appManager = new DefaultAppManager(
                 Substitute.For<IAppManifest>(),
@@ -216,9 +207,9 @@ namespace Kephas.Core.Tests.Application
                 new List<IExportFactory<IAppLifecycleBehavior, AppServiceMetadata>>(),
                 new[]
                     {
-                        new ExportFactory<IFeatureManager, FeatureManagerMetadata>(() => featureManager1, new FeatureManagerMetadata(new FeatureInfo("1", "1.0", dependencies: new[] { "3" }))),
-                        new ExportFactory<IFeatureManager, FeatureManagerMetadata>(() => featureManager2, new FeatureManagerMetadata(new FeatureInfo("2", "1.0", dependencies: new[] { "3" }))),
-                        new ExportFactory<IFeatureManager, FeatureManagerMetadata>(() => featureManager3, new FeatureManagerMetadata(new FeatureInfo("3", "1.0"))),
+                        this.CreateFeatureManagerFactory(featureManager1, "1", "1.0", dependencies: new[] { "3" }),
+                        this.CreateFeatureManagerFactory(featureManager2, "2", "1.0", dependencies: new[] { "3" }),
+                        this.CreateFeatureManagerFactory(featureManager3, "3", "1.0"),
                     },
                 null);
 
@@ -228,6 +219,108 @@ namespace Kephas.Core.Tests.Application
             Assert.AreEqual(3, order[0]);
             Assert.AreEqual(1, order[1]);
             Assert.AreEqual(2, order[2]);
+        }
+
+        [Test]
+        public async Task InitializeAppAsync_features_set_in_app_manifest()
+        {
+            var featureManager1 = this.CreateFeatureManager();
+            var featureManager2 = this.CreateFeatureManager();
+            var featureManager3 = this.CreateFeatureManager();
+
+            var factory1 = this.CreateFeatureManagerFactory(featureManager1, "1", "1.0", dependencies: new[] { "3" }, isRequired: true);
+            var factory2 = this.CreateFeatureManagerFactory(featureManager2, "2", "1.0", dependencies: new[] { "3" }, isRequired: false);
+            var factory3 = this.CreateFeatureManagerFactory(featureManager3, "3", "1.0", isRequired: true);
+
+            var appManifest = Substitute.For<AppManifestBase>("app-id", new Version(1, 2, 3, 4), (IEnumerable<IFeatureInfo>)null);
+            var appManager = new DefaultAppManager(
+                appManifest,
+                this.GetAmbientServices(),
+                this.GetServiceBehaviorProvider(),
+                new List<IExportFactory<IAppLifecycleBehavior, AppServiceMetadata>>(),
+                new[] { factory1, factory2, factory3 },
+                null);
+
+            await appManager.InitializeAppAsync(Substitute.For<IAppContext>(), CancellationToken.None);
+
+            Assert.AreEqual(3, appManifest.Features.Count());
+
+            var featureList = appManifest.Features.ToList();
+            Assert.AreEqual("3", featureList[0].Name);
+            Assert.AreEqual("1", featureList[1].Name);
+            Assert.AreEqual("2", featureList[2].Name);
+        }
+
+        [Test]
+        public async Task InitializeAppAsync_ok_failed_optional_feature()
+        {
+            var order = new List<int>();
+
+            var featureManager1 = this.CreateFeatureManager(initialization: _ => order.Add(1));
+            var featureManager2 = this.CreateFeatureManager(initialization: _ => throw new InvalidOperationException("bad guy"));
+            var featureManager3 = this.CreateFeatureManager(initialization: _ => order.Add(3));
+
+            var factory1 = this.CreateFeatureManagerFactory(featureManager1, "1", "1.0", dependencies: new[] { "3" }, isRequired: true);
+            var factory2 = this.CreateFeatureManagerFactory(featureManager2, "2", "1.0", dependencies: new[] { "3" }, isRequired: false);
+            var factory3 = this.CreateFeatureManagerFactory(featureManager3, "3", "1.0", isRequired: true);
+
+            var sb = new StringBuilder();
+            var logger = Substitute.For<ILogger<IAppManager>>();
+            logger
+                .WhenForAnyArgs(l => l.Log(LogLevel.Debug, null, null, null))
+                .Do(ci => sb.AppendLine($"{ci.Arg<LogLevel>()}: {ci.Arg<string>()} ({ci.Arg<Exception>()?.GetType().Name})"));
+
+            var appManager = new DefaultAppManager(
+                Substitute.For<IAppManifest>(),
+                this.GetAmbientServices(),
+                this.GetServiceBehaviorProvider(),
+                new List<IExportFactory<IAppLifecycleBehavior, AppServiceMetadata>>(),
+                new[] { factory1, factory2, factory3 },
+                null);
+
+            appManager.Logger = logger;
+            await appManager.InitializeAppAsync(Substitute.For<IAppContext>(), CancellationToken.None);
+
+            Assert.AreEqual(2, order.Count);
+            Assert.AreEqual(3, order[0]);
+            Assert.AreEqual(1, order[1]);
+
+            var log = sb.ToString();
+            Assert.IsTrue(log.Contains("Error: '2'"));
+            Assert.IsTrue(log.Contains("(InvalidOperationException)"));
+        }
+
+
+        [Test]
+        public async Task InitializeAppAsync_exception_failed_required_feature()
+        {
+            var order = new List<int>();
+
+            var featureManager1 = this.CreateFeatureManager(initialization: ci => throw new InvalidOperationException());
+
+            var factory1 = this.CreateFeatureManagerFactory(featureManager1, "1", "1.0", isRequired: true);
+
+            var sb = new StringBuilder();
+            var logger = Substitute.For<ILogger<IAppManager>>();
+            logger
+                .WhenForAnyArgs(l => l.Log(LogLevel.Debug, null, null, null))
+                .Do(ci => sb.AppendLine($"{ci.Arg<LogLevel>()}: {ci.Arg<string>()} ({ci.Arg<Exception>()?.GetType().Name})"));
+
+            var appManager = new DefaultAppManager(
+                Substitute.For<IAppManifest>(),
+                this.GetAmbientServices(),
+                this.GetServiceBehaviorProvider(),
+                new List<IExportFactory<IAppLifecycleBehavior, AppServiceMetadata>>(),
+                new[] { factory1 },
+                null);
+
+            appManager.Logger = logger;
+            Assert.That(() => appManager.InitializeAppAsync(Substitute.For<IAppContext>(), CancellationToken.None), Throws.InvalidOperationException);
+
+            var log = sb.ToString();
+            Assert.IsTrue(log.Contains("Error: '1'"));
+            Assert.IsTrue(log.Contains("(InvalidOperationException)"));
+            Assert.IsTrue(log.Contains("Error: The application's initialize procedure encountered an exception"));
         }
 
         [Test]
