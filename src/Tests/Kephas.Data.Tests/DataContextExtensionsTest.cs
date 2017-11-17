b54e7862a5c3b619e119f899a00809c8a5560544
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-11-17 12:03:26 +0200

    Added more unit tests for DataContextExtensions

--- src/Tests/Kephas.Data.Tests/DataContextExtensionsTest.cs
+++ src/Tests/Kephas.Data.Tests/DataContextExtensionsTest.cs
@@ -10,9 +10,12 @@
 namespace Kephas.Data.Tests
 {
     using System;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     using Kephas.Data.Caching;
     using Kephas.Data.Capabilities;
+    using Kephas.Data.Commands;
 
     using NSubstitute;
 
@@ -30,7 +33,7 @@ namespace Kephas.Data.Tests
             var entity = "hello";
             var entityInfo = dataContext.AttachEntity(entity);
 
-            var detachedEntityInfo = dataContext.DetachEntity(entity);
+            var detachedEntityInfo = DataContextExtensions.DetachEntity(dataContext, entity);
             Assert.AreSame(entityInfo, detachedEntityInfo);
             Assert.AreEqual(0, localCache.Count);
         }
@@ -43,7 +46,7 @@ namespace Kephas.Data.Tests
 
             var entity = "hello";
 
-            var detachedEntityInfo = dataContext.DetachEntity(entity);
+            var detachedEntityInfo = DataContextExtensions.DetachEntity(dataContext, entity);
             Assert.IsNull(detachedEntityInfo);
         }
 
@@ -58,9 +61,72 @@ namespace Kephas.Data.Tests
             entity.GetStructuralEntityGraph().Returns(new[] { entity, entityPart });
             var entityInfo = dataContext.AttachEntity(entity);
 
-            var detachedEntityInfo = dataContext.DetachEntity(entity);
+            var detachedEntityInfo = DataContextExtensions.DetachEntity(dataContext, entity);
             Assert.AreSame(entityInfo, detachedEntityInfo);
             Assert.AreEqual(0, localCache.Count);
+        }
+
+        [Test]
+        public void CreateCommand()
+        {
+            var dataContext = Substitute.For<IDataContext>();
+            var command = Substitute.For<IExecuteCommand>();
+            dataContext.CreateCommand(typeof(IExecuteCommand)).Returns(command);
+            var actualCommand = DataContextExtensions.CreateCommand<IExecuteCommand>(dataContext);
+            Assert.AreSame(command, actualCommand);
+        }
+
+        [Test]
+        public async Task ExecuteAsync_commandText()
+        {
+            var dataContext = Substitute.For<IDataContext>();
+            var command = Substitute.For<IExecuteCommand>();
+            command.ExecuteAsync(Arg.Any<IExecuteContext>(), Arg.Any<CancellationToken>()).Returns(
+                ci =>
+                    {
+                        var res = Substitute.For<IExecuteResult>();
+                        res.Result.Returns(ci.Arg<IExecuteContext>().CommandText + " executed");
+                        return Task.FromResult(res);
+                    });
+            dataContext.CreateCommand(typeof(IExecuteCommand)).Returns(command);
+            var result = await DataContextExtensions.ExecuteAsync(dataContext, "do this");
+            Assert.AreEqual("do this executed", result);
+        }
+
+        [Test]
+        public async Task ExecuteAsync_commandContext()
+        {
+            var dataContext = Substitute.For<IDataContext>();
+            var command = Substitute.For<IExecuteCommand>();
+            command.ExecuteAsync(Arg.Any<IExecuteContext>(), Arg.Any<CancellationToken>()).Returns(
+                ci =>
+                    {
+                        var res = Substitute.For<IExecuteResult>();
+                        res.Result.Returns(ci.Arg<IExecuteContext>().CommandText + " executed");
+                        return Task.FromResult(res);
+                    });
+            dataContext.CreateCommand(typeof(IExecuteCommand)).Returns(command);
+            var commandContext = new ExecuteContext(dataContext) { CommandText = "do this" };
+            var result = await DataContextExtensions.ExecuteAsync(dataContext, commandContext);
+            Assert.AreEqual("do this executed", result);
+        }
+
+        [Test]
+        public async Task ExecuteAsync_commandContext_mismatched_data_context()
+        {
+            var dataContext = Substitute.For<IDataContext>();
+            var command = Substitute.For<IExecuteCommand>();
+            command.ExecuteAsync(Arg.Any<IExecuteContext>(), Arg.Any<CancellationToken>()).Returns(
+                ci =>
+                    {
+                        var res = Substitute.For<IExecuteResult>();
+                        res.Result.Returns(ci.Arg<IExecuteContext>().CommandText + " executed");
+                        return Task.FromResult(res);
+                    });
+            dataContext.CreateCommand(typeof(IExecuteCommand)).Returns(command);
+            var commandContext = new ExecuteContext(Substitute.For<IDataContext>());
+
+            Assert.ThrowsAsync<DataException>(() => DataContextExtensions.ExecuteAsync(dataContext, commandContext));
         }
     }
 }
