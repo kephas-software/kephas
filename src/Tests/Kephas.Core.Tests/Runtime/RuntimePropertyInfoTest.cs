6b9aa6b93c7570bcb2c4a1b14409a9e56757f84e
Ioan Crisan <ioan.crisan@quartz-software.com>
2017-11-25 23:44:50 +0200

    RuntimePropertyInfo tests

---
+++ src/Tests/Kephas.Core.Tests/Runtime/RuntimePropertyInfoTest.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RuntimePropertyInfoTest.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the runtime property information test class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Core.Tests.Runtime
+{
+    using Kephas.Runtime;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RuntimePropertyInfoTest
+    {
+        [Test]
+        public void ToString()
+        {
+            var propInfo = new RuntimePropertyInfo<string, int>(typeof(string).GetProperty("Length"));
+
+            var toString = propInfo.ToString();
+            Assert.AreEqual("Length: System.Int32", toString);
+        }
+    }
+}
