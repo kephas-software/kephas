a1614f619337a72155d6f93ca9c53370a747604f
Ioan Crisan <ioan.crisan@quartz-software.com>
2018-02-16 15:14:35 +0200

    Added the LazyExpando variation to allow lazy value evaluation/providing.

---
+++ src/Kephas.Core/Reflection/CircularDependencyException.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CircularDependencyException.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the circular dependency exception class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Reflection
+{
+    using System;
+
+    /// <summary>
+    /// Exception for signalling circular dependency errors.
+    /// </summary>
+    public class CircularDependencyException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CircularDependencyException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public CircularDependencyException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CircularDependencyException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner.</param>
+        public CircularDependencyException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ src/Tests/Kephas.Core.Tests/Dynamic/LazyExpandoTest.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LazyExpandoTest.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the lazy expando test class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Core.Tests.Dynamic
+{
+    using System;
+
+    using Kephas.Dynamic;
+    using Kephas.Reflection;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LazyExpandoTest
+    {
+        [Test]
+        public void Indexer_get()
+        {
+            var expando = new LazyExpando(s => s);
+            Assert.AreEqual("hello", expando["hello"]);
+        }
+
+        [Test]
+        public void Indexer_get_no_value_resolver()
+        {
+            var expando = new LazyExpando();
+            Assert.IsNull(expando["hello"]);
+        }
+
+        [Test]
+        public void Indexer_circular_reference_exception()
+        {
+            var expando = new CircularExpando();
+            expando.ValueResolver = s => s == "a" ? expando["b"] :
+                                         s == "b" ? expando["a"] : null;
+
+            Assert.Throws<CircularDependencyException>(() =>
+                {
+                    var x = expando["a"];
+                });
+        }
+
+        [Test]
+        public void Indexer_circular_reference_exception_ensure_not_locked()
+        {
+            var expando = new CircularExpando();
+            expando.ValueResolver = s => s == "a" ? expando["b"] :
+                                         s == "b" ? expando["a"] : null;
+
+            Assert.Throws<CircularDependencyException>(() =>
+                {
+                    var x = expando["a"];
+                });
+
+            expando.ValueResolver = s => "hello";
+            Assert.AreEqual("hello", expando["a"]);
+        }
+
+        public class CircularExpando : LazyExpando
+        {
+            public new Func<string, object> ValueResolver
+            {
+                get => base.ValueResolver;
+                set => base.ValueResolver = value;
+            }
+        }
+    }
+}
