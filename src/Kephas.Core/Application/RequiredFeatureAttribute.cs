a1aba160a2167854b4d9b65d1f26f2d0a5dba192
Ioan Crisan <ioan.crisan@quartz-software.com>
2018-03-26 18:10:02 +0300

    Added the RequiredFeature attribute to Core

---
+++ src/Kephas.Core/Application/RequiredFeatureAttribute.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RequiredFeatureAttribute.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the require feature attribute class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Application
+{
+    using System;
+
+    using Kephas.Composition.Metadata;
+
+    /// <summary>
+    /// Attribute declaring a required feature for the annotated entity.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class RequiredFeatureAttribute : Attribute, IMetadataValue<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequiredFeatureAttribute"/> class.
+        /// </summary>
+        /// <param name="value">The name of the required feature.</param>
+        public RequiredFeatureAttribute(string value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the metadata value for Kephas.
+        /// </summary>
+        object IMetadataValue.Value => this.Value;
+
+        /// <summary>
+        /// Gets the name of the required feature.
+        /// </summary>
+        public string Value { get; }
+    }
+}
