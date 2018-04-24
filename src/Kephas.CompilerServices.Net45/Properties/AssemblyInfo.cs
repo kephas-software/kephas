84f689537bdd8dc17b6c2190567f069976bf86f6
Ioan Crisan <ioan.crisan@quartz-software.com>
2018-04-24 18:04:16 +0300

    Added Kephas.CompilerServices skeleton

---
+++ src/Kephas.CompilerServices.Net45/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the assembly information class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kephas.CompilerServices")]
+[assembly: AssemblyDescription("Provides a common infrastructure for code compilation and interpretation.")]
