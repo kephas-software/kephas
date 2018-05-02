03ad8c5351165d59de96727e719709a68b50078d
Ioan Crisan <ioan.crisan@quartz-software.com>
2018-05-03 02:34:29 +0300

    First steps in Autofac support

---
+++ src/Kephas.Composition.Autofac/AutofacAmbientServicesBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AutofacAmbientServicesBuilderExtensions.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the autofac ambient services builder extensions class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas
+{
+    using System;
+
+    using Kephas.Composition.Autofac.Hosting;
+    using Kephas.Composition.Hosting;
+    using Kephas.Diagnostics.Contracts;
+
+    /// <summary>
+    /// Autofac related ambient services builder extensions.
+    /// </summary>
+    public static class AutofacAmbientServicesBuilderExtensions
+    {
+        /// <summary>
+        /// Sets the composition container to the ambient services.
+        /// </summary>
+        /// <param name="ambientServicesBuilder">The ambient services builder.</param>
+        /// <param name="containerBuilderConfig">The container builder configuration.</param>
+        /// <returns>The provided ambient services builder.</returns>
+        public static AmbientServicesBuilder WithAutofacCompositionContainer(this AmbientServicesBuilder ambientServicesBuilder, Action<AutofacCompositionContainerBuilder> containerBuilderConfig = null)
+        {
+            Requires.NotNull(ambientServicesBuilder, nameof(ambientServicesBuilder));
+
+            var containerBuilder = new AutofacCompositionContainerBuilder(new CompositionRegistrationContext(ambientServicesBuilder.AmbientServices));
+
+            containerBuilderConfig?.Invoke(containerBuilder);
+
+            var container = containerBuilder.CreateContainer();
+            return ambientServicesBuilder.WithCompositionContainer(container);
+        }
+    }
+}
---
+++ src/Kephas.Composition.Autofac/Composition/Autofac/Hosting/AutofacCompositionContainerBuilder.cs
@@ -0,0 +1,86 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AutofacCompositionContainerBuilder.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the autofac composition container builder class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Composition.Autofac.Hosting
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Kephas.Composition.Conventions;
+    using Kephas.Composition.Hosting;
+
+    /// <summary>
+    /// An Autofac composition container builder.
+    /// </summary>
+    public class AutofacCompositionContainerBuilder : CompositionContainerBuilderBase<AutofacCompositionContainerBuilder>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutofacCompositionContainerBuilder"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public AutofacCompositionContainerBuilder(ICompositionRegistrationContext context)
+            : base(context)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new factory export provider.
+        /// </summary>
+        /// <typeparam name="TContract">The type of the contract.</typeparam>
+        /// <param name="factory">The factory.</param>
+        /// <param name="isShared">If set to <c>true</c>, the factory returns a shared component, otherwise an instance component.</param>
+        /// <returns>
+        /// The export provider.
+        /// </returns>
+        protected override IExportProvider CreateFactoryExportProvider<TContract>(Func<TContract> factory, bool isShared = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a new export provider based on a <see cref="IServiceProvider"/>.
+        /// </summary>
+        /// <remarks>
+        /// This allows for the services registered in the <see cref="IAmbientServices"/> before the composition container was created
+        /// to be registered also in the composition container.
+        /// </remarks>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <param name="isServiceRegisteredFunc">Function used to query whether the service provider registers a specific service.</param>
+        /// <returns>
+        /// The export provider.
+        /// </returns>
+        protected override IExportProvider CreateServiceProviderExportProvider(IServiceProvider serviceProvider, Func<IServiceProvider, Type, bool> isServiceRegisteredFunc)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Factory method for creating the conventions builder.
+        /// </summary>
+        /// <returns>A newly created conventions builder.</returns>
+        protected override IConventionsBuilder CreateConventionsBuilder()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a new composition container based on the provided conventions and assembly parts.
+        /// </summary>
+        /// <param name="conventions">The conventions.</param>
+        /// <param name="parts">The parts candidating for composition.</param>
+        /// <returns>
+        /// A new composition container.
+        /// </returns>
+        protected override ICompositionContext CreateContainerCore(IConventionsBuilder conventions, IEnumerable<Type> parts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
