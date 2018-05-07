73063bb660273eca9b340f39467640945fe9a63f
Ioan Crisan <ioan.crisan@quartz-software.com>
2018-05-07 17:51:11 +0300

    Added workflow execution behavior contracts

---
+++ src/Kephas.Workflow/Behaviors/Composition/WorkflowExecutionBehaviorMetadata.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WorkflowExecutionBehaviorMetadata.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the workflow execution behavior metadata class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Workflow.Behaviors.Composition
+{
+    using System.Collections.Generic;
+    using Kephas.Services.Composition;
+
+    /// <summary>
+    /// A workflow execution behavior metadata.
+    /// </summary>
+    public class WorkflowExecutionBehaviorMetadata : AppServiceMetadata
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkflowExecutionBehaviorMetadata"/> class.
+        /// </summary>
+        /// <param name="metadata">The metadata.</param>
+        public WorkflowExecutionBehaviorMetadata(IDictionary<string, object> metadata)
+            : base(metadata)
+        {
+            if (metadata == null)
+            {
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkflowExecutionBehaviorMetadata"/> class.
+        /// </summary>
+        /// <param name="processingPriority">The processing priority (optional).</param>
+        /// <param name="overridePriority">The override priority (optional).</param>
+        /// <param name="optionalService"><c>true</c> if the service is optional, <c>false</c> if not (optional).</param>
+        /// <param name="serviceName">The name of the service (optional).</param>
+        public WorkflowExecutionBehaviorMetadata(int processingPriority = 0, int overridePriority = 0, bool optionalService = false, string serviceName = null)
+            : base(processingPriority, overridePriority, optionalService, serviceName)
+        {
+        }
+    }
+}
---
+++ src/Kephas.Workflow/Behaviors/IWorkflowExecutionBehavior.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWorkflowExecutionBehavior.cs" company="Quartz Software SRL">
+//   Copyright (c) Quartz Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Declares the IWorkflowExecutionBehavior interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Workflow.Behaviors
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Services;
+
+    /// <summary>
+    /// Interface for workflow execution behavior.
+    /// </summary>
+    [SharedAppServiceContract(AllowMultiple = true)]
+    public interface IWorkflowExecutionBehavior
+    {
+        /// <summary>
+        /// Interception called before invoking the service to execute the activity.
+        /// </summary>
+        /// <param name="context">The execution context.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>The asynchronous result.</returns>
+        Task BeforeExecuteAsync(IWorkflowExecutionContext context, CancellationToken token);
+
+        /// <summary>
+        /// Interception called after invoking the service to execute the activity.
+        /// </summary>
+        /// <param name="context">The execution context.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>The asynchronous result.</returns>
+        /// <remarks>
+        /// The context will contain the response returned by the service.
+        /// The interceptor may change the response or even replace it with another one.
+        /// </remarks>
+        Task AfterExecuteAsync(IWorkflowExecutionContext context, CancellationToken token);
+    }
+}
