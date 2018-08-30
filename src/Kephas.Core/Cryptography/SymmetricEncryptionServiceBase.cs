c9f0e270d7c443c319eaed8c608c316c958d520d
Ioan Crisan <ioan.crisan@quartz-software.com>
2018-08-30 18:06:44 +0300

    Fix a bug in the SymmetricEncryptionServiceBase

--- src/Kephas.Core/Cryptography/SymmetricEncryptionServiceBase.cs
+++ src/Kephas.Core/Cryptography/SymmetricEncryptionServiceBase.cs
@@ -237,7 +237,7 @@ namespace Kephas.Cryptography
         /// </returns>
         protected virtual byte[] GetKey(IEncryptionContext encryptionContext)
         {
-            return encryptionContext.Key;
+            return encryptionContext?.Key;
         }
 
         /// <summary>
