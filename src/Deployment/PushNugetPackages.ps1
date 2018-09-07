04b03e5eead0b71ca3635da73c8e4da6805627d0
Ioan Crisan <ioan.crisan@quartz-software.com>
2018-09-08 00:42:23 +0300

    Fixed the package push script to nuget.org

--- src/Deployment/PushNugetPackages.ps1
+++ src/Deployment/PushNugetPackages.ps1
@@ -35,4 +35,6 @@ $packages = @(
 
 foreach ($package in $packages) {
     .\NuGet.exe push "$package.$version.nupkg" -Source https://nuget.smbsrc.net
+    .\NuGet.exe push "$package.$version.nupkg" -Source https://api.nuget.org/v3/index.json 
+    .\NuGet.exe push "$package.$version.symbols.nupkg" -Source https://nuget.smbsrc.net
 }
