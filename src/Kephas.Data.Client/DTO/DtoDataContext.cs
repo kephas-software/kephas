ae420fe7d4a017fc33cd7c6c884c05e2213e9a63
Ioan Crisan <ioan.crisan@quartz-software.com>
2018-10-28 02:47:26 +0300

    Added the DtoDataContext to the Data.Client - ignores all data behaviors

---
+++ src/Kephas.Data.Client/DTO/DtoDataContext.cs
@@ -0,0 +1,72 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DtoDataContext.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the dto data context class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Client.DTO
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+
+    using Kephas.Composition;
+    using Kephas.Composition.AttributedModel;
+    using Kephas.Data.Behaviors;
+    using Kephas.Data.Commands.Factory;
+    using Kephas.Data.InMemory;
+    using Kephas.Data.Store;
+    using Kephas.Serialization;
+
+    /// <summary>
+    /// A data context for DTOs.
+    /// </summary>
+    [SupportedDataStoreKinds(DataStoreKind.DTO)]
+    public class DtoDataContext : InMemoryDataContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DtoDataContext"/> class.
+        /// </summary>
+        /// <param name="compositionContext">Context for the composition.</param>
+        /// <param name="dataCommandProvider">The data command provider.</param>
+        /// <param name="serializationService">The serialization service.</param>
+        public DtoDataContext(
+            ICompositionContext compositionContext,
+            IDataCommandProvider dataCommandProvider,
+            ISerializationService serializationService)
+            : base(compositionContext, dataCommandProvider, new NoneDataBehaviorProvider(), serializationService)
+        {
+        }
+
+        /// <summary>
+        /// A none data behavior provider.
+        /// </summary>
+        [ExcludeFromComposition]
+        internal class NoneDataBehaviorProvider : IDataBehaviorProvider
+        {
+            /// <summary>
+            /// The empty behaviors.
+            /// </summary>
+            private static readonly ConcurrentDictionary<Type, IEnumerable> EmptyBehaviors = new ConcurrentDictionary<Type, IEnumerable>();
+
+            /// <summary>
+            /// Gets the data behaviors of type <typeparamref name="TBehavior" /> for the provided type.
+            /// </summary>
+            /// <typeparam name="TBehavior">Type of the behavior.</typeparam>
+            /// <param name="type">The type.</param>
+            /// <returns>
+            /// An enumeration of behaviors matching the provided type.
+            /// </returns>
+            public IEnumerable<TBehavior> GetDataBehaviors<TBehavior>(Type type)
+            {
+                var behaviors = EmptyBehaviors.GetOrAdd(typeof(TBehavior), t => new TBehavior[0]);
+                return (IEnumerable<TBehavior>)behaviors;
+            }
+        }
+    }
+}
---
+++ src/Kephas.Data.Client/DTO/DtoPersistChangesCommand.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DtoPersistChangesCommand.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements a persist changes command for DTOs.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Client.DTO
+{
+    using Kephas.Data.Commands;
+
+    /// <summary>
+    /// A persist changes command for DTOs.
+    /// </summary>
+    [DataContextType(typeof(DtoDataContext))]
+    public class DtoPersistChangesCommand : PersistChangesCommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DtoPersistChangesCommand"/> class.
+        /// </summary>
+        /// <remarks>
+        /// The DTO entities do not need behaviors for persistence.
+        /// </remarks>
+        public DtoPersistChangesCommand()
+            : base(new DtoDataContext.NoneDataBehaviorProvider())
+        {
+        }
+    }
+}
