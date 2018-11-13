fc967639c38de5726fd5e5f03a97599101e2bb55
Ioan Crisan <ioan.crisan@quartz-software.com>
2018-11-14 00:08:30 +0200

    Fixed the RequiredRef attribute & test projects

--- src/Kephas.Data/AttributedModel/RequiredRefAttribute.cs
+++ src/Kephas.Data/AttributedModel/RequiredRefAttribute.cs
@@ -29,31 +29,10 @@ namespace Kephas.Data.AttributedModel
         {
         }
 
-        /// <summary>
-        /// Validates the specified value with respect to the current validation attribute.
-        /// </summary>
-        /// <param name="value">The value to validate.</param>
-        /// <param name="validationContext">The context information about the validation operation.</param>
-        /// <returns>
-        /// An instance of the <see cref="T:System.ComponentModel.DataAnnotations.ValidationResult" />
-        /// class.
-        /// </returns>
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (!this.IsEmptyRef(value))
-            {
-                return ValidationResult.Success;
-            }
-
-            var memberNames = validationContext.MemberName != null ? new[] { validationContext.MemberName } : null;
-            return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName), memberNames);
-        }
-
-        /// <summary>Checks that the value of the required data field is not empty.</summary>
-        /// <returns>true if validation is successful; otherwise, false.</returns>
-        /// <param name="value">The data field value to validate.</param>
-        /// <exception cref="T:System.ComponentModel.DataAnnotations.ValidationException">The data field value was null.</exception>
-        protected virtual bool IsEmptyRef(object value)
+        /// <summary>Determines whether the specified value of the object is valid. </summary>
+        /// <returns>true if the specified value is valid; otherwise, false.</returns>
+        /// <param name="value">The value of the object to validate. </param>
+        public override bool IsValid(object value)
         {
             if (value is IRef refValue)
             {
