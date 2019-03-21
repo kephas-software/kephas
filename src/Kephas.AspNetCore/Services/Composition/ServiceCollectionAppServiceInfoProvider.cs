015e858431d61f2a2bbbcd7d21192b96cbb10ddc
Ioan Crisan <ioan.crisan@quartz-software.com>
2019-03-21 11:29:02 +0200

    Renamed ServiceCollectionConvetionsRegistrar to AppServiceInfoProvider

--- src/Kephas.AspNetCore/Services/Composition/ServiceCollectionAppServiceInfoProvider.cs
+++ src/Kephas.AspNetCore/Services/Composition/ServiceCollectionAppServiceInfoProvider.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ServiceCollectionConventionsRegistrar.cs" company="Kephas Software SRL">
+// <copyright file="ServiceCollectionAppServiceInfoProvider.cs" company="Kephas Software SRL">
 //   Copyright (c) Kephas Software SRL. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -26,52 +26,8 @@ namespace Kephas.AspNetCore.Services.Composition
     /// <summary>
     /// A conventions registrar for a collection of service definitions.
     /// </summary>
-    public class ServiceCollectionConventionsRegistrar : IAppServiceInfoProvider
+    public class ServiceCollectionAppServiceInfoProvider : IAppServiceInfoProvider
     {
-        /// <summary>
-        /// Registers the conventions.
-        /// </summary>
-        /// <param name="builder">The registration builder.</param>
-        /// <param name="candidateTypes">The candidate types which can take part in the composition.</param>
-        /// <param name="registrationContext">Context for the registration.</param>
-        public void RegisterConventions(
-            IConventionsBuilder builder,
-            IEnumerable<TypeInfo> candidateTypes,
-            ICompositionRegistrationContext registrationContext)
-        {
-            var ambientServices = registrationContext.AmbientServices;
-            var serviceCollection = ambientServices.GetService<IServiceCollection>();
-
-            foreach (var descriptor in serviceCollection)
-            {
-                if (descriptor.ImplementationInstance != null)
-                {
-                    builder.ForInstance(descriptor.ServiceType, descriptor.ImplementationInstance);
-                }
-                else if (descriptor.ImplementationFactory != null)
-                {
-                    builder.ForInstanceFactory(descriptor.ServiceType, ctx => descriptor.ImplementationFactory((ctx ?? ambientServices.CompositionContainer).ToServiceProvider()));
-                }
-                else
-                {
-                    var partBuilder = descriptor.ImplementationType != null
-                                          ? builder.ForType(descriptor.ImplementationType)
-                                          : builder.ForType(descriptor.ServiceType);
-
-                    if (descriptor.Lifetime == ServiceLifetime.Singleton)
-                    {
-                        partBuilder.Shared();
-                    }
-                    else if (descriptor.Lifetime == ServiceLifetime.Scoped)
-                    {
-                        partBuilder.ScopeShared();
-                    }
-
-                    partBuilder.Export(b => b.AsContractType(descriptor.ServiceType));
-                }
-            }
-        }
-
         /// <summary>
         /// Gets an enumeration of application service information objects.
         /// </summary>
