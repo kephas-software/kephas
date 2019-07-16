bd3a0840a999d8e38631231126314d7fa74f0a51
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-07-16 20:05:27 +0300

    Added In, Out, and ReturnType attributes to Runtime to be used in Workflow

---
+++ src/Kephas.Core/Runtime/AttributedModel/InAttribute.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InAttribute.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the in attribute class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Runtime.AttributedModel
+{
+    using System;
+
+    /// <summary>
+    /// Attribute for input parameters.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = false)]
+    public class InAttribute : Attribute
+    {
+    }
+}
---
+++ src/Kephas.Core/Runtime/AttributedModel/OutAttribute.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OutAttribute.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the out attribute class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Runtime.AttributedModel
+{
+    using System;
+
+    /// <summary>
+    /// Attribute for output parameters.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = false)]
+    public class OutAttribute : Attribute
+    {
+    }
+}
