7b6c7de8e7044f8b9ae2bdf5bcbcbcb89a4ef949
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-08-17 00:15:30 +0300

    Add support for Options service registrations

---
+++ src/Kephas.AspNetCore/Options/OptionsFactory.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OptionsFactory.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the options factory class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.AspNetCore.Options
+{
+    using System.Collections.Generic;
+
+    using Microsoft.Extensions.Options;
+
+    /// <summary>
+    /// The options factory.
+    /// </summary>
+    /// <typeparam name="T">The options type.</typeparam>
+    public class OptionsFactory<T> : Microsoft.Extensions.Options.OptionsFactory<T>
+        where T : class, new()
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OptionsFactory{T}"/> class.
+        /// </summary>
+        public OptionsFactory()
+            : base(
+                AmbientServices.Instance.CompositionContainer.GetExports<IConfigureOptions<T>>(),
+                AmbientServices.Instance.CompositionContainer.GetExports<IPostConfigureOptions<T>>())
+        {
+            // TODO until the https://github.com/dotnet/corefx/issues/40094 bug is fixed, make this constructor parameterless.
+            // TODO remove this class when the bug is corrected.
+        }
+    }
+}
---
+++ src/Kephas.AspNetCore/Options/OptionsSnapshot.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OptionsSnapshot.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the options snapshot class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.AspNetCore.Options
+{
+    using Microsoft.Extensions.Options;
+
+    /// <summary>
+    /// The options snapshot.
+    /// </summary>
+    /// <typeparam name="T">The options type.</typeparam>
+    public class OptionsSnapshot<T> : OptionsManager<T>
+        where T : class, new()
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OptionsSnapshot{T}"/> class.
+        /// </summary>
+        public OptionsSnapshot()
+            : base(AmbientServices.Instance.CompositionContainer.GetExport<IOptionsFactory<T>>())
+        {
+            // TODO until the https://github.com/dotnet/corefx/issues/40094 bug is fixed, make this constructor parameterless.
+            // Options and OptionsSnapshot main purpose is to split the registration of the OptionsManager with two contracts,
+            // as MEF does not like this scenario. For some reason it fails with SharingBoundary duplicate metadata registration.
+        }
+    }
+}
