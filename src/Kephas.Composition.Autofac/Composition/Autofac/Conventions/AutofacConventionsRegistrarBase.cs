36c2aabaaf61764897a613ac926aa645492bc80f
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-08-20 00:34:53 +0300

    Added AutofacConventionsRegistrarBase

--- src/Kephas.Composition.Autofac/Composition/Autofac/Conventions/AutofacConventionsRegistrarBase.cs
+++ src/Kephas.Composition.Autofac/Composition/Autofac/Conventions/AutofacConventionsRegistrarBase.cs
@@ -1,33 +1,34 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="MefConventionsRegistrarBase.cs" company="Kephas Software SRL">
+// <copyright file="AutofacConventionsRegistrarBase.cs" company="Kephas Software SRL">
 //   Copyright (c) Kephas Software SRL. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 // <summary>
-//   Base class for MEF conventions registrars.
+//   Implements the Autofac conventions registrar base class.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace Kephas.Composition.Mef.Conventions
+namespace Kephas.Composition.Autofac.Conventions
 {
     using System;
     using System.Collections.Generic;
-    using System.Composition.Convention;
 
+    using global::Autofac;
+
+    using Kephas.Composition.Autofac.Resources;
     using Kephas.Composition.Conventions;
     using Kephas.Composition.Hosting;
-    using Kephas.Composition.Mef.Resources;
     using Kephas.Diagnostics.Contracts;
 
     /// <summary>
-    /// Base class for MEF conventions registrars.
+    /// An Autofac conventions registrar base.
     /// </summary>
-    public abstract class MefConventionsRegistrarBase : IConventionsRegistrar
+    public abstract class AutofacConventionsRegistrarBase : IConventionsRegistrar
     {
         /// <summary>
         /// Registers the conventions.
         /// </summary>
-        /// <exception cref="InvalidOperationException">Thrown when the provided conventions are not MEF conventions.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when the requested operation is invalid.</exception>
         /// <param name="builder">The registration builder.</param>
         /// <param name="candidateTypes">The candidate types which can take part in the composition.</param>
         /// <param name="registrationContext">Context for the registration.</param>
@@ -40,22 +41,22 @@ namespace Kephas.Composition.Mef.Conventions
             Requires.NotNull(candidateTypes, nameof(candidateTypes));
             Requires.NotNull(registrationContext, nameof(registrationContext));
 
-            if (!(builder is IMefConventionBuilderProvider mefBuilder))
+            if (!(builder is IAutofacContainerBuilderProvider autofacBuilder))
             {
-                throw new InvalidOperationException(string.Format(Strings.InvalidConventions, typeof(IMefConventionBuilderProvider)));
+                throw new InvalidOperationException(string.Format(Strings.InvalidConventions, typeof(IAutofacContainerBuilderProvider)));
             }
 
-            this.RegisterConventions(mefBuilder.GetConventionBuilder(), candidateTypes, registrationContext);
+            this.RegisterConventions(autofacBuilder.GetContainerBuilder(), candidateTypes, registrationContext);
         }
 
         /// <summary>
         /// Registers the conventions.
         /// </summary>
-        /// <param name="builder">The registration builder.</param>
+        /// <param name="builder">The container builder.</param>
         /// <param name="candidateTypes">The candidate types which can take part in the composition.</param>
         /// <param name="registrationContext">Context for the registration.</param>
         protected abstract void RegisterConventions(
-            ConventionBuilder builder,
+            ContainerBuilder builder,
             IList<Type> candidateTypes,
             ICompositionRegistrationContext registrationContext);
     }
--- src/Kephas.Composition.Medi/Composition/Medi/Conventions/MediConventionsRegistrarBase.cs
+++ src/Kephas.Composition.Medi/Composition/Medi/Conventions/MediConventionsRegistrarBase.cs
@@ -1,33 +1,33 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="MefConventionsRegistrarBase.cs" company="Kephas Software SRL">
+// <copyright file="MediConventionsRegistrarBase.cs" company="Kephas Software SRL">
 //   Copyright (c) Kephas Software SRL. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 // <summary>
-//   Base class for MEF conventions registrars.
+//   Implements the medi conventions registrar base class.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace Kephas.Composition.Mef.Conventions
+namespace Kephas.Composition.Medi.Conventions
 {
     using System;
     using System.Collections.Generic;
-    using System.Composition.Convention;
 
     using Kephas.Composition.Conventions;
     using Kephas.Composition.Hosting;
-    using Kephas.Composition.Mef.Resources;
+    using Kephas.Composition.Medi.Resources;
     using Kephas.Diagnostics.Contracts;
 
+    using Microsoft.Extensions.DependencyInjection;
+
     /// <summary>
-    /// Base class for MEF conventions registrars.
+    /// A Microsoft.Extensions.DependencyInjection conventions registrar base.
     /// </summary>
-    public abstract class MefConventionsRegistrarBase : IConventionsRegistrar
+    public abstract class MediConventionsRegistrarBase : IConventionsRegistrar
     {
         /// <summary>
         /// Registers the conventions.
         /// </summary>
-        /// <exception cref="InvalidOperationException">Thrown when the provided conventions are not MEF conventions.</exception>
         /// <param name="builder">The registration builder.</param>
         /// <param name="candidateTypes">The candidate types which can take part in the composition.</param>
         /// <param name="registrationContext">Context for the registration.</param>
@@ -40,22 +40,22 @@ namespace Kephas.Composition.Mef.Conventions
             Requires.NotNull(candidateTypes, nameof(candidateTypes));
             Requires.NotNull(registrationContext, nameof(registrationContext));
 
-            if (!(builder is IMefConventionBuilderProvider mefBuilder))
+            if (!(builder is IMediServiceCollectionProvider mediBuilder))
             {
-                throw new InvalidOperationException(string.Format(Strings.InvalidConventions, typeof(IMefConventionBuilderProvider)));
+                throw new InvalidOperationException(string.Format(Strings.InvalidConventions, typeof(IMediServiceCollectionProvider)));
             }
 
-            this.RegisterConventions(mefBuilder.GetConventionBuilder(), candidateTypes, registrationContext);
+            this.RegisterConventions(mediBuilder.GetServiceCollection(), candidateTypes, registrationContext);
         }
 
         /// <summary>
         /// Registers the conventions.
         /// </summary>
-        /// <param name="builder">The registration builder.</param>
+        /// <param name="serviceCollection">The service collection.</param>
         /// <param name="candidateTypes">The candidate types which can take part in the composition.</param>
         /// <param name="registrationContext">Context for the registration.</param>
         protected abstract void RegisterConventions(
-            ConventionBuilder builder,
+            IServiceCollection serviceCollection,
             IList<Type> candidateTypes,
             ICompositionRegistrationContext registrationContext);
     }
