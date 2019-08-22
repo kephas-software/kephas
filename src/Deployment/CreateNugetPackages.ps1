1c0e98e4ee6ebfaa35919ccc402a483a63afb10d
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-08-22 19:45:09 +0300

    Fix typo

--- src/Deployment/CreateNugetPackages.ps1
+++ src/Deployment/CreateNugetPackages.ps1
@@ -8,7 +8,7 @@
 
 # The symbol source is changed since Nov. 2018
 # https://blog.nuget.org/20181116/Improved-debugging-experience-with-the-NuGet-org-symbol-server-and-snupkg.html
-# however, a bug is still present - until it is fixed stay with the ols symbols
+# however, a bug is still present - until it is fixed stay with the old symbols
 
 
 function get-packagename([string]$pathname) {
