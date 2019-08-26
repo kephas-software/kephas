778a4c648298c2d7a914127e1d01b6abdd0dc36d
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-08-26 10:58:10 +0300

    Added an aggregated service provider to the DependencyInjection adapter.

---
+++ src/Kephas.Composition.DependencyInjection/Composition/DependencyInjection/AggregatedServiceProvider.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AggregatedServiceProvider.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the aggregated service provider class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Composition.DependencyInjection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Kephas.Diagnostics.Contracts;
+
+    /// <summary>
+    /// An aggregated service provider.
+    /// </summary>
+    public class AggregatedServiceProvider : IServiceProvider, IDisposable
+    {
+        private readonly IList<IServiceProvider> innerServiceProviders;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregatedServiceProvider"/> class.
+        /// </summary>
+        /// <param name="innerServiceProviders">A variable-length parameters list containing inner
+        ///                                     service providers.</param>
+        public AggregatedServiceProvider(params IServiceProvider[] innerServiceProviders)
+        {
+            Requires.NotNullOrEmpty(innerServiceProviders, nameof(innerServiceProviders));
+
+            this.innerServiceProviders = innerServiceProviders.ToList();
+        }
+
+        /// <summary>
+        /// Gets the service object of the specified type.
+        /// </summary>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <returns>
+        /// A service object of type <paramref name="serviceType">serviceType</paramref>.   -or-  null if
+        /// there is no service object of type <paramref name="serviceType">serviceType</paramref>.
+        /// </returns>
+        public object GetService(Type serviceType)
+        {
+            foreach (var serviceProvider in this.innerServiceProviders)
+            {
+                var service = serviceProvider.GetService(serviceType);
+                if (service != null)
+                {
+                    return service;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged
+        /// resources.
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (var serviceProvider in this.innerServiceProviders)
+            {
+                (serviceProvider as IDisposable)?.Dispose();
+            }
+
+            this.innerServiceProviders.Clear();
+        }
+    }
+}
