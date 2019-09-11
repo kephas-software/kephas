a16c4653b300c8c115bfa09e338a6eb9b8b4d94f
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-09-11 18:13:29 +0300

    Added an AggregateMessage to gather multiple replies

---
+++ src/Kephas.Messaging/Messages/AggregateMessage.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AggregateMessage.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the aggregate message class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Messaging.Messages
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// An aggregate message.
+    /// </summary>
+    public class AggregateMessage : IMessage
+    {
+        /// <summary>
+        /// Gets or sets the messages.
+        /// </summary>
+        /// <value>
+        /// The messages.
+        /// </value>
+        public IEnumerable<IMessage> Messages { get; set; }
+    }
+}
