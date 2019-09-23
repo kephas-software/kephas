8edfb33fca71e0e7d8267b67da811b130fc89329
Ioan Crisan <ioan.crisan@quartz-software.com>
2019-09-24 01:43:02 +0300

    Renamed I/AppInfo to I/RuntimeAppInfo. Do not inherit from IExpando, instead add expando Properties.

--- src/Kephas.Orchestration/IRuntimeAppInfo.cs
+++ src/Kephas.Orchestration/IRuntimeAppInfo.cs
@@ -1,68 +1,78 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AppInfo.cs" company="Kephas Software SRL">
+// <copyright file="IAppInfo.cs" company="Kephas Software SRL">
 //   Copyright (c) Kephas Software SRL. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 // <summary>
-//   Implements the application information class.
+//   Declares the IRuntimeAppInfo interface.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace Kephas.Orchestration
 {
+    using System.Collections.Generic;
+
     using Kephas.Dynamic;
 
     /// <summary>
-    /// Information about the application.
+    /// Interface for runtime application information.
     /// </summary>
-    public class AppInfo : Expando, IAppInfo
+    public interface IRuntimeAppInfo
     {
         /// <summary>
-        /// Gets or sets the identifier of the application.
+        /// Gets the identifier of the application.
         /// </summary>
         /// <value>
         /// The identifier of the application.
         /// </value>
-        public string AppId { get; set; }
+        string AppId { get; }
 
         /// <summary>
-        /// Gets or sets the identifier of the application instance.
+        /// Gets the identifier of the application instance.
         /// </summary>
         /// <value>
         /// The identifier of the application instance.
         /// </value>
-        public string AppInstanceId { get; set; }
+        string AppInstanceId { get; }
 
         /// <summary>
-        /// Gets or sets the aupported app features.
+        /// Gets the aupported app features.
         /// </summary>
         /// <value>
         /// The supported app features.
         /// </value>
-        public string[] Features { get; set; }
+        string[] Features { get; }
 
         /// <summary>
-        /// Gets or sets the identifier of the process running the application.
+        /// Gets the identifier of the process running the application.
         /// </summary>
         /// <value>
         /// The identifier of the process.
         /// </value>
-        public int ProcessId { get; set; }
+        int ProcessId { get; }
 
         /// <summary>
-        /// Gets or sets the name of the host where the application process runs.
+        /// Gets the name of the host where the application process runs.
         /// </summary>
         /// <value>
         /// The name of the host.
         /// </value>
-        public string HostName { get; set; }
+        string HostName { get; }
 
         /// <summary>
-        /// Gets or sets the address of the host where the application process runs.
+        /// Gets the address of the host where the application process runs.
         /// </summary>
         /// <value>
         /// The host address.
         /// </value>
-        public string HostAddress { get; set; }
+        string HostAddress { get; }
+
+        /// <summary>
+        /// Gets or sets the application properties.
+        /// </summary>
+        /// <value>
+        /// The application properties.
+        /// </value>
+        IDictionary<string, object> Properties { get; set; }
     }
 }
---
+++ src/Kephas.Orchestration/RuntimeAppInfo.cs
@@ -0,0 +1,116 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppInfo.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the application information class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Orchestration
+{
+    using System.Collections.Generic;
+
+    using Kephas.Dynamic;
+
+    /// <summary>
+    /// Runtime information about the application.
+    /// </summary>
+    public class RuntimeAppInfo : IRuntimeAppInfo
+    {
+        /// <summary>
+        /// Gets or sets the identifier of the application.
+        /// </summary>
+        /// <value>
+        /// The identifier of the application.
+        /// </value>
+        public string AppId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier of the application instance.
+        /// </summary>
+        /// <value>
+        /// The identifier of the application instance.
+        /// </value>
+        public string AppInstanceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the aupported app features.
+        /// </summary>
+        /// <value>
+        /// The supported app features.
+        /// </value>
+        public string[] Features { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier of the process running the application.
+        /// </summary>
+        /// <value>
+        /// The identifier of the process.
+        /// </value>
+        public int ProcessId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the host where the application process runs.
+        /// </summary>
+        /// <value>
+        /// The name of the host.
+        /// </value>
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the host where the application process runs.
+        /// </summary>
+        /// <value>
+        /// The host address.
+        /// </value>
+        public string HostAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the application properties.
+        /// </summary>
+        /// <value>
+        /// The application properties.
+        /// </value>
+        public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
+
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current object.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return this.AppInstanceId?.GetHashCode() ?? 0;
+        }
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>
+        /// True if the specified object is equal to the current object; otherwise, false.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is RuntimeAppInfo info)
+            {
+                return info.AppInstanceId == this.AppInstanceId;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current object.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"{this.AppId}/{this.AppInstanceId} on {this.HostName} ({this.HostAddress}), pid:{this.ProcessId}";
+        }
+    }
+}
