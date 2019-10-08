53bf4bb946088966999a5c8696f27a1b4b6ba54d
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-10-08 15:44:03 +0300

    Added tests for ContextFactory and Signature

---
+++ src/Tests/Kephas.Core.Tests/Reflection/SignatureTest.cs
@@ -0,0 +1,79 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SignatureTest.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the signature test class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Core.Tests.Reflection
+{
+    using Kephas.Reflection;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SignatureTest
+    {
+        [Test]
+        public void Equals_this()
+        {
+            var s = new Signature();
+            Assert.IsTrue(s.Equals(s));
+        }
+
+        [Test]
+        public void Equals_empty()
+        {
+            var s = new Signature();
+            var s2 = new Signature();
+            Assert.IsTrue(s.Equals(s2));
+        }
+
+        [Test]
+        public void Equals_same_types()
+        {
+            var s = new Signature(typeof(string), typeof(int));
+            var s2 = new Signature(typeof(string), typeof(int));
+            Assert.IsTrue(s.Equals(s2));
+        }
+
+        [Test]
+        public void Equals_different_types()
+        {
+            var s = new Signature(typeof(string), typeof(long));
+            var s2 = new Signature(typeof(string), typeof(int));
+            Assert.IsFalse(s.Equals(s2));
+        }
+
+        [Test]
+        public void ToString_int_string()
+        {
+            var s = new Signature(typeof(int), typeof(string));
+            Assert.AreEqual("(Int32, String)", s.ToString());
+        }
+
+        [Test]
+        public void ToString_Signature_nullablelong_null()
+        {
+            var s = new Signature(typeof(Signature), typeof(long?), null);
+            Assert.AreEqual("(Signature, Int64?, )", s.ToString());
+        }
+
+        [Test]
+        public void GetEnumerator_type()
+        {
+            var s = new Signature(typeof(Signature), typeof(bool), typeof(int?));
+            var e = s.GetEnumerator();
+            e.MoveNext();
+            Assert.AreSame(typeof(Signature), e.Current);
+            e.MoveNext();
+            Assert.AreSame(typeof(bool), e.Current);
+            e.MoveNext();
+            Assert.AreSame(typeof(int?), e.Current);
+            Assert.IsFalse(e.MoveNext());
+        }
+    }
+}
---
+++ src/Tests/Kephas.Core.Tests/Services/ContextFactoryTest.cs
@@ -0,0 +1,83 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ContextFactoryTest.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the context factory test class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Core.Tests.Services
+{
+    using System.Linq;
+
+    using Kephas.Composition;
+    using Kephas.Cryptography;
+    using Kephas.Serialization;
+    using Kephas.Services;
+    using Kephas.Services.Reflection;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ContextFactoryTest
+    {
+        [Test]
+        public void CreateContext_Context()
+        {
+            var (ambientServices, compositionContext) = this.GetServices();
+            var factory = new ContextFactory(compositionContext, ambientServices);
+            var context = factory.CreateContext<Context>();
+
+            Assert.AreSame(ambientServices, context.AmbientServices);
+            Assert.AreSame(compositionContext, context.CompositionContext);
+        }
+
+        [Test]
+        public void CreateContext_EncryptionContext()
+        {
+            var (ambientServices, compositionContext) = this.GetServices();
+            var factory = new ContextFactory(compositionContext, ambientServices);
+            var context = factory.CreateContext<EncryptionContext>();
+
+            Assert.AreSame(ambientServices, context.AmbientServices);
+            Assert.AreSame(compositionContext, context.CompositionContext);
+        }
+
+        [Test]
+        public void CreateContext_SerializationContext()
+        {
+            var serializationService = Substitute.For<ISerializationService>();
+
+            var (ambientServices, compositionContext) = this.GetServices(new AppServiceInfo(typeof(ISerializationService), serializationService));
+            compositionContext.GetExport(typeof(ISerializationService), Arg.Any<string>()).Returns(serializationService);
+            var factory = new ContextFactory(compositionContext, ambientServices);
+            var context = factory.CreateContext<SerializationContext>(typeof(string));
+
+            Assert.AreSame(ambientServices, context.AmbientServices);
+            Assert.AreSame(compositionContext, context.CompositionContext);
+            Assert.AreSame(serializationService, context.SerializationService);
+            Assert.AreSame(typeof(string), context.MediaType);
+        }
+
+        private (IAmbientServices ambientServices, ICompositionContext compositionContext) GetServices(params IAppServiceInfo[] appServiceInfos)
+        {
+            var ambientServices = Substitute.For<IAmbientServices>();
+            var compositionContext = Substitute.For<ICompositionContext>();
+
+            var infos = appServiceInfos.Select(i => (contractType: i.ContractType, appServiceInfo: i));
+
+            ambientServices.CompositionContainer.Returns(compositionContext);
+            ambientServices.GetService(typeof(ICompositionContext)).Returns(compositionContext);
+            ambientServices["__AppServiceInfosKey"].Returns(infos);
+
+            compositionContext.GetExport(typeof(IAmbientServices), Arg.Any<string>()).Returns(ambientServices);
+            compositionContext.GetExport<IAmbientServices>(Arg.Any<string>()).Returns(ambientServices);
+            compositionContext.GetExport(typeof(ICompositionContext), Arg.Any<string>()).Returns(compositionContext);
+            compositionContext.GetExport<ICompositionContext>(Arg.Any<string>()).Returns(compositionContext);
+
+            return (ambientServices, compositionContext);
+        }
+    }
+}
