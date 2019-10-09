7ede2917e4542bd0b36410769ab985f062b75575
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-10-09 13:11:12 +0300

    Add ShutdownAwaiterAppLifecycleBehavior to initialize the awaiter before features are called.

---
+++ src/Kephas.Application/ShutdownAwaiterAppLifecycleBehavior.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ShutdownAwaiterAppLifecycleBehavior.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the shutdown awaiter application lifecycle behavior class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Application
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Services;
+    using Kephas.Threading.Tasks;
+
+    /// <summary>
+    /// A shutdown awaiter application lifecycle behavior.
+    /// </summary>
+    /// <remarks>
+    /// Makes sure that the shutdown awaiter has a chance to initialize before the application manager starts initializing the features.
+    /// Sometimes the features and the behaviors order a shutdown (like a setup routine).
+    /// </remarks>
+    [ProcessingPriority(Priority.Highest)]
+    public class ShutdownAwaiterAppLifecycleBehavior : AppLifecycleBehaviorBase
+    {
+        private readonly IAppShutdownAwaiter awaiter;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShutdownAwaiterAppLifecycleBehavior"/> class.
+        /// </summary>
+        /// <param name="awaiter">The awaiter.</param>
+        public ShutdownAwaiterAppLifecycleBehavior(IAppShutdownAwaiter awaiter)
+        {
+            this.awaiter = awaiter;
+        }
+
+        /// <summary>
+        /// Interceptor called before the application starts its asynchronous initialization.
+        /// </summary>
+        /// <param name="appContext">Context for the application.</param>
+        /// <param name="cancellationToken">Optional. The cancellation token.</param>
+        /// <returns>
+        /// A Task.
+        /// </returns>
+        public override async Task BeforeAppInitializeAsync(IAppContext appContext, CancellationToken cancellationToken = default)
+        {
+            if (this.awaiter is IAsyncInitializable asyncAwaiter)
+            {
+                await asyncAwaiter.InitializeAsync(appContext).PreserveThreadContext();
+            }
+            else if (this.awaiter is IInitializable syncAwaiter)
+            {
+                syncAwaiter.Initialize(appContext);
+            }
+        }
+    }
+}
