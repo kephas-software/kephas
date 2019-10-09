333547ac7d8033960975d2a7ac1e694a9bd748bf
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-10-09 13:22:21 +0300

    Bumped version to rc.36

--- src/Deployment/CreateNugetPackages.ps1
+++ src/Deployment/CreateNugetPackages.ps1
@@ -6,11 +6,6 @@
     [string]$Timestamper = "http://timestamp.digicert.com"
 )
 
-# The symbol source is changed since Nov. 2018
-# https://blog.nuget.org/20181116/Improved-debugging-experience-with-the-NuGet-org-symbol-server-and-snupkg.html
-# however, a bug is still present - until it is fixed stay with the old symbols
-
-
 function get-packagename([string]$pathname) {
     return $pathname.Replace("..\", "").Replace("TestingFramework\", "")
 }
