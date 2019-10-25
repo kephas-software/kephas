3b9c9a3d4d31969b7a60fc28289314acd95f379a
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-10-25 13:56:39 +0300

    Added more tools to sign folder

--- tools/sign/mongo-sign.ps1
+++ tools/sign/mongo-sign.ps1
@@ -7,18 +7,19 @@ $editorPath = "$Env:ProgramFiles\Notepad++\notepad++.exe"
 
 $assemblies = @(
     "mongodb.bson.$version\lib\net452\MongoDB.Bson",
-    "mongodb.bson.$version\lib\netstandard1.5\MongoDB.Bson",
-    "mongodb.driver.$version\lib\net452\MongoDB.Driver",
-    "mongodb.driver.$version\lib\netstandard1.5\MongoDB.Driver",
-    "mongodb.driver.core.$version\lib\net452\MongoDB.Driver.Core",
-    "mongodb.driver.core.$version\lib\netstandard1.5\MongoDB.Driver.Core",
-    "mongodb.driver.gridfs.$version\lib\net452\MongoDB.Driver.GridFS",
-    "mongodb.driver.gridfs.$version\lib\netstandard1.5\MongoDB.Driver.GridFS"
+    "mongodb.bson.$version\lib\netstandard1.5\MongoDB.Bson"
+#    "mongodb.bson.$version\lib\netstandard1.5\MongoDB.Bson",
+#    "mongodb.driver.$version\lib\net452\MongoDB.Driver",
+#    "mongodb.driver.$version\lib\netstandard1.5\MongoDB.Driver",
+#    "mongodb.driver.core.$version\lib\net452\MongoDB.Driver.Core",
+#    "mongodb.driver.core.$version\lib\netstandard1.5\MongoDB.Driver.Core",
+#    "mongodb.driver.gridfs.$version\lib\net452\MongoDB.Driver.GridFS",
+#    "mongodb.driver.gridfs.$version\lib\netstandard1.5\MongoDB.Driver.GridFS"
 )
 
 foreach ($assembly in $assemblies) {
 	Write-Host "Decompiling $assembly.dll..."
-	iex "ildasm.exe /all /out=$assembly.signed.il $assembly.dll > $assembly.decompile.log"
+	iex ".\ildasm.exe /all /out=$assembly.signed.il $assembly.dll > $assembly.decompile.log"
 	iex "& ""$editorPath"" ""$assembly.signed.il"""
 	Read-Host -Prompt "Decompiling complete, press ENTER key to start with compiling..."
 	iex "ilasm.exe /dll /key=$keyPath $assembly.signed.il > $assembly.compile.log"
