4b356b39899baa571202225848ed11d682c832de
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-12-02 16:28:04 +0200

    Added IGuidIdentifiable to Model.Abstractions

---
+++ src/Kephas.Data.Model.Abstractions/Abstractions/IGuidIdentifiable.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IGuidIdentifiable.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Declares the IGuidIdentifiable interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Model.Abstractions
+{
+    using System;
+
+    using Kephas.Data.Model.AttributedModel;
+    using Kephas.Model.AttributedModel;
+
+    /// <summary>
+    /// Interface for unique identifier identifiable.
+    /// </summary>
+    [Mixin]
+    [Key(new[] { nameof(Guid) })]
+    public interface IGuidIdentifiable
+    {
+        /// <summary>
+        /// Gets or sets a unique identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier of the unique.
+        /// </value>
+        Guid Guid { get; set; }
+    }
+}
