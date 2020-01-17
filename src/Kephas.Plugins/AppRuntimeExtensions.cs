059d177422af09c5bf9877c99772b43f0c19526e
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-01-17 12:14:55 +0200

    Added IAppRuntime.GetPluginsFolder() extension method.

---
+++ src/Kephas.Plugins/AppRuntimeExtensions.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppRuntimeExtensions.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the application runtime extensions class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Plugins
+{
+    using Kephas.Application;
+    using Kephas.Plugins.Application;
+
+    /// <summary>
+    /// An application runtime extensions.
+    /// </summary>
+    public static class AppRuntimeExtensions
+    {
+        /// <summary>
+        /// An IAppRuntime extension method that gets plugins folder.
+        /// </summary>
+        /// <param name="appRuntime">The appRuntime to act on.</param>
+        /// <returns>
+        /// The plugins folder.
+        /// </returns>
+        public static string GetPluginsFolder(this IAppRuntime appRuntime)
+        {
+            if (appRuntime is PluginsAppRuntime pluginsAppRuntime)
+            {
+                return pluginsAppRuntime.PluginsFolder;
+            }
+
+            return appRuntime?[nameof(PluginsAppRuntime.PluginsFolder)] as string;
+        }
+    }
+}
--- src/Kephas.Plugins/Application/PluginsAppRuntime.cs
+++ src/Kephas.Plugins/Application/PluginsAppRuntime.cs
@@ -15,7 +15,7 @@ namespace Kephas.Plugins.Application
     using System.IO;
     using System.Linq;
     using System.Reflection;
-    using Kephas;
+
     using Kephas.Application;
     using Kephas.Dynamic;
     using Kephas.Logging;
