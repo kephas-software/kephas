4a6a5a0080db1daba602bc32d5538feb52db628c
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-01-20 11:23:31 +0200

    Added text processing tests

---
+++ src/Tests/Kephas.TextProcessing.Tests/TextProcessingTestBase.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TextProcessingTestBase.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the text processing test base class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.TextProcessing.Tests
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Kephas.Testing.Composition;
+
+    public abstract class TextProcessingTestBase : CompositionTestBase
+    {
+        public override IEnumerable<Assembly> GetDefaultConventionAssemblies()
+        {
+            return new List<Assembly>(base.GetDefaultConventionAssemblies()) {
+                typeof(ITokenizer).Assembly,            // Kephas.TextProcessing
+            };
+        }
+    }
+}
---
+++ src/Tests/Kephas.TextProcessing.Tests/TokenizerTest.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TokenizerTest.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the tokenizer test class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.TextProcessing.Tests
+{
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TokenizerTest : TextProcessingTestBase
+    {
+        [Test]
+        public void Composition()
+        {
+            var container = this.CreateContainer();
+            var tokenizer = container.GetExport<ITokenizer>();
+
+            Assert.IsInstanceOf<Tokenizer>(tokenizer);
+        }
+
+        [TestCase("hi there", new[] { "hi" })]
+        [TestCase("Please, come home quickly!", new[] { "Please", "come", "home", "quickly" })]
+        [TestCase("The \"stereotype\" is 'key'!", new[] { "stereotype", "is", "key" })]
+        public void Tokenize_simple(string text, string[] expectedTokens)
+        {
+            var container = this.CreateContainer();
+            var tokenizer = container.GetExport<ITokenizer>();
+
+            var tokens = tokenizer.Tokenize(text).ToArray();
+
+            CollectionAssert.AreEqual(expectedTokens, tokens);
+        }
+    }
+}
