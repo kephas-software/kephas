47e35f8dd2d3779badf62706da16ac0714a867b9
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-01-28 17:36:18 +0200

    Added UpdatePlugin operation

---
+++ src/Kephas.Plugins.Endpoints/UpdatePluginMessage.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UpdatePluginMessage.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the update plugin message class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Plugins.Endpoints
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Kephas.ComponentModel.DataAnnotations;
+    using Kephas.Messaging;
+
+    /// <summary>
+    /// An update plugin message.
+    /// </summary>
+    [TypeDisplay(Description = "Updates the installed plugin with the indicated version.")]
+    public class UpdatePluginMessage : IMessage
+    {
+        /// <summary>
+        /// Gets or sets the package ID.
+        /// </summary>
+        /// <value>
+        /// The package ID.
+        /// </value>
+        [Display(Description = "The package ID of the plugin.")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version.
+        /// </summary>
+        /// <value>
+        /// The version.
+        /// </value>
+        [Display(Description = "The package version of the plugin.")]
+        public string Version { get; set; }
+    }
+}
---
+++ src/Kephas.Plugins.Endpoints/UpdatePluginMessageHandler.cs
@@ -0,0 +1,69 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UpdatePluginMessageHandler.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the update plugin message handler class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Plugins.Endpoints
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Application;
+    using Kephas.Logging;
+    using Kephas.Messaging;
+    using Kephas.Messaging.Messages;
+    using Kephas.Plugins;
+    using Kephas.Threading.Tasks;
+
+    /// <summary>
+    /// An update plugin message handler.
+    /// </summary>
+    public class UpdatePluginMessageHandler : MessageHandlerBase<UpdatePluginMessage, ResponseMessage>
+    {
+        private readonly IPluginManager pluginManager;
+        private readonly IAppContext appContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdatePluginMessageHandler"/> class.
+        /// </summary>
+        /// <param name="pluginManager">Manager for plugins.</param>
+        /// <param name="appContext">Context for the application.</param>
+        public UpdatePluginMessageHandler(IPluginManager pluginManager, IAppContext appContext)
+        {
+            this.pluginManager = pluginManager;
+            this.appContext = appContext;
+        }
+
+        /// <summary>
+        /// Processes the provided message asynchronously and returns a response promise.
+        /// </summary>
+        /// <param name="message">The message to be handled.</param>
+        /// <param name="context">The processing context.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// The response promise.
+        /// </returns>
+        public override async Task<ResponseMessage> ProcessAsync(UpdatePluginMessage message, IMessagingContext context, CancellationToken token)
+        {
+            this.appContext.Logger.Info("Installing plugin {plugin} {version}...", message.Id, message.Version);
+
+            var result = await this.pluginManager.UpdatePluginAsync(new AppIdentity(message.Id, message.Version)).PreserveThreadContext();
+
+            var plugin = result.ReturnValue;
+            var pluginId = plugin?.GetTypeInfo().Name ?? message.Id;
+            var pluginVersion = plugin?.GetTypeInfo().Version ?? message.Version;
+
+            this.appContext.Logger.Info("Plugin {plugin} {version} updated in {pluginPath}. Elapsed: {elapsed:c}.", pluginId, pluginVersion, plugin?.FolderPath, result.Elapsed);
+
+            return new ResponseMessage
+            {
+                Message = $"Plugin {pluginId} ({pluginVersion}) updated in {plugin?.FolderPath}. Elapsed: {result.Elapsed:c}.",
+            };
+        }
+    }
+}
