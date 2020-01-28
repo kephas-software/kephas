07d18b34a906ad2c3c1637e5953c89c669b8b4a4
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-01-28 17:35:30 +0200

    Added specialized plugin context fro plugin operations

---
+++ src/Kephas.Plugins/IPluginContext.cs
@@ -0,0 +1,124 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IPluginContext.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Declares the IPluginContext interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Plugins
+{
+    using Kephas.Application;
+    using Kephas.Diagnostics.Contracts;
+    using Kephas.Services;
+
+    /// <summary>
+    /// Values that represent plugin operations.
+    /// </summary>
+    /// <seealso/>
+    public enum PluginOperation
+    {
+        /// <summary>
+        /// An enum constant representing the install option.
+        /// </summary>
+        Install,
+
+        /// <summary>
+        /// An enum constant representing the initialize option.
+        /// </summary>
+        Initialize,
+
+        /// <summary>
+        /// An enum constant representing the uninitialize option.
+        /// </summary>
+        Uninitialize,
+
+        /// <summary>
+        /// An enum constant representing the uninstall option.
+        /// </summary>
+        Uninstall,
+
+        /// <summary>
+        /// An enum constant representing the update option.
+        /// </summary>
+        Update,
+    }
+
+    /// <summary>
+    /// Interface for plugin context.
+    /// </summary>
+    public interface IPluginContext : IContext
+    {
+        /// <summary>
+        /// Gets or sets the plugin.
+        /// </summary>
+        /// <value>
+        /// The plugin.
+        /// </value>
+        AppIdentity Plugin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the operation.
+        /// </summary>
+        /// <value>
+        /// The operation.
+        /// </value>
+        PluginOperation? Operation { get; set; }
+    }
+
+    /// <summary>
+    /// A plugin context extensions.
+    /// </summary>
+    public static class PluginContextExtensions
+    {
+        /// <summary>
+        /// Sets the plugin operation.
+        /// </summary>
+        /// <typeparam name="TContext">Type of the context.</typeparam>
+        /// <param name="context">The context to act on.</param>
+        /// <param name="operation">The operation.</param>
+        /// <param name="overwrite">Optional. True to overwrite the previously set value, false to preserve it.</param>
+        /// <returns>
+        /// This <paramref name="context"/>.
+        /// </returns>
+        public static TContext Operation<TContext>(this TContext context, PluginOperation operation, bool overwrite = true)
+            where TContext : class, IPluginContext
+        {
+            Requires.NotNull(context, nameof(context));
+
+            if (context.Operation.HasValue && !overwrite)
+            {
+                return context;
+            }
+
+            context.Operation = operation;
+            return context;
+        }
+
+        /// <summary>
+        /// Sets the plugin operation.
+        /// </summary>
+        /// <typeparam name="TContext">Type of the context.</typeparam>
+        /// <param name="context">The context to act on.</param>
+        /// <param name="plugin">The plugin.</param>
+        /// <param name="overwrite">Optional. True to overwrite the previously set value, false to preserve it.</param>
+        /// <returns>
+        /// This <paramref name="context"/>.
+        /// </returns>
+        public static TContext Plugin<TContext>(this TContext context, AppIdentity plugin, bool overwrite = true)
+            where TContext : class, IPluginContext
+        {
+            Requires.NotNull(context, nameof(context));
+
+            if (context.Plugin != null && !overwrite)
+            {
+                return context;
+            }
+
+            context.Plugin = plugin;
+            return context;
+        }
+    }
+}
---
+++ src/Kephas.Plugins/PluginContext.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PluginContext.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the plugin context class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Plugins
+{
+    using Kephas.Application;
+    using Kephas.Composition;
+    using Kephas.Services;
+
+    /// <summary>
+    /// A plugin context.
+    /// </summary>
+    public class PluginContext : Context, IPluginContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluginContext"/> class.
+        /// </summary>
+        /// <param name="compositionContext">Context for the composition.</param>
+        public PluginContext(ICompositionContext compositionContext)
+            : base(compositionContext)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the plugin.
+        /// </summary>
+        /// <value>
+        /// The plugin.
+        /// </value>
+        public AppIdentity Plugin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the operation.
+        /// </summary>
+        /// <value>
+        /// The operation.
+        /// </value>
+        public PluginOperation? Operation { get; set; }
+    }
+}
