df0019e0d01b9460832fa032ef3bc8c6c8b02639
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-02-05 10:45:26 +0200

    Added sketch for Kephas.Packaging

---
+++ src/Kephas.Packaging/IPackageService.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IPackageService.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Declares the IPackageService interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Packaging
+{
+    using Kephas.Services;
+
+    /// <summary>
+    /// Interface for package service.
+    /// </summary>
+    [SingletonAppServiceContract]
+    public interface IPackageService
+    {
+    }
+}
