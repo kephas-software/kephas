4157856e83261d9fb59b07eb0859c362ae1f8c7a
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-05-06 12:40:35 +0300

    Added primary implementation of the BSON serialization using MongoDB.Bson

---
+++ src/Kephas.Serialization.MongoDBBson/BsonSerializer.cs
@@ -0,0 +1,174 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BsonSerializer.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the bson serializer class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Serialization.Bson
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Net.Mime;
+    using Kephas.Serialization;
+    using Kephas.Services;
+    using Kephas.Threading.Tasks;
+    using MongoDB.Bson.IO;
+
+    using MongoBsonSerializer = MongoDB.Bson.Serialization.BsonSerializer;
+
+    /// <summary>
+    /// A BSON serializer.
+    /// </summary>
+    [OverridePriority(Priority.Low)]
+    public class BsonSerializer : ISerializer<BsonMediaType>
+#if NETSTANDARD2_1
+#else
+        , ISyncSerializer
+#endif
+    {
+        /// <summary>
+        /// Deserializes an object.
+        /// </summary>
+        /// <param name="textReader">The <see cref="TextReader"/> containing the serialized object.</param>
+        /// <param name="context">The context containing serialization options.</param>
+        /// <returns>
+        /// The deserialized object.
+        /// </returns>
+        public object? Deserialize(TextReader textReader, ISerializationContext context)
+        {
+            if (context.RootObjectType == null)
+            {
+                throw new InvalidOperationException($"The root object type must be specified in the context.");
+            }
+
+            using var jsonReader = new JsonReader(textReader);
+            return MongoBsonSerializer.Deserialize(jsonReader, context.RootObjectType, context.DeserializeConfigurator());
+        }
+
+        /// <summary>
+        /// Deserializes an object.
+        /// </summary>
+        /// <param name="serializedObj">The serialized object.</param>
+        /// <param name="context">The context containing serialization options.</param>
+        /// <returns>
+        /// The deserialized object.
+        /// </returns>
+        public object? Deserialize(string? serializedObj, ISerializationContext context)
+        {
+            if (serializedObj == null)
+            {
+                return null;
+            }
+
+            if (context.RootObjectType == null)
+            {
+                throw new InvalidOperationException($"The root object type must be specified in the context.");
+            }
+
+            using var jsonReader = new JsonReader(serializedObj);
+            return MongoBsonSerializer.Deserialize(jsonReader, context.RootObjectType, context.DeserializeConfigurator());
+        }
+
+        /// <summary>
+        /// Deserializes an object asynchronously.
+        /// </summary>
+        /// <param name="textReader">The <see cref="TextReader"/> containing the serialized object.</param>
+        /// <param name="context">The context containing serialization options.</param>
+        /// <param name="cancellationToken">Optional. The cancellation token.</param>
+        /// <returns>
+        /// An asynchronous result that yields the deserialized object.
+        /// </returns>
+        public Task<object?> DeserializeAsync(TextReader textReader, ISerializationContext context, CancellationToken cancellationToken = default)
+        {
+            return ((Func<object?>)(() => Deserialize(textReader, context))).AsAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Deserializes an object asynchronously.
+        /// </summary>
+        /// <param name="serializedObj">The serialized object.</param>
+        /// <param name="context">The context containing serialization options.</param>
+        /// <param name="cancellationToken">Optional. The cancellation token.</param>
+        /// <returns>
+        /// An asynchronous result that yields the deserialized object.
+        /// </returns>
+        public Task<object?> DeserializeAsync(string? serializedObj, ISerializationContext context, CancellationToken cancellationToken = default)
+        {
+            return ((Func<object?>)(() => Deserialize(serializedObj, context))).AsAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Serializes the provided object.
+        /// </summary>
+        /// <param name="obj">The object to be serialized.</param>
+        /// <param name="textWriter">The <see cref="TextWriter"/> used to write the object content.</param>
+        /// <param name="context">The context containing serialization options.</param>
+        public void Serialize(object? obj, TextWriter textWriter, ISerializationContext context)
+        {
+            if (obj == null)
+            {
+                return;
+            }
+
+            using var jsonWriter = new JsonWriter(textWriter);
+            MongoBsonSerializer.Serialize(jsonWriter, context.RootObjectType ?? obj.GetType(), obj, context.SerializeConfigurator());
+        }
+
+        /// <summary>
+        /// Serializes the provided object.
+        /// </summary>
+        /// <param name="obj">The object to be serialized.</param>
+        /// <param name="context">The context containing serialization options.</param>
+        /// <returns>
+        /// The serialized object.
+        /// </returns>
+        public string? Serialize(object? obj, ISerializationContext context)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+
+            using var sbWriter = new StringWriter();
+            using var jsonWriter = new JsonWriter(sbWriter);
+            MongoBsonSerializer.Serialize(jsonWriter, context.RootObjectType ?? obj.GetType(), obj, context.SerializeConfigurator());
+            return sbWriter.ToString();
+        }
+
+        /// <summary>
+        /// Serializes the provided object asynchronously.
+        /// </summary>
+        /// <param name="obj">The object to be serialized.</param>
+        /// <param name="textWriter">The <see cref="TextWriter"/> used to write the object content.</param>
+        /// <param name="context">The context containing serialization options.</param>
+        /// <param name="cancellationToken">Optional. The cancellation token.</param>
+        /// <returns>
+        /// An asynchronous result.
+        /// </returns>
+        public Task SerializeAsync(object? obj, TextWriter textWriter, ISerializationContext context, CancellationToken cancellationToken = default)
+        {
+            return ((Action)(() => Serialize(obj, textWriter, context))).AsAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Serializes the provided object asynchronously.
+        /// </summary>
+        /// <param name="obj">The object to be serialized.</param>
+        /// <param name="context">The context containing serialization options.</param>
+        /// <param name="cancellationToken">Optional. The cancellation token.</param>
+        /// <returns>
+        /// An asynchronous result that yields the serialized object.
+        /// </returns>
+        public Task<string?> SerializeAsync(object? obj, ISerializationContext context, CancellationToken cancellationToken = default)
+        {
+            return ((Func<string?>)(() => Serialize(obj, context))).AsAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Tests/Kephas.Serialization.MongoDBBson.Tests/BsonSerializerTest.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BsonSerializerTest.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the bson serializer test class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Serialization.Bson.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Kephas.Dynamic;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BsonSerializerTest : SerializationTestBase
+    {
+        [Test]
+        public async Task SerializeAsync_Composition()
+        {
+            var container = this.CreateContainer();
+            var serializationService = container.GetExport<ISerializationService>();
+
+            var obj = new TestEntity
+            {
+                Name = "John Doe",
+                PersonalSite = new Uri("http://site.com/my-site"),
+            };
+
+            var serializedObj = await serializationService.BsonSerializeAsync(obj);
+
+            Assert.AreEqual(@"{ ""_t"" : ""TestEntity"", ""Name"" : ""John Doe"", ""PersonalSite"" : ""http://site.com/my-site"" }", serializedObj);
+        }
+
+        public class TestEntity
+        {
+            public string Name { get; set; }
+
+            public Uri PersonalSite { get; set; }
+        }
+
+        public class ExpandoEntity : Expando
+        {
+            public string Description { get; set; }
+        }
+    }
+}
