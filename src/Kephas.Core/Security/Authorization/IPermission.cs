5536cf357f138b9b3a9e9dce4d017e466a7be5cb
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-06-01 18:45:17 +0300

    Added IPermission marker interface for permissions.

---
+++ src/Kephas.Core/Security/Authorization/IPermission.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IPermission.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Security.Authorization
+{
+    /// <summary>
+    /// Marker interface for permissions.
+    /// </summary>
+    public interface IPermission
+    {
+    }
+}
