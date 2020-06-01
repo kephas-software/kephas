11d5958c97262583ce463e2c33dc8da52fb148e7
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-06-01 18:48:23 +0300

    Added IToken(TokenName: string) to be implemented by permission metadata (and not only).

---
+++ src/Kephas.Core/Reflection/IToken.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IToken.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Reflection
+{
+    /// <summary>
+    /// Defines a token.
+    /// </summary>
+    public interface IToken
+    {
+        /// <summary>
+        /// Gets the token name.
+        /// </summary>
+        public string TokenName { get; }
+    }
+}
