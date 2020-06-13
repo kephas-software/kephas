73a41ccfec535044b97f280789f85ca9e7894aa2
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-06-14 02:52:04 +0300

    Added AppSetupQueryEvent for querying whether application setup is possible.

---
+++ src/Kephas.Core/Application/Interaction/AppSetupQueryEvent.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppSetupQueryEvent.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Application.Interaction
+{
+    /// <summary>
+    /// Event for querying whether setup activities can be executed.
+    /// </summary>
+    public class AppSetupQueryEvent
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the setup activities are enabled.
+        /// </summary>
+        public bool SetupEnabled { get; set; } = true;
+    }
+}
