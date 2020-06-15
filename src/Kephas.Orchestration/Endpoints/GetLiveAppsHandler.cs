0fc3d21770c0f61c9bbc38dbc68fb40c097b5f27
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-06-15 23:21:07 +0300

    Added the GetLiveApps endpoint

---
+++ src/Kephas.Orchestration/Endpoints/GetLiveAppsHandler.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetLiveAppsHandler.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Orchestration.Endpoints
+{
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Messaging;
+    using Kephas.Threading.Tasks;
+
+    /// <summary>
+    /// Handler for the <see cref="GetLiveAppsMessage"/>.
+    /// </summary>
+    public class GetLiveAppsHandler : MessageHandlerBase<GetLiveAppsMessage, GetLiveAppsResponseMessage>
+    {
+        private readonly IOrchestrationManager orchestrationManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetLiveAppsHandler"/> class.
+        /// </summary>
+        /// <param name="orchestrationManager">The orchestration manager.</param>
+        public GetLiveAppsHandler(IOrchestrationManager orchestrationManager)
+        {
+            this.orchestrationManager = orchestrationManager;
+        }
+
+        /// <summary>
+        /// Processes the provided message asynchronously and returns a response promise.
+        /// </summary>
+        /// <param name="message">The message to be handled.</param>
+        /// <param name="context">The processing context.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// The response promise.
+        /// </returns>
+        public override async Task<GetLiveAppsResponseMessage> ProcessAsync(GetLiveAppsMessage message, IMessagingContext context, CancellationToken token)
+        {
+            var liveApps = await this.orchestrationManager.GetLiveAppsAsync(cancellationToken: token)
+                .PreserveThreadContext();
+            return new GetLiveAppsResponseMessage
+            {
+                Apps = liveApps.ToArray(),
+            };
+        }
+    }
+}
---
+++ src/Kephas.Orchestration/Endpoints/GetLiveAppsMessage.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetLiveAppsMessage.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Orchestration.Endpoints
+{
+    using Kephas.ComponentModel.DataAnnotations;
+    using Kephas.Messaging;
+    using Kephas.Messaging.Messages;
+    using Kephas.Security.Authorization;
+    using Kephas.Security.Authorization.AttributedModel;
+
+    /// <summary>
+    /// Message for getting the live application instances.
+    /// </summary>
+    [DisplayInfo(Description = "Gets the live application instances.")]
+    [RequiresPermission(typeof(AppAdminPermission))]
+    public class GetLiveAppsMessage : IMessage
+    {
+    }
+
+    /// <summary>
+    /// Response message for <see cref="GetLiveAppsMessage"/>.
+    /// </summary>
+    public class GetLiveAppsResponseMessage : ResponseMessage
+    {
+        /// <summary>
+        /// Gets or sets the live apps.
+        /// </summary>
+        public IRuntimeAppInfo[] Apps { get; set; }
+    }
+}
