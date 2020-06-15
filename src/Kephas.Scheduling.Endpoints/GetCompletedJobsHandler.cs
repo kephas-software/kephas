131cca0545a3218e6d43aedcc37b70de558e7711
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-06-15 20:53:17 +0300

    Added the GetCompletedJobs endpoint

---
+++ src/Kephas.Scheduling.Endpoints/GetCompletedJobsHandler.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetCompletedJobsHandler.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Scheduling.Endpoints
+{
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Messaging;
+
+    /// <summary>
+    /// Handler for the <see cref="GetCompletedJobsMessage"/>.
+    /// </summary>
+    public class GetCompletedJobsHandler : MessageHandlerBase<GetCompletedJobsMessage, GetCompletedJobsResponseMessage>
+    {
+        private readonly IScheduler scheduler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetCompletedJobsHandler"/> class.
+        /// </summary>
+        /// <param name="scheduler">The scheduler.</param>
+        public GetCompletedJobsHandler(IScheduler scheduler)
+        {
+            this.scheduler = scheduler;
+        }
+
+        /// <summary>
+        /// Processes the provided message asynchronously and returns a response promise.
+        /// </summary>
+        /// <param name="message">The message to be handled.</param>
+        /// <param name="context">The processing context.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// The response promise.
+        /// </returns>
+        public override async Task<GetCompletedJobsResponseMessage> ProcessAsync(GetCompletedJobsMessage message, IMessagingContext context, CancellationToken token)
+        {
+            await Task.Yield();
+
+            var jobsQuery = this.scheduler.GetCompletedJobs()
+                .Skip(message.Skip)
+                .Take(message.Take);
+
+            return new GetCompletedJobsResponseMessage
+            {
+                Jobs = jobsQuery
+                    .Select(jobResult => new CompletedJobData
+                    {
+                        JobId = jobResult.JobId,
+                        JobInfo = jobResult.JobInfo.ToString(),
+                        OperationState = jobResult.OperationState,
+                        PercentCompleted = jobResult.PercentCompleted,
+                        StartedAt = jobResult.StartedAt,
+                        EndedAt = jobResult.EndedAt,
+                        Elapsed = jobResult.Elapsed,
+                    })
+                    .ToArray(),
+            };
+        }
+    }
+}
---
+++ src/Kephas.Scheduling.Endpoints/GetCompletedJobsMessage.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetCompletedJobsMessage.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Kephas.Scheduling.Endpoints
+{
+    using Kephas.ComponentModel.DataAnnotations;
+    using Kephas.Messaging;
+    using Kephas.Messaging.Messages;
+
+    /// <summary>
+    /// Message for getting the completed jobs.
+    /// </summary>
+    [DisplayInfo(Description = "Gets the running jobs.")]
+    public class GetCompletedJobsMessage : IMessage
+    {
+        /// <summary>
+        /// Gets or sets the number of packages to skip.
+        /// </summary>
+        /// <value>
+        /// The number of packages to skip.
+        /// </value>
+        [DisplayInfo(Description = "Optional. Value indicating the number of jobs to skip (default: 0).")]
+        public int Skip { get; set; } = 0;
+
+        /// <summary>
+        /// Gets or sets the number of packages to take.
+        /// </summary>
+        /// <value>
+        /// The number of packages to take.
+        /// </value>
+        [DisplayInfo(Description = "Optional. Value indicating the number of jobs to take (default: 20).")]
+        public int Take { get; set; } = 20;
+    }
+
+    /// <summary>
+    /// Response message for getting the completed jobs.
+    /// </summary>
+    public class GetCompletedJobsResponseMessage : ResponseMessage
+    {
+        /// <summary>
+        /// Gets or sets the jobs data.
+        /// </summary>
+        public RunningJobData[] Jobs { get; set; }
+    }
+
+    /// <summary>
+    /// Provides data fro completed jobs.
+    /// </summary>
+    public class CompletedJobData : RunningJobData
+    {
+        /// <summary>
+        /// Gets the time when the job ended.
+        /// </summary>
+        public DateTimeOffset? EndedAt { get; set; }
+    }
+}
