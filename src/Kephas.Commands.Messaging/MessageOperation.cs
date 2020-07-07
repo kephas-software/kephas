433a566868539da3d5bfda75cc16bad946e46ec5
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-07-07 12:22:39 +0300

    Provided extension point in MessageOperationInfo.CreateOperation(message), which creates an MessageOperation.

--- src/Kephas.Commands.Messaging/MessageOperation.cs
+++ src/Kephas.Commands.Messaging/MessageOperation.cs
@@ -35,7 +35,7 @@ namespace Kephas.Commands.Messaging
         /// </summary>
         /// <param name="message">The message.</param>
         /// <param name="lazyMessageProcessor">The lazy message processor.</param>
-        internal MessageOperation(object message, Lazy<IMessageProcessor> lazyMessageProcessor)
+        protected internal MessageOperation(object message, Lazy<IMessageProcessor> lazyMessageProcessor)
         {
             this.message = message;
             this.lazyMessageProcessor = lazyMessageProcessor;
@@ -48,11 +48,11 @@ namespace Kephas.Commands.Messaging
         /// <returns>
         /// An object.
         /// </returns>
-        public object? Execute(IContext? context = null)
+        public virtual object? Execute(IContext? context = null)
         {
             var result = this.lazyMessageProcessor.Value.ProcessAsync(this.message, ctx => ctx.Merge(context))
                 .GetResultNonLocking();
-            return result.GetContent();
+            return result?.GetContent();
         }
 
         /// <summary>
@@ -63,13 +63,13 @@ namespace Kephas.Commands.Messaging
         /// <returns>
         /// An object.
         /// </returns>
-        public async Task<object?> ExecuteAsync(IContext? context = null, CancellationToken cancellationToken = default)
+        public virtual async Task<object?> ExecuteAsync(IContext? context = null, CancellationToken cancellationToken = default)
         {
             var result = await this.lazyMessageProcessor.Value.ProcessAsync(
                     this.message,
                     ctx => ctx.Merge(context),
                     cancellationToken).PreserveThreadContext();
-            return result.GetContent();
+            return result?.GetContent();
         }
     }
 }
