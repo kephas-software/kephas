b104e82a79899880a3a8272dcb89cc181c3f8af6
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-09-08 22:47:48 +0300

    Core: Added IDataFormattable and support for it.
    (cherry picked from commit 6dfaa39a089bb6951679310c032f7898eff6df15)

---
+++ src/Kephas.Core/Data/Formatting/IDataFormattingContext.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IDataFormattingContext.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Data.Formatting
+{
+    using Kephas.Services;
+
+    /// <summary>
+    /// Context for data formatting.
+    /// </summary>
+    public interface IDataFormattingContext : IContext
+    {
+    }
+}
