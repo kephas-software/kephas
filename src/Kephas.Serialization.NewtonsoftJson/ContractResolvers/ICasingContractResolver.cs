cade1a976c041c0bfa2b8505420951edd0ba8e31
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-11-10 22:05:10 +0200

    Serialization.NewtonsoftJson: Added ICasingContractResolver to convert back and forth the property names. Optimized the resolvers to set the proper converter on the types.

---
+++ src/Kephas.Serialization.NewtonsoftJson/ContractResolvers/ICasingContractResolver.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ICasingContractResolver.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Serialization.Json.ContractResolvers
+{
+    using Newtonsoft.Json.Serialization;
+
+    /// <summary>
+    /// Contract resolver interface handling name casing.
+    /// </summary>
+    public interface ICasingContractResolver : IContractResolver
+    {
+        /// <summary>
+        /// Gets the deserialized property name.
+        /// </summary>
+        /// <param name="propertyName">The serialized property name.</param>
+        /// <returns>The deserialized property name.</returns>
+        string GetDeserializedPropertyName(string propertyName);
+
+        /// <summary>
+        /// Gets the serialized property name.
+        /// </summary>
+        /// <param name="propertyName">The deserialized property name.</param>
+        /// <returns>The serialized property name.</returns>
+        string GetSerializedPropertyName(string propertyName);
+    }
+}
---
+++ src/Kephas.Serialization.NewtonsoftJson/ContractResolvers/InvariantCaseContractResolver.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InvariantCaseContractResolver.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Serialization.Json.ContractResolvers
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    /// <summary>
+    /// Optimized contract resolver for invariant case properties.
+    /// </summary>
+    public class InvariantCaseContractResolver : DefaultContractResolver, ICasingContractResolver
+    {
+        private readonly IEnumerable<JsonConverter> converters;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvariantCaseContractResolver"/> class.
+        /// </summary>
+        /// <param name="converters">The converters.</param>
+        public InvariantCaseContractResolver(IEnumerable<JsonConverter> converters)
+        {
+            this.converters = converters;
+        }
+
+        /// <summary>
+        /// Gets the deserialized property name.
+        /// </summary>
+        /// <param name="propertyName">The serialized property name.</param>
+        /// <returns>The deserialized property name.</returns>
+        public string GetDeserializedPropertyName(string propertyName)
+            => propertyName;
+
+        /// <summary>
+        /// Gets the serialized property name.
+        /// </summary>
+        /// <param name="propertyName">The deserialized property name.</param>
+        /// <returns>The serialized property name.</returns>
+        public string GetSerializedPropertyName(string propertyName)
+            => propertyName;
+
+        /// <summary>
+        /// Determines which contract type is created for the given type.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>A <see cref="T:Newtonsoft.Json.Serialization.JsonContract" /> for the given type.</returns>
+        protected override JsonContract CreateContract(Type objectType)
+        {
+            var contract = base.CreateContract(objectType);
+            foreach (var converter in this.converters)
+            {
+                if (converter.CanConvert(objectType) && converter.CanWrite && converter.CanRead)
+                {
+                    contract.Converter = converter;
+                    break;
+                }
+            }
+
+            return contract;
+        }
+    }
+}
