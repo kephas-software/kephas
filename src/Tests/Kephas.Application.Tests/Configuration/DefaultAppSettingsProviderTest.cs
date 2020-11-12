ed2c1d9a732d5c2fca7f9bce9a89acab119bad88
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2020-11-12 22:04:01 +0200

    Add composition test for DEfaultAppSettingsProvider

---
+++ src/Tests/Kephas.Application.Tests/Configuration/DefaultAppSettingsProviderTest.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefaultAppSettingsProviderTest.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Application.Tests.Configuration
+{
+    using Kephas.Application.Configuration;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DefaultAppSettingsProviderTest : ApplicationTestBase
+    {
+        [Test]
+        public void Composition()
+        {
+            var container = this.CreateContainer();
+            var provider = container.GetExport<IAppSettingsProvider>();
+
+            Assert.IsInstanceOf<DefaultAppSettingsProvider>(provider);
+        }
+    }
+}
