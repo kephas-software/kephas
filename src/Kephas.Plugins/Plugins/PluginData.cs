e2bc82009a8aa23e84fd2446f30914037010c6d4
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2020-11-17 14:56:30 +0200

    Fix .NET Standard 2.1 refactorings
    (cherry picked from commit dbcd5a2aa6435643d8ee5df095d40606f5e30c62)

--- src/Kephas.Plugins/Plugins/PluginData.cs
+++ src/Kephas.Plugins/Plugins/PluginData.cs
@@ -136,7 +136,7 @@ namespace Kephas.Plugins
 
 
 #if NETSTANDARD2_1
-            var data = splits.Length > 3 ? DataParse(splits[3..^4]) : null;
+            var data = splits.Length > 3 ? DataParse(splits[3..^1]) : null;
             if (!int.TryParse(splits[^1], out var checksum))
 #else
             var data = splits.Length > 3 ? DataParse(splits.Skip(3).Take(splits.Length - 4)) : null;
