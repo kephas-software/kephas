1ad151e9e30f9f5d7d49a98e91bcc36c7e507478
Alexandru Cruceru <alex.cruceru@kephas-software.com>
2021-02-03 00:18:00 +0200

    tests FormatWith_success and JoinWith_success updated tests FormatWith_format_null and JoinWith_Join_ null added

--- src/Tests/Kephas.Core.Tests/Text/StringExtensionsTest.cs
+++ src/Tests/Kephas.Core.Tests/Text/StringExtensionsTest.cs
@@ -10,7 +10,9 @@
 
 namespace Kephas.Core.Tests.Text
 {
+    using System;
     using System.Linq;
+
     using Kephas;
 
     using NUnit.Framework;
@@ -77,14 +79,22 @@ namespace Kephas.Core.Tests.Text
         [TestCase("1{0}4{1}7{2}", new[] { "23", "56", "89" }, "123456789")]
         [TestCase("1{0}2{1}3{2}4", new[] { " x ", " y ", " z " }, "1 x 2 y 3 z 4")]
         [TestCase("123456789", new[] { " x ", " y ", " z " }, "123456789")]
-        public void Format_with(string format, string[] args, string expected)
+        public void FormatWith_success(string format, string[] args, string expected)
         {
             var formatted = StringExtensions.FormatWith(format, args);
+
             Assert.AreEqual(expected.Length, formatted.Length);
-            for (var i = 0; i < expected.Length; i++)
+            var i = 0;
+            foreach (var expectedValue in expected)
             {
-                Assert.AreEqual(expected[i], formatted[i]);
+                Assert.AreEqual(expectedValue, formatted[i++]);
             }
+        }
+
+        [Test]
+        public void FormatWith_format_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => StringExtensions.FormatWith(null, new string[0]));
         }
 
         [Test]
@@ -93,14 +103,28 @@ namespace Kephas.Core.Tests.Text
         [TestCase("x", new[] { "123456789" }, "123456789")]
         [TestCase("xx", new[] { "123 ", " 456 ", " 789" }, "123 xx 456 xx 789")]
 
-        public void Join_with(string separator, string[] args, string expected)
+        public void JoinWith_success(string separator, string[] args, string expected)
         {
             var joined = StringExtensions.JoinWith(separator, args);
+
             Assert.AreEqual(expected.Length, joined.Length);
-            for (var i = 0; i < expected.Length; i++)
+            var i = 0;
+            foreach (var expectedValue in expected)
             {
-                Assert.AreEqual(expected[i], joined[i]);
+                Assert.AreEqual(expectedValue, joined[i++]);
             }
+
+            //i = 0;
+            //var badValues = from v in expected
+            //                where v != joined[i++]
+            //                select v + "ok";
+            //badValues = expected.Where(v => v != joined[i++]).Select(v => v + "ok");
+        }
+
+        [Test]
+        public void JoinWith_join_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => StringExtensions.JoinWith("123", null));
         }
     }
 }
