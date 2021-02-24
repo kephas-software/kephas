3a6714c4cc55f8870baa45669f31e4dc7b83e4ec
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-02-24 20:50:39 +0200

    Breaking change: Renamed IHostConfigurator to IMiddlewareConfigurator.

--- src/Kephas.AspNetCore/Hosting/IMiddlewareConfigurator.cs
+++ src/Kephas.AspNetCore/Hosting/IMiddlewareConfigurator.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IHostConfigurator.cs" company="Kephas Software SRL">
+// <copyright file="IMiddlewareConfigurator.cs" company="Kephas Software SRL">
 //   Copyright (c) Kephas Software SRL. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -14,13 +14,13 @@ namespace Kephas.Application.AspNetCore.Hosting
     using Kephas.Services;
 
     /// <summary>
-    /// Interface for host configurator.
+    /// Interface for middleware configurator.
     /// </summary>
     [SingletonAppServiceContract(AllowMultiple = true)]
-    public interface IHostConfigurator
+    public interface IMiddlewareConfigurator
     {
         /// <summary>
-        /// Configures the host using the given application context.
+        /// Configures a specific middleware using the given application context.
         /// </summary>
         /// <param name="appContext">Context for the application.</param>
         void Configure(IAspNetAppContext appContext);
