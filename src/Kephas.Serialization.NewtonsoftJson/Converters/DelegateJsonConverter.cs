27b7c08d45fec689bee171ecee04526d8a86e8d1
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-02-27 20:01:14 +0200

    Serialization.NewtonsoftJson: Added DelegateJsonConverter.

---
+++ src/Kephas.Serialization.NewtonsoftJson/Converters/DelegateJsonConverter.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DelegateJsonConverter.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Serialization.Json.Converters
+{
+    using System;
+
+    using Kephas.Services.Reflection;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// JSON converter for <see cref="IAppServiceInfo"/>.
+    /// </summary>
+    /// <remarks>
+    /// Dictionaries should be processed before collections, that's why the higher priority.
+    /// </remarks>
+    public class DelegateJsonConverter : JsonConverterBase
+    {
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:Newtonsoft.Json.JsonConverter" /> can read JSON.
+        /// </summary>
+        /// <value><c>true</c> if this <see cref="T:Newtonsoft.Json.JsonConverter" /> can read JSON; otherwise, <c>false</c>.</value>
+        public override bool CanRead => false;
+
+        /// <summary>Writes the JSON representation of the object.</summary>
+        /// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter" /> to write to.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        public override void WriteJson(JsonWriter writer, object? value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            writer.WriteUndefined();
+        }
+
+        /// <summary>Reads the JSON representation of the object.</summary>
+        /// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader" /> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <param name="existingValue">The existing value of object being read.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        /// <returns>The object value.</returns>
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>
+        /// <c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(Delegate).IsAssignableFrom(objectType);
+        }
+    }
+}
