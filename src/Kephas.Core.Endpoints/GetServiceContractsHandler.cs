50eb7ce39188f86861a07deafbd7a61e2399598a
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-02-27 20:42:34 +0200

    Core.Endpoints: Published the GetServiceContracts endpoint.

---
+++ src/Kephas.Core.Endpoints/GetServiceContractsHandler.cs
@@ -0,0 +1,69 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetServiceContractsHandler.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Core.Endpoints
+{
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Messaging;
+    using Kephas.Services.Composition;
+
+    /// <summary>
+    /// Message handler for <see cref="GetServiceContractsMessage"/>.
+    /// </summary>
+    public class GetServiceContractsHandler : MessageHandlerBase<GetServiceContractsMessage, GetServiceContractsResponseMessage>
+    {
+        private readonly IAmbientServices ambientServices;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetServiceContractsHandler"/> class.
+        /// </summary>
+        /// <param name="ambientServices">The ambient services.</param>
+        public GetServiceContractsHandler(IAmbientServices ambientServices)
+        {
+            this.ambientServices = ambientServices;
+        }
+
+        /// <summary>
+        /// Processes the provided message asynchronously and returns a response promise.
+        /// </summary>
+        /// <param name="message">The message to be handled.</param>
+        /// <param name="context">The processing context.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// The response promise.
+        /// </returns>
+        public override Task<GetServiceContractsResponseMessage> ProcessAsync(GetServiceContractsMessage message, IMessagingContext context, CancellationToken token)
+        {
+            var appServiceInfos = this.ambientServices
+                .GetAppServiceInfos()
+                .Select(i => i.appServiceInfo);
+
+            if (!string.IsNullOrEmpty(message.ContractType))
+            {
+                appServiceInfos = appServiceInfos.Where(i => i.ContractType.Name.Contains(message.ContractType));
+            }
+
+            if (message.AllowMultiple != null)
+            {
+                appServiceInfos = appServiceInfos.Where(i => i.AllowMultiple == message.AllowMultiple.Value);
+            }
+
+            if (message.AsOpenGeneric != null)
+            {
+                appServiceInfos = appServiceInfos.Where(i => i.AsOpenGeneric == message.AsOpenGeneric);
+            }
+
+            return Task.FromResult(new GetServiceContractsResponseMessage
+            {
+                ServiceInfos = appServiceInfos.ToArray(),
+            });
+        }
+    }
+}
---
+++ src/Kephas.Core.Endpoints/GetServiceContractsMessage.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetServiceContractsMessage.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Core.Endpoints
+{
+    using Kephas.ComponentModel.DataAnnotations;
+    using Kephas.Messaging;
+    using Kephas.Messaging.Messages;
+    using Kephas.Services.Reflection;
+
+    /// <summary>
+    /// Message for retrieving the application service contracts.
+    /// </summary>
+    [DisplayInfo(Description = "Gets the application service contracts.")]
+    public class GetServiceContractsMessage : IMessage
+    {
+        /// <summary>
+        /// Gets or sets the contract type.
+        /// </summary>
+        [DisplayInfo(Description = "Filter on the contract type.")]
+        public string? ContractType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flag for allowing multiple registrations.
+        /// </summary>
+        [DisplayInfo(Description = "Filter on the AllowMultiple flag.")]
+        public bool? AllowMultiple { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flag for the open generic registration.
+        /// </summary>
+        [DisplayInfo(Description = "Filter on the AsOpenGeneric flag.")]
+        public bool? AsOpenGeneric { get; set; }
+    }
+
+    /// <summary>
+    /// The response message for <see cref="GetServiceContractsMessage"/>.
+    /// </summary>
+    public class GetServiceContractsResponseMessage : ResponseMessage
+    {
+        /// <summary>
+        /// Gets or sets the service information.
+        /// </summary>
+        public IAppServiceInfo[]? ServiceInfos { get; set; }
+    }
+}
