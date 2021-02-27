62466c48c402acd59acab7d31bbec1d709cf3870
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-02-27 20:02:27 +0200

    Serialization.NewtonsoftJson: Added tests for AppServiceInfoJsonConverter and AppServiceMetadataJsonConverter.

---
+++ src/Tests/Kephas.Serialization.NewtonsoftJson.Tests/Converters/AppServiceInfoJsonConverterTest.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppServiceInfoJsonConverterTest.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Serialization.Json.Tests.Converters
+{
+    using System.Threading.Tasks;
+
+    using Kephas.Composition;
+    using Kephas.Services;
+    using Kephas.Services.Reflection;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AppServiceInfoJsonConverterTest : SerializationTestBase
+    {
+        [Test]
+        public async Task SerializeAsync_transient()
+        {
+            var settingsProvider = GetJsonSerializerSettingsProvider();
+            var serializer = new JsonSerializer(settingsProvider);
+            var obj = new AppServiceInfo(typeof(string), AppServiceLifetime.Transient);
+            var serializedObj = await serializer.SerializeAsync(obj);
+
+            Assert.AreEqual(@"{""ContractType"":""System.String"",""Lifetime"":""Transient"",""AllowMultiple"":false,""AsOpenGeneric"":false}", serializedObj);
+        }
+
+        [Test]
+        public async Task DeserializeAsync_transient()
+        {
+            var settingsProvider = GetJsonSerializerSettingsProvider();
+            var serializer = new JsonSerializer(settingsProvider);
+            var obj = await serializer.DeserializeAsync(
+                @"{""ContractType"":""System.String"",""Lifetime"":""Transient"",""AllowMultiple"":false,""AsOpenGeneric"":false}",
+                new SerializationContext(Substitute.For<ICompositionContext>(), Substitute.For<ISerializationService>()) { RootObjectType = typeof(IAppServiceInfo) });
+
+            Assert.IsInstanceOf<AppServiceInfo>(obj);
+            var appServiceInfo = (IAppServiceInfo)obj!;
+            Assert.AreEqual(typeof(string), appServiceInfo.ContractType);
+            Assert.AreEqual(AppServiceLifetime.Transient, appServiceInfo.Lifetime);
+            Assert.IsFalse(appServiceInfo.AllowMultiple);
+            Assert.IsNull(appServiceInfo.InstanceType);
+        }
+    }
+}
---
+++ src/Tests/Kephas.Serialization.NewtonsoftJson.Tests/Converters/AppServiceMetadataJsonConverterTest.cs
@@ -0,0 +1,84 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppServiceMetadataJsonConverterTest.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Serialization.Json.Tests.Converters
+{
+    using System.Threading.Tasks;
+
+    using Kephas.Net.Mime;
+    using Kephas.Serialization.Composition;
+    using Kephas.Services.Composition;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AppServiceMetadataJsonConverterTest : SerializationTestBase
+    {
+        [Test]
+        public async Task SerializeAsync_AppServiceMetadata()
+        {
+            var settingsProvider = GetJsonSerializerSettingsProvider();
+            var serializer = new JsonSerializer(settingsProvider);
+            var obj = new AppServiceMetadata(processingPriority: 12, overridePriority: 24, serviceName: "gigi", isOverride: true)
+            {
+                ServiceInstanceType = typeof(int),
+            };
+            var serializedObj = await serializer.SerializeAsync(obj);
+
+            Assert.AreEqual(@"{""processingPriority"":12,""overridePriority"":24,""isOverride"":true,""serviceName"":""gigi"",""serviceInstanceType"":""System.Int32"",""dependencies"":[]}", serializedObj);
+        }
+
+        [Test]
+        public async Task DeserializeAsync_AppServiceMetadata()
+        {
+            var settingsProvider = GetJsonSerializerSettingsProvider();
+            var serializer = new JsonSerializer(settingsProvider);
+            var obj = await serializer.DeserializeAsync(
+                @"{""processingPriority"":12,""overridePriority"":24,""isOverride"":true,""serviceName"":""gigi"",""serviceInstanceType"":""System.Int32"",""dependencies"":[]}",
+                GetSerializationContext(typeof(AppServiceMetadata)));
+
+            Assert.IsInstanceOf<AppServiceMetadata>(obj);
+            var expando = (AppServiceMetadata)obj;
+            Assert.AreEqual("gigi", expando.ServiceName);
+            Assert.AreEqual(12, expando.ProcessingPriority);
+            Assert.AreEqual(24, expando.OverridePriority);
+            Assert.AreEqual(typeof(int), expando.ServiceInstanceType);
+            Assert.IsTrue(expando.IsOverride);
+        }
+
+        [Test]
+        public async Task SerializeAsync_SerializerMetadata()
+        {
+            var settingsProvider = GetJsonSerializerSettingsProvider();
+            var serializer = new JsonSerializer(settingsProvider);
+            var obj = new SerializerMetadata(typeof(XmlMediaType), processingPriority: 12, overridePriority: 24, serviceName: "gigi", isOverride: true)
+            {
+                ServiceInstanceType = typeof(int),
+            };
+            var serializedObj = await serializer.SerializeAsync(obj);
+
+            Assert.AreEqual(@"{""$type"":""Kephas.Serialization.Composition.SerializerMetadata"",""mediaType"":""Kephas.Net.Mime.XmlMediaType"",""processingPriority"":12,""overridePriority"":24,""isOverride"":true,""serviceName"":""gigi"",""serviceInstanceType"":""System.Int32"",""dependencies"":[]}", serializedObj);
+        }
+
+        [Test]
+        public async Task DeserializeAsync_SerializerMetadata()
+        {
+            var settingsProvider = GetJsonSerializerSettingsProvider();
+            var serializer = new JsonSerializer(settingsProvider);
+            var obj = await serializer.DeserializeAsync(
+                @"{""$type"":""Kephas.Serialization.Composition.SerializerMetadata"",""mediaType"":""Kephas.Net.Mime.XmlMediaType"",""processingPriority"":12,""overridePriority"":24,""isOverride"":true,""serviceName"":""gigi"",""serviceInstanceType"":""System.Int32"",""dependencies"":[]}",
+                GetSerializationContext(typeof(SerializerMetadata)));
+
+            Assert.IsInstanceOf<SerializerMetadata>(obj);
+            var expando = (SerializerMetadata)obj;
+            Assert.AreEqual("gigi", expando.ServiceName);
+            Assert.AreEqual(12, expando.ProcessingPriority);
+            Assert.AreEqual(24, expando.OverridePriority);
+            Assert.AreEqual(typeof(int), expando.ServiceInstanceType);
+            Assert.IsTrue(expando.IsOverride);
+        }
+    }
+}
