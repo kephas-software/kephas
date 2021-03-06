10430f6bc71b68be81392925407120c79d0f4793
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-03-06 11:56:03 +0200

    AspNetCore.IdentityServer4: Made IdentityUserStoreServiceBase<TUser> implement IUserPasswordStore<TUser>.

--- src/Kephas.AspNetCore.IdentityServer4/Stores/IdentityUserStoreServiceBase.cs
+++ src/Kephas.AspNetCore.IdentityServer4/Stores/IdentityUserStoreServiceBase.cs
@@ -17,7 +17,7 @@ namespace Kephas.AspNetCore.IdentityServer4.Stores
     /// Base class for in-memory user store service.
     /// </summary>
     /// <typeparam name="TUser">The user type.</typeparam>
-    public abstract class IdentityUserStoreServiceBase<TUser> : IUserStoreService<TUser>, IUserEmailStore<TUser>
+    public abstract class IdentityUserStoreServiceBase<TUser> : IUserStoreService<TUser>, IUserPasswordStore<TUser>, IUserEmailStore<TUser>
         where TUser : IdentityUser
     {
         /// <summary>
@@ -195,6 +195,44 @@ namespace Kephas.AspNetCore.IdentityServer4.Stores
         public virtual Task SetEmailConfirmedAsync(TUser user, bool confirmed, CancellationToken cancellationToken)
         {
             user.EmailConfirmed = confirmed;
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Gets the password hash for the specified <paramref name="user" />.
+        /// </summary>
+        /// <param name="user">The user whose password hash to retrieve.</param>
+        /// <param name="cancellationToken">The <see cref="T:System.Threading.CancellationToken" /> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>The <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous operation, returning the password hash for the specified <paramref name="user" />.</returns>
+        public Task<string> GetPasswordHashAsync(TUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        /// <summary>
+        /// Gets a flag indicating whether the specified <paramref name="user" /> has a password.
+        /// </summary>
+        /// <param name="user">The user to return a flag for, indicating whether they have a password or not.</param>
+        /// <param name="cancellationToken">The <see cref="T:System.Threading.CancellationToken" /> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>
+        /// The <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous operation, returning true if the specified <paramref name="user" /> has a password
+        /// otherwise false.
+        /// </returns>
+        public Task<bool> HasPasswordAsync(TUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(!string.IsNullOrEmpty(user.PasswordHash));
+        }
+
+        /// <summary>
+        /// Sets the password hash for the specified <paramref name="user" />.
+        /// </summary>
+        /// <param name="user">The user whose password hash to set.</param>
+        /// <param name="passwordHash">The password hash to set.</param>
+        /// <param name="cancellationToken">The <see cref="T:System.Threading.CancellationToken" /> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>The <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous operation.</returns>
+        public Task SetPasswordHashAsync(TUser user, string passwordHash, CancellationToken cancellationToken)
+        {
+            user.PasswordHash = passwordHash;
             return Task.CompletedTask;
         }
 
