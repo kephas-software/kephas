9dde9f667afddd2ce79d82c2b531bd0d35856f2c
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-03-06 11:42:23 +0200

    AspNetCore.IdentityServer4: Added AuthenticationBuilder.AddIdentityServerJwt extension method.

---
+++ src/Kephas.AspNetCore.IdentityServer4/Authentication/AuthenticationBuilderExtensions.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AuthenticationBuilderExtensions.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.AspNetCore.IdentityServer4.Authentication
+{
+    using System;
+
+    using Kephas.AspNetCore.IdentityServer4.Configuration;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+    using Microsoft.Extensions.Options;
+
+    /// <summary>
+    /// Extension methods to configure authentication for existing APIs coexisting with an Authorization Server.
+    /// </summary>
+    public static class AuthenticationBuilderExtensions
+    {
+        private const string IdentityServerJwtNameSuffix = "API";
+
+        private static readonly PathString DefaultIdentityUIPathPrefix =
+            new PathString("/Identity");
+
+        /// <summary>
+        /// Adds an authentication handler for an API that coexists with an Authorization Server.
+        /// </summary>
+        /// <param name="builder">The <see cref="AuthenticationBuilder"/>.</param>
+        /// <returns>The provided <see cref="AuthenticationBuilder"/>.</returns>
+        public static AuthenticationBuilder AddIdentityServerJwt(this AuthenticationBuilder builder)
+        {
+            var services = builder.Services;
+            services.TryAddSingleton<IIdentityServerJwtDescriptor, IdentityServerJwtDescriptor>();
+            services.TryAddEnumerable(ServiceDescriptor
+                .Transient<IConfigureOptions<JwtBearerOptions>, IdentityServerJwtBearerOptionsConfiguration>(JwtBearerOptionsFactory));
+
+            services.AddAuthentication(IdentityServerJwtConstants.IdentityServerJwtScheme)
+                .AddPolicyScheme(IdentityServerJwtConstants.IdentityServerJwtScheme, null, options =>
+                {
+                    options.ForwardDefaultSelector = new IdentityServerJwtPolicySchemeForwardSelector(
+                        DefaultIdentityUIPathPrefix,
+                        IdentityServerJwtConstants.IdentityServerJwtBearerScheme).SelectScheme;
+                })
+                .AddJwtBearer(IdentityServerJwtConstants.IdentityServerJwtBearerScheme, null, o => { });
+
+            return builder;
+
+            IdentityServerJwtBearerOptionsConfiguration JwtBearerOptionsFactory(IServiceProvider sp)
+            {
+                var schemeName = IdentityServerJwtConstants.IdentityServerJwtBearerScheme;
+
+                var localApiDescriptor = sp.GetRequiredService<IIdentityServerJwtDescriptor>();
+                var hostingEnvironment = sp.GetRequiredService<IWebHostEnvironment>();
+                var apiName = hostingEnvironment.ApplicationName + IdentityServerJwtNameSuffix;
+
+                return new IdentityServerJwtBearerOptionsConfiguration(schemeName, apiName, localApiDescriptor);
+            }
+        }
+    }
+}
--- src/Kephas.AspNetCore.IdentityServer4/Services/ClientRequestParametersProvider.cs
+++ src/Kephas.AspNetCore.IdentityServer4/Services/ClientRequestParametersProvider.cs
@@ -18,9 +18,17 @@ namespace Kephas.AspNetCore.IdentityServer4.Services
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Options;
 
+    /// <summary>
+    /// Default implementation of <see cref="IClientRequestParametersProvider"/>.
+    /// </summary>
     [OverridePriority(Priority.Low)]
     public class ClientRequestParametersProvider : IClientRequestParametersProvider
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientRequestParametersProvider"/> class.
+        /// </summary>
+        /// <param name="urlResolver">The absolute URL resolver.</param>
+        /// <param name="options">The options.</param>
         public ClientRequestParametersProvider(
             IAbsoluteUrlResolver urlResolver,
             IOptions<ApiAuthorizationOptions> options)
@@ -29,13 +37,25 @@ namespace Kephas.AspNetCore.IdentityServer4.Services
             this.Options = options;
         }
 
+        /// <summary>
+        /// Gets the absolute URL resolver.
+        /// </summary>
         protected IAbsoluteUrlResolver UrlResolver { get; }
 
+        /// <summary>
+        /// Gets the API authorization options.
+        /// </summary>
         protected IOptions<ApiAuthorizationOptions> Options { get; }
 
+        /// <summary>
+        /// Gets parameter values for the client with client id<paramref name="clientId"/>.
+        /// </summary>
+        /// <param name="context">The current <see cref="HttpContext"/>.</param>
+        /// <param name="clientId">The client id for the client.</param>
+        /// <returns>A <see cref="IDictionary{TKey, TValue}"/> containing the client parameters and their values.</returns>
         public IDictionary<string, string> GetClientParameters(HttpContext context, string clientId)
         {
-            var client = Options.Value.Clients[clientId];
+            var client = this.Options.Value.Clients[clientId];
             var authority = context.GetIdentityServerIssuerUri();
             var responseType = string.Empty;
             if (!client.Properties.TryGetValue(ApplicationProfilesPropertyNames.Profile, out var type))
@@ -65,5 +85,4 @@ namespace Kephas.AspNetCore.IdentityServer4.Services
             };
         }
     }
-
 }
