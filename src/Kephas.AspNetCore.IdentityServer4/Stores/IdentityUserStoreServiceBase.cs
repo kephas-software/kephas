19fa5ba0af850b749a641f0862f3a3453032926a
Ioan Crișan <ioan.crisan@kephas-software.com>
2021-03-12 08:15:26 +0200

    Made IdentityUserStoreServiceBase support phone number.

--- src/Kephas.AspNetCore.IdentityServer4/Stores/IdentityUserStoreServiceBase.cs
+++ src/Kephas.AspNetCore.IdentityServer4/Stores/IdentityUserStoreServiceBase.cs
@@ -18,7 +18,7 @@ namespace Kephas.AspNetCore.IdentityServer4.Stores
     /// Base class for in-memory user store service.
     /// </summary>
     /// <typeparam name="TUser">The user type.</typeparam>
-    public abstract class IdentityUserStoreServiceBase<TUser> : Loggable, IUserStoreService<TUser>, IUserPasswordStore<TUser>, IUserEmailStore<TUser>, IUserConfirmation<TUser>, IUserLockoutStore<TUser>
+    public abstract class IdentityUserStoreServiceBase<TUser> : Loggable, IUserStoreService<TUser>, IUserPasswordStore<TUser>, IUserEmailStore<TUser>, IUserConfirmation<TUser>, IUserLockoutStore<TUser>, IUserPhoneNumberStore<TUser>
         where TUser : IdentityUser
     {
         /// <summary>
@@ -353,6 +353,57 @@ namespace Kephas.AspNetCore.IdentityServer4.Stores
         public virtual Task SetLockoutEndDateAsync(TUser user, DateTimeOffset? lockoutEnd, CancellationToken cancellationToken)
         {
             user.LockoutEnd = lockoutEnd;
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Gets the telephone number, if any, for the specified <paramref name="user" />.
+        /// </summary>
+        /// <param name="user">The user whose telephone number should be retrieved.</param>
+        /// <param name="cancellationToken">The <see cref="T:System.Threading.CancellationToken" /> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>The <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous operation, containing the user's telephone number, if any.</returns>
+        public virtual Task<string> GetPhoneNumberAsync(TUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PhoneNumber);
+        }
+
+        /// <summary>
+        /// Gets a flag indicating whether the specified <paramref name="user" />'s telephone number has been confirmed.
+        /// </summary>
+        /// <param name="user">The user to return a flag for, indicating whether their telephone number is confirmed.</param>
+        /// <param name="cancellationToken">The <see cref="T:System.Threading.CancellationToken" /> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>
+        /// The <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous operation, returning true if the specified <paramref name="user" /> has a confirmed
+        /// telephone number otherwise false.
+        /// </returns>
+        public virtual Task<bool> GetPhoneNumberConfirmedAsync(TUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PhoneNumberConfirmed);
+        }
+
+        /// <summary>
+        /// Sets the telephone number for the specified <paramref name="user" />.
+        /// </summary>
+        /// <param name="user">The user whose telephone number should be set.</param>
+        /// <param name="phoneNumber">The telephone number to set.</param>
+        /// <param name="cancellationToken">The <see cref="T:System.Threading.CancellationToken" /> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>The <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous operation.</returns>
+        public virtual Task SetPhoneNumberAsync(TUser user, string phoneNumber, CancellationToken cancellationToken)
+        {
+            user.PhoneNumber = phoneNumber;
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Sets a flag indicating if the specified <paramref name="user" />'s phone number has been confirmed.
+        /// </summary>
+        /// <param name="user">The user whose telephone number confirmation status should be set.</param>
+        /// <param name="confirmed">A flag indicating whether the user's telephone number has been confirmed.</param>
+        /// <param name="cancellationToken">The <see cref="T:System.Threading.CancellationToken" /> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>The <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous operation.</returns>
+        public virtual Task SetPhoneNumberConfirmedAsync(TUser user, bool confirmed, CancellationToken cancellationToken)
+        {
+            user.PhoneNumberConfirmed = confirmed;
             return Task.CompletedTask;
         }
 
