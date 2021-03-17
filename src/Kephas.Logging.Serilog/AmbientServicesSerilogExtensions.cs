a5010a190c6832659c6b1db2b7ab8579e99315ca
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-03-17 23:13:43 +0200

    Logging.Serilog: Made the SerilogManager setup configurable: the minimum level and whether dynamic log level setting should be used.

--- src/Kephas.Logging.Serilog/AmbientServicesSerilogExtensions.cs
+++ src/Kephas.Logging.Serilog/AmbientServicesSerilogExtensions.cs
@@ -11,11 +11,10 @@
 namespace Kephas
 {
     using global::Serilog;
-    using global::Serilog.Core;
-
     using Kephas.Diagnostics.Contracts;
     using Kephas.Logging;
     using Kephas.Logging.Serilog;
+    using Serilog.Events;
 
     /// <summary>
     /// Extension methods for the <see cref="IAmbientServices"/>.
@@ -27,16 +26,21 @@ namespace Kephas
         /// </summary>
         /// <param name="ambientServices">The ambient services builder.</param>
         /// <param name="configuration">Optional. The logger configuration.</param>
-        /// <param name="levelSwitch">Optional.The logging level switch.</param>
+        /// <param name="minimumLevel">Optional. The minimum logging level.</param>
+        /// <param name="dynamicMinimumLevel">
+        /// Optional. Indicates whether to allow changing the minimum level at runtime
+        /// (true) or leave it controlled by the configuration (false). If not provided (null),
+        /// a dynamic minimum level will be used only if no configuration is provided.
+        /// </param>
         /// <param name="replaceDefault">Optional. True to replace <see cref="LoggingHelper.DefaultLogManager"/>.</param>
         /// <returns>
         /// This <paramref name="ambientServices"/>.
         /// </returns>
-        public static IAmbientServices WithSerilogManager(this IAmbientServices ambientServices, LoggerConfiguration? configuration = null, LoggingLevelSwitch? levelSwitch = null, bool replaceDefault = true)
+        public static IAmbientServices WithSerilogManager(this IAmbientServices ambientServices, LoggerConfiguration? configuration = null, LogEventLevel? minimumLevel = null, bool? dynamicMinimumLevel = null, bool replaceDefault = true)
         {
             Requires.NotNull(ambientServices, nameof(ambientServices));
 
-            return ambientServices.WithLogManager(new SerilogManager(configuration, levelSwitch), replaceDefault);
+            return ambientServices.WithLogManager(new SerilogManager(configuration, minimumLevel, dynamicMinimumLevel), replaceDefault);
         }
     }
 }
--- src/Kephas.Logging.Serilog/Logging/Serilog/SerilogManager.cs
+++ src/Kephas.Logging.Serilog/Logging/Serilog/SerilogManager.cs
@@ -22,21 +22,36 @@ namespace Kephas.Logging.Serilog
     /// </summary>
     public class SerilogManager : ILogManager, IDisposable
     {
-        private readonly LoggerConfiguration configuration;
-        private readonly LoggingLevelSwitch levelSwitch;
+        private readonly LogLevel staticMinimumLevel = LogLevel.Info;
+        private readonly LoggingLevelSwitch? levelSwitch;
         private readonly ConcurrentDictionary<string, global::Kephas.Logging.ILogger> loggers = new ConcurrentDictionary<string, global::Kephas.Logging.ILogger>();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SerilogManager"/> class.
         /// </summary>
         /// <param name="configuration">Optional. The configuration.</param>
-        /// <param name="levelSwitch">Optional. The logging level switch.</param>
-        public SerilogManager(LoggerConfiguration? configuration = null, LoggingLevelSwitch? levelSwitch = null)
+        /// <param name="minimumLevel">Optional. The minimum logging level.</param>
+        /// <param name="dynamicMinimumLevel">
+        /// Optional. Indicates whether to allow changing the minimum level at runtime
+        /// (true) or leave it controlled by the configuration (false). If not provided (null),
+        /// a dynamic minimum level will be used only if no configuration is provided.
+        /// </param>
+        public SerilogManager(LoggerConfiguration? configuration = null, LogEventLevel? minimumLevel = null, bool? dynamicMinimumLevel = null)
         {
-            this.configuration = configuration ?? new LoggerConfiguration();
-            this.levelSwitch = levelSwitch ?? new LoggingLevelSwitch();
-            this.configuration.MinimumLevel.ControlledBy(this.levelSwitch);
-            this.RootLogger = this.configuration.CreateLogger();
+            var configurationNotSet = configuration == null;
+            configuration ??= new LoggerConfiguration();
+            if ((dynamicMinimumLevel == null && configurationNotSet) || (dynamicMinimumLevel.HasValue && dynamicMinimumLevel.Value))
+            {
+                this.levelSwitch = new LoggingLevelSwitch(minimumLevel ?? LogEventLevel.Information);
+                configuration.MinimumLevel.ControlledBy(this.levelSwitch);
+            }
+            else if (minimumLevel != null)
+            {
+                configuration.MinimumLevel.Is(minimumLevel.Value);
+                this.staticMinimumLevel = minimumLevel.Value.ToLogLevel();
+            }
+
+            this.RootLogger = configuration.CreateLogger();
         }
 
         /// <summary>
@@ -44,8 +59,18 @@ namespace Kephas.Logging.Serilog
         /// </summary>
         public LogLevel MinimumLevel
         {
-            get => this.levelSwitch.MinimumLevel.ToLogLevel();
-            set => this.levelSwitch.MinimumLevel = value.ToLogEventLevel();
+            get => this.levelSwitch?.MinimumLevel.ToLogLevel() ?? this.staticMinimumLevel;
+            set
+            {
+                if (this.levelSwitch != null)
+                {
+                    this.levelSwitch.MinimumLevel = value.ToLogEventLevel();
+                }
+                else
+                {
+                    this.RootLogger.Error($"To allow dynamic change of the minimum level please configure the {nameof(SerilogManager)} appropriately in the constructor.");
+                }
+            }
         }
 
         /// <summary>
