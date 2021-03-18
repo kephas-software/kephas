6caed2abed18ccbcded001f5d6674c468fe65c02
Ioan Crișan <ioan.crisan@kephas-software.com>
2021-03-18 12:02:14 +0200

    Renamed SpaServicesConfigurator to WebUIServicesConfigurator in tests.

--- src/Tests/Kephas.AspNetCore.IdentityServer4.InteractiveTests/ServicesConfigurators/WebUIServicesConfigurator.cs
+++ src/Tests/Kephas.AspNetCore.IdentityServer4.InteractiveTests/ServicesConfigurators/WebUIServicesConfigurator.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="SpaServicesConfigurator.cs" company="Kephas Software SRL">
+// <copyright file="WebUIServicesConfigurator.cs" company="Kephas Software SRL">
 //   Copyright (c) Kephas Software SRL. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -15,7 +15,7 @@ namespace Kephas.AspNetCore.IdentityServer4.InteractiveTests.ServicesConfigurato
     using Microsoft.AspNetCore.Mvc.ApplicationParts;
     using Microsoft.Extensions.DependencyInjection;
 
-    public class SpaServicesConfigurator : IServicesConfigurator
+    public class WebUIServicesConfigurator : IServicesConfigurator
     {
         public void ConfigureServices(IServiceCollection services, IAmbientServices ambientServices)
         {
