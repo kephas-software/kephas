e4585b74c7341f98f2c67cd21607b497a294bf9d
Ioan Crișan <ioan.crisan@kephas-software.com>
2021-03-23 21:56:43 +0200

    Added Support for CommandController in interactive ASP.NET Core tests (on Counter page)

--- src/Tests/Kephas.AspNetCore.IdentityServer4.InteractiveTests/ClientApp/src/app/counter/counter.component.ts
+++ src/Tests/Kephas.AspNetCore.IdentityServer4.InteractiveTests/ClientApp/src/app/counter/counter.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html'
 })
 export class CounterComponent {
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+  }
   public currentCount = 0;
 
   public incrementCounter() {
     this.currentCount++;
+  }
+
+  public async ping() {
+    try {
+      this.pingResponse = (await this.http.get<{ message: string }>(this.baseUrl + 'api/cmd/ping').toPromise()).message;
+    }
+    catch(err) {
+      this.pingResponse = err.message;
+    }
   }
 }
