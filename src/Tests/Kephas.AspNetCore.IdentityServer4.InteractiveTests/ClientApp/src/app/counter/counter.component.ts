73e1a04b53f06f0448cea8e5ac4fe58e5b23a4c7
Ioan Crișan <ioan.crisan@kephas-software.com>
2021-03-23 13:47:01 +0200

    Added tests for the command controller.

--- src/Tests/Kephas.AspNetCore.IdentityServer4.InteractiveTests/ClientApp/src/app/counter/counter.component.ts
+++ src/Tests/Kephas.AspNetCore.IdentityServer4.InteractiveTests/ClientApp/src/app/counter/counter.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,9 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './counter.component.html'
 })
 export class CounterComponent {
+  constructor(private http: HttpClient) {
+  }
   public currentCount = 0;
+  public pingResponse = 'Ping not invoked';
 
   public incrementCounter() {
     this.currentCount++;
+  }
+
+  public async ping() {
+    try {
+      this.pingResponse = (await this.http.get<{ message: string }>('api/cmd/ping').toPromise()).message;
+    }
+    catch(err) {
+      this.pingResponse = err.message;
+    }
   }
 }
