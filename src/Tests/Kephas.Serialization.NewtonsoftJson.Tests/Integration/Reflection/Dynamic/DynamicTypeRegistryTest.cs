4f6904092834b61f2a412211490fd1326ba0edb4
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-04-23 12:40:44 +0300

    Fixed deserialization of the DynamicTypeRegistry.

--- src/Tests/Kephas.Serialization.NewtonsoftJson.Tests/Integration/Reflection/Dynamic/DynamicTypeRegistryTest.cs
+++ src/Tests/Kephas.Serialization.NewtonsoftJson.Tests/Integration/Reflection/Dynamic/DynamicTypeRegistryTest.cs
@@ -7,6 +7,8 @@
 
 namespace Kephas.Serialization.Json.Tests.Integration.Reflection.Dynamic
 {
+    using System.Linq;
+
     using Kephas.IO;
     using Kephas.Reflection.Dynamic;
     using Kephas.Runtime;
@@ -19,11 +21,24 @@ namespace Kephas.Serialization.Json.Tests.Integration.Reflection.Dynamic
         public void Deserialize_Schema1()
         {
             var container = this.CreateContainer();
-            var runtimeTypeRegistry = container.GetExport<IRuntimeTypeRegistry>();
             var serializationService = this.CreateContainer().GetExport<ISerializationService>();
             var dynTypeRegistry = serializationService.JsonDeserialize<DynamicTypeRegistry>(
-                this.GetJson("Schema1.json").ReadAllString(),
-                cfg => cfg.RootObjectFactory = () => new DynamicTypeRegistry(runtimeTypeRegistry));
+                this.GetJson("Schema1.json").ReadAllString());
+
+            Assert.AreEqual("schema-1", dynTypeRegistry.Name);
+            Assert.AreEqual(2, dynTypeRegistry.Types.Count);
+
+            var customerType = dynTypeRegistry.Types.First();
+            Assert.AreEqual("Customer", customerType.Name);
+            Assert.AreEqual("Customer", customerType.FullName);
+
+            var docType = dynTypeRegistry.Types.Skip(1).First();
+            Assert.AreEqual("Document", docType.Name);
+            Assert.AreEqual("Document", docType.FullName);
+
+            var syncIdProperty = docType.Properties.Single(p => p.Name == "SyncId");
+            Assert.AreEqual(typeof(long), ((IRuntimeTypeInfo)syncIdProperty.ValueType).Type);
+            Assert.AreEqual("System.Int64", ((DynamicPropertyInfo)syncIdProperty).ValueTypeName);
         }
     }
 }
