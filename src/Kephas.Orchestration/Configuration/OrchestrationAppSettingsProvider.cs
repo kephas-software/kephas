bdd444504aadaabca57f3b73baa61c9aaed024db
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-09-12 00:39:12 +0300

    Breaking change: The OrchestrationAppSettingsProvider retrieves the AppSettings from the OrchestrationSettings.

--- src/Kephas.Orchestration/Configuration/OrchestrationAppSettingsProvider.cs
+++ src/Kephas.Orchestration/Configuration/OrchestrationAppSettingsProvider.cs
@@ -7,48 +7,67 @@
 
 namespace Kephas.Orchestration.Configuration
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
     using Kephas.Application;
     using Kephas.Application.Configuration;
     using Kephas.Collections;
     using Kephas.Configuration;
+    using Kephas.Configuration.Providers;
     using Kephas.Services;
 
     /// <summary>
-    /// The orchestration implementation of the <see cref="IAppSettingsProvider"/>
+    /// Settings provider for <see cref="AppSettings"/> retrieving them from the <see cref="OrchestrationSettings"/>
+    /// for the current application instance.
     /// </summary>
-    [OverridePriority(Priority.BelowNormal)]
-    public class OrchestrationAppSettingsProvider : IAppSettingsProvider
+    [OverridePriority(Priority.AboveNormal)]
+    [SettingsType(typeof(AppSettings))]
+    public class OrchestrationAppSettingsProvider : ISettingsProvider
     {
         private readonly IAppRuntime appRuntime;
-        private readonly IConfiguration<OrchestrationSettings> systemConfiguration;
+        private readonly Lazy<IConfiguration<OrchestrationSettings>> lazyConfiguration;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="OrchestrationAppSettingsProvider"/> class.
         /// </summary>
         /// <param name="appRuntime">The application runtime.</param>
-        /// <param name="systemConfiguration">The system configuration.</param>
-        public OrchestrationAppSettingsProvider(IAppRuntime appRuntime, IConfiguration<OrchestrationSettings> systemConfiguration)
+        /// <param name="lazyConfiguration">The lazy configuration for orchestration.</param>
+        public OrchestrationAppSettingsProvider(IAppRuntime appRuntime, Lazy<IConfiguration<OrchestrationSettings>> lazyConfiguration)
         {
             this.appRuntime = appRuntime;
-            this.systemConfiguration = systemConfiguration;
+            this.lazyConfiguration = lazyConfiguration;
         }
 
         /// <summary>
-        /// Gets the application settings for the executing instance.
+        /// Gets the settings with the provided type.
         /// </summary>
-        /// <returns>The application settings.</returns>
-        public AppSettings? GetAppSettings()
-            => this.GetAppSettings(this.appRuntime.GetAppId()!);
+        /// <param name="settingsType">Type of the settings.</param>
+        /// <param name="context">The context.</param>
+        /// <returns>
+        /// The settings.
+        /// </returns>
+        public object? GetSettings(Type settingsType, IContext? context)
+        {
+            var appId = this.appRuntime.GetAppId()!;
+            var orchestrationSettings = this.lazyConfiguration.Value.GetSettings();
+            return orchestrationSettings.Instances.TryGetValue(appId);
+        }
 
         /// <summary>
-        /// Gets the application settings for the provided application.
+        /// Updates the settings asynchronously.
         /// </summary>
-        /// <param name="appId">The application identifier.</param>
-        /// <returns>The application settings.</returns>
-        public AppSettings? GetAppSettings(string appId)
+        /// <param name="settings">The settings to be updated.</param>
+        /// <param name="context">The context.</param>
+        /// <param name="cancellationToken">Optional. The cancellation token.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task UpdateSettingsAsync(object settings, IContext? context, CancellationToken cancellationToken = default)
         {
-            var systemSettings = this.systemConfiguration.GetSettings();
-            return systemSettings.Instances?.TryGetValue(appId);
+            var appId = this.appRuntime.GetAppId()!;
+            var orchestrationSettings = this.lazyConfiguration.Value.GetSettings();
+            orchestrationSettings.Instances[appId] = (AppSettings)settings;
+            return this.lazyConfiguration.Value.UpdateSettingsAsync(orchestrationSettings, context, cancellationToken);
         }
     }
 }
