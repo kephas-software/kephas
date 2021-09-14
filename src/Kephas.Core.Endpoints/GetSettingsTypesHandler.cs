06cc1e12d557bd9c1fef7ce3d0ce61f014a2c2ca
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-09-14 12:49:57 +0300

    Added GetSettingsTypes endpoint

---
+++ src/Kephas.Core.Endpoints/GetSettingsTypesHandler.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetSettingsTypesHandler.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Core.Endpoints
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Kephas.Application;
+    using Kephas.Configuration;
+    using Kephas.Messaging;
+    using Kephas.Reflection;
+    using Kephas.Reflection.Dynamic;
+
+    /// <summary>
+    /// Handler for <see cref="GetSettingsTypesMessage"/>.
+    /// </summary>
+    public class GetSettingsTypesHandler : MessageHandlerBase<GetSettingsTypesMessage, GetSettingsTypesResponseMessage>
+    {
+        private readonly IAppRuntime appRuntime;
+        private readonly ITypeLoader typeLoader;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetSettingsTypesHandler"/> class.
+        /// </summary>
+        /// <param name="appRuntime">The application runtime.</param>
+        /// <param name="typeLoader">The type loader.</param>
+        public GetSettingsTypesHandler(IAppRuntime appRuntime, ITypeLoader typeLoader)
+        {
+            this.appRuntime = appRuntime;
+            this.typeLoader = typeLoader;
+        }
+
+        /// <summary>
+        /// Processes the provided message asynchronously and returns a response promise.
+        /// </summary>
+        /// <param name="message">The message to be handled.</param>
+        /// <param name="context">The processing context.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// The response promise.
+        /// </returns>
+        public override async Task<GetSettingsTypesResponseMessage> ProcessAsync(GetSettingsTypesMessage message, IMessagingContext context, CancellationToken token)
+        {
+            await Task.Yield();
+
+            var settingsTypes = this.appRuntime.GetAppAssemblies()
+                .SelectMany(a => this.typeLoader.GetExportedTypes(a)
+                        .Where(t => t.IsAssignableFrom(typeof(ISettings)) && !t.IsAbstract))
+                .ToList();
+
+            return new GetSettingsTypesResponseMessage
+            {
+                SettingsTypes = settingsTypes
+                    .Select(t => (ITypeInfo)new DynamicTypeInfo { Name = t.Name, Namespace = t.Namespace, FullName = t.FullName ?? t.Name })
+                    .ToArray(),
+            };
+        }
+    }
+}
---
+++ src/Kephas.Core.Endpoints/GetSettingsTypesMessage.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetSettingsTypesMessage.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Core.Endpoints
+{
+    using System;
+
+    using Kephas.ComponentModel.DataAnnotations;
+    using Kephas.Messaging;
+    using Kephas.Messaging.Messages;
+    using Kephas.Reflection;
+
+    /// <summary>
+    /// Message for getting the setting types.
+    /// </summary>
+    [DisplayInfo(Description = "Gets the registered settings types.")]
+    public class GetSettingsTypesMessage : IMessage
+    {
+    }
+
+    /// <summary>
+    /// Response to the <see cref="GetSettingsTypesMessage"/>.
+    /// </summary>
+    public class GetSettingsTypesResponseMessage : ResponseMessage
+    {
+        /// <summary>
+        /// Gets or sets the settings types.
+        /// </summary>
+        public ITypeInfo[] SettingsTypes { get; set; } = Array.Empty<ITypeInfo>();
+    }
+}
