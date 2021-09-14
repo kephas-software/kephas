c73432f8ee5e04ed78bcacb45573087f02d93e06
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-09-14 12:26:46 +0300

    Added SettingsType and SettingsTypeConstructor in Model.

---
+++ src/Kephas.Model/Elements/SettingsType.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SettingsType.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Model.Elements
+{
+    using Kephas.Composition;
+    using Kephas.Configuration.Reflection;
+    using Kephas.Diagnostics.Contracts;
+    using Kephas.Model.Construction;
+
+    /// <summary>
+    /// Classifier class for application services.
+    /// </summary>
+    public class SettingsType : ClassifierBase<ISettingsType>, ISettingsType
+    {
+        /// <summary>
+        /// The application service attribute.
+        /// </summary>
+        private readonly ISettingsInfo settingsInfo;
+
+        /// <summary>
+        /// Context for the composition.
+        /// </summary>
+        private readonly ICompositionContext compositionContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettingsType" /> class.
+        /// </summary>
+        /// <param name="constructionContext">Context for the construction.</param>
+        /// <param name="settingsInfo">The settings information.</param>
+        /// <param name="name">The name.</param>
+        public SettingsType(
+            IModelConstructionContext constructionContext,
+            ISettingsInfo settingsInfo,
+            string name)
+            : base(constructionContext, name)
+        {
+            Requires.NotNull(settingsInfo, nameof(settingsInfo));
+
+            this.settingsInfo = settingsInfo;
+            this.compositionContext = constructionContext.CompositionContext;
+        }
+    }
+}
---
+++ src/Kephas.Model/Runtime/Construction/SettingsTypeConstructor.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SettingsTypeConstructor.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Model.Runtime.Construction
+{
+    using Kephas.Configuration.Reflection;
+    using Kephas.Model.Construction;
+    using Kephas.Model.Elements;
+    using Kephas.Runtime;
+
+    /// <summary>
+    /// A constructor for <see cref="SettingsType"/>.
+    /// </summary>
+    public class SettingsTypeConstructor : ClassifierConstructorBase<SettingsType, ISettingsType>
+    {
+        /// <summary>
+        /// Determines whether a model element can be created for the provided runtime element.
+        /// </summary>
+        /// <param name="constructionContext">Context for the construction.</param>
+        /// <param name="runtimeElement">The runtime element.</param>
+        /// <returns>
+        /// <c>true</c> if a model element can be created, <c>false</c> if not.
+        /// </returns>
+        protected override bool CanCreateModelElement(IModelConstructionContext constructionContext, IRuntimeTypeInfo runtimeElement)
+        {
+            return runtimeElement is ISettingsInfo;
+        }
+
+        /// <summary>
+        /// Core implementation of trying to get the element information.
+        /// </summary>
+        /// <param name="constructionContext">Context for the construction.</param>
+        /// <param name="runtimeElement">The runtime element.</param>
+        /// <returns>
+        /// A new element information based on the provided runtime element information, or <c>null</c>
+        /// if the runtime element information is not supported.
+        /// </returns>
+        protected override SettingsType? TryCreateModelElementCore(IModelConstructionContext constructionContext, IRuntimeTypeInfo runtimeElement)
+        {
+            return new SettingsType(constructionContext, (ISettingsInfo)runtimeElement, this.TryComputeName(runtimeElement, constructionContext)!);
+        }
+
+        /// <summary>
+        /// Computes the model element name based on the runtime element. The "Settings" ending is stripped away.
+        /// </summary>
+        /// <param name="runtimeElement">The runtime element.</param>
+        /// <param name="constructionContext">The construction context.</param>
+        /// <returns>The element name, or <c>null</c> if the name could not be computed.</returns>
+        protected override string? TryComputeNameCore(object runtimeElement, IModelConstructionContext constructionContext)
+        {
+            const string ending = "Settings";
+            var runtimeType = (ISettingsInfo)runtimeElement;
+            var name = runtimeType.Name;
+            if (name.EndsWith(ending) && name != ending)
+            {
+                return name[ending.Length..];
+            }
+
+            return name;
+        }
+    }
+}
