76b54f948afe74d0a1311af95ccbac22de9bfa18
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-09-14 13:40:43 +0300

    Added PluginsSettings with EnablePlugins option.

---
+++ src/Kephas.Plugins/Plugins/Configuration/PluginsSettings.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PluginsSettings.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Plugins.Configuration
+{
+    using Kephas.Configuration;
+    using Kephas.Dynamic;
+
+    /// <summary>
+    /// Settings for plugins.
+    /// </summary>
+    public class PluginsSettings : Expando, ISettings
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the plugins are enabled.
+        /// </summary>
+        public bool EnablePlugins { get; set; } = true;
+    }
+}
