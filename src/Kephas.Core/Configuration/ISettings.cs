e22d2c2b407182abbdab9000fdbf39dbbfdfaeb2
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-09-14 12:22:35 +0300

    Added ISettings as marker interface for all settings.

---
+++ src/Kephas.Core/Configuration/ISettings.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ISettings.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Configuration
+{
+    /// <summary>
+    /// Marker interface for settings.
+    /// </summary>
+    public interface ISettings
+    {
+    }
+}
