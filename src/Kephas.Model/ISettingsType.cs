30f0b81593482c58e2b7b5b50da49446324467d1
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-09-14 12:28:02 +0300

    Added ISettingsType to Model

---
+++ src/Kephas.Model/ISettingsType.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ISettingsType.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Model
+{
+    using Kephas.Configuration.Reflection;
+
+    /// <summary>
+    /// Interface for settings.
+    /// </summary>
+    public interface ISettingsType : ISettingsInfo, IClassifier
+    {
+    }
+}
