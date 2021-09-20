3c5e728c69a3fc8ae4e719d4c7f4c08bb5404d84
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-09-20 20:49:46 +0300

    Moved the default configurators to the test asp net apps

---
+++ src/Tests/Kephas.AspNetCore.IdentityServer4.InteractiveTests/ServicesConfigurators/HttpContextAccessorServicesConfigurator.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HttpContextAccessorServicesConfigurator.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.AspNetCore.InteractiveTests.ServicesConfigurators
+{
+    using Kephas.Services;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+
+    /// <summary>
+    /// Services configurator
+    /// </summary>
+    [ProcessingPriority(ProcessingPriority)]
+    public class HttpContextAccessorServicesConfigurator : IServicesConfigurator
+    {
+        /// <summary>
+        /// The processing priority of <see cref="HttpContextAccessorServicesConfigurator"/>.
+        /// </summary>
+        public const Priority ProcessingPriority = Priority.Lowest;
+
+        /// <summary>
+        /// Configure the services.
+        /// </summary>
+        /// <param name="services">The services to configure.</param>
+        /// <param name="ambientServices">The ambient services.</param>
+        public void ConfigureServices(IServiceCollection services, IAmbientServices ambientServices)
+        {
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+    }
+}
