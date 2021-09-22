d1ff32d6031ce933c294ffbf7e6bd9ad8961b51a
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-09-22 18:42:42 +0300

    Adusted tests

--- src/Tests/Kephas.Core.Tests/Injection/Conventions/ConventionsBuilderExtensionsTest.cs
+++ src/Tests/Kephas.Core.Tests/Injection/Conventions/ConventionsBuilderExtensionsTest.cs
@@ -8,16 +8,18 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Kephas.Injection.Conventions;
-using Kephas.Injection.Hosting;
-using Kephas.Injection.Lite;
-using NUnit.Framework;
-
 namespace Kephas.Core.Tests.Injection.Conventions
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Kephas.Injection.Conventions;
+    using Kephas.Injection.Hosting;
+    using Kephas.Services;
+    using Kephas.Services.Reflection;
+    using NUnit.Framework;
+
     /// <summary>
     /// The conventions builder extensions test.
     /// </summary>
@@ -29,33 +31,7 @@ namespace Kephas.Core.Tests.Injection.Conventions
         {
             var builder = new InjectorBuilderBaseTest.TestConventionsBuilder();
             var newBuilder = builder.RegisterConventions(
-                new[] { typeof(ScientificCalculator), typeof(StandardCalculator) },
-                new TestBuildContext(new AmbientServices().Register<IConventionsRegistrar>(b => b.WithType<CalculatorConventionsRegistrar>().AllowMultiple())));
-
-            Assert.AreSame(builder, newBuilder);
-            Assert.AreEqual(1, builder.DerivedConventionsBuilders.Count);
-            var partBuilder = (InjectorBuilderBaseTest.TestPartConventionsBuilder)builder.DerivedConventionsBuilders.First().Value;
-            Assert.IsTrue(partBuilder.IsSingleton);
-            Assert.AreEqual(typeof(ICalculator), partBuilder.Type);
-            var exportBuilder = partBuilder.ExportBuilder;
-            Assert.AreEqual(typeof(ICalculator), exportBuilder.ContractType);
-            Assert.AreEqual(1, exportBuilder.Metadata.Count);
-            Assert.AreEqual("type", exportBuilder.Metadata.Keys.First());
-            var metadataExtractor = (Func<Type, object>)exportBuilder.Metadata.Values.First();
-            Assert.AreEqual("Scientific", metadataExtractor(typeof(ScientificCalculator)));
-            Assert.AreEqual("Classical", metadataExtractor(typeof(StandardCalculator)));
-        }
-
-        [Test]
-        public void RegisterConventions_singleton_derived_conventions_with_metadata_from_context_registrar()
-        {
-            var builder = new InjectorBuilderBaseTest.TestConventionsBuilder();
-            var newBuilder = builder.RegisterConventions(
-                new[] { typeof(ScientificCalculator), typeof(StandardCalculator) },
-                new TestBuildContext
-                {
-                    Registrars = new IConventionsRegistrar[] { new CalculatorConventionsRegistrar() }
-                });
+                new TestBuildContext(new AmbientServices()).WithAppServiceInfosProvider(new CalculatorAppServiceInfosProvider()));
 
             Assert.AreSame(builder, newBuilder);
             Assert.AreEqual(1, builder.DerivedConventionsBuilders.Count);
@@ -76,19 +52,19 @@ namespace Kephas.Core.Tests.Injection.Conventions
     {
     }
 
-    public class CalculatorConventionsRegistrar : IConventionsRegistrar
+    public class CalculatorAppServiceInfosProvider : IAppServiceInfosProvider
     {
-        public void RegisterConventions(
-            IConventionsBuilder builder,
-            IList<Type> candidateTypes,
-            IInjectionBuildContext buildContext)
+        public IEnumerable<(Type contractDeclarationType, IAppServiceInfo appServiceInfo)> GetAppServiceInfos(dynamic? context = null)
         {
-            builder
-                .ForTypesDerivedFrom(typeof(ICalculator))
-                .Export(
-                    b => b.AsContractType(typeof(ICalculator))
-                          .AddMetadata("type", t => t.Name.StartsWith("Scientific") ? "Scientific" : "Classical"))
-                .Singleton();
+            yield return (
+                typeof(ICalculator),
+                new AppServiceInfo(typeof(ICalculator), typeof(ScientificCalculator))
+                    .AddMetadata("type", "Scientific"));
+
+            yield return (
+                typeof(ICalculator),
+                new AppServiceInfo(typeof(ICalculator), typeof(StandardCalculator))
+                    .AddMetadata("type", "Standard"));
         }
     }
 
