62f8643b5751446dc1ccfe850c68aed526d4eb87
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-09-22 17:21:53 +0300

    Breaking change: Parts will be removed from injection build, instead, for backward compatibility, a PartsAppServiceInfosProvider is used.

--- src/Kephas.Core/Injection/Conventions/ConventionsBuilderExtensions.cs
+++ src/Kephas.Core/Injection/Conventions/ConventionsBuilderExtensions.cs
@@ -8,68 +8,37 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using Kephas.Collections;
-using Kephas.Diagnostics.Contracts;
-using Kephas.Injection.Hosting;
-using Kephas.Logging;
-
 namespace Kephas.Injection.Conventions
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Kephas.Diagnostics.Contracts;
+    using Kephas.Injection.Hosting;
+    using Kephas.Logging;
+
     /// <summary>
     /// Extension methods for <see cref="IConventionsBuilder"/>.
     /// </summary>
     public static class ConventionsBuilderExtensions
     {
         /// <summary>
-        /// Defines a registration for the specified type and its singleton instance.
-        /// </summary>
-        /// <typeparam name="TService">Type of the registered service.</typeparam>
-        /// <param name="conventionsBuilder">The conventionsBuilder to act on.</param>
-        /// <param name="instance">The singleton instance.</param>
-        public static void ForInstance<TService>(this IConventionsBuilder conventionsBuilder, TService instance)
-        {
-            Requires.NotNull(conventionsBuilder, nameof(conventionsBuilder));
-
-            conventionsBuilder.ForInstance(typeof(TService), instance);
-        }
-
-        /// <summary>
-        /// Defines a registration for the specified type and its instance factory.
-        /// </summary>
-        /// <typeparam name="TService">Type of the registered service.</typeparam>
-        /// <param name="conventionsBuilder">The conventionsBuilder to act on.</param>
-        /// <param name="factory">The instance factory.</param>
-        /// <returns>
-        /// A <see cref="IPartBuilder"/> to further configure the rule.
-        /// </returns>
-        public static IPartBuilder ForInstanceFactory<TService>(this IConventionsBuilder conventionsBuilder, Func<IInjector, TService> factory)
-        {
-            Requires.NotNull(conventionsBuilder, nameof(conventionsBuilder));
-
-            return conventionsBuilder.ForInstanceFactory(typeof(TService), c => factory(c));
-        }
-
-        /// <summary>
         /// Adds the conventions from the provided types implementing
         /// <see cref="IConventionsRegistrar" />.
         /// </summary>
         /// <param name="builder">The builder.</param>
-        /// <param name="parts">The parts.</param>
         /// <param name="buildContext">Context for the registration.</param>
         /// <returns>
         /// The convention builder.
         /// </returns>
-        public static IConventionsBuilder RegisterConventions(this IConventionsBuilder builder, IList<Type> parts, IInjectionBuildContext buildContext)
+        public static IConventionsBuilder RegisterConventions(this IConventionsBuilder builder, IInjectionBuildContext buildContext)
         {
             Requires.NotNull(builder, nameof(builder));
-            Requires.NotNull(parts, nameof(parts));
             Requires.NotNull(buildContext, nameof(buildContext));
 
-            return RegisterConventionsCore(builder, parts, buildContext);
+            return RegisterConventionsCore(builder, buildContext);
         }
 
         /// <summary>
@@ -78,19 +47,17 @@ namespace Kephas.Injection.Conventions
         /// </summary>
         /// <param name="builder">The builder.</param>
         /// <param name="assemblies">The assemblies.</param>
-        /// <param name="parts">The parts.</param>
         /// <param name="buildContext">Context for the registration.</param>
         /// <returns>
         /// The convention builder.
         /// </returns>
-        public static IConventionsBuilder RegisterConventionsFrom(this IConventionsBuilder builder, IEnumerable<Assembly> assemblies, IList<Type> parts, IInjectionBuildContext buildContext)
+        public static IConventionsBuilder RegisterConventionsFrom(this IConventionsBuilder builder, IEnumerable<Assembly> assemblies, IInjectionBuildContext buildContext)
         {
             Requires.NotNull(builder, nameof(builder));
             Requires.NotNull(assemblies, nameof(assemblies));
-            Requires.NotNull(parts, nameof(parts));
             Requires.NotNull(buildContext, nameof(buildContext));
 
-            return RegisterConventionsCore(builder, parts, buildContext);
+            return RegisterConventionsCore(builder, buildContext);
         }
 
         /// <summary>
@@ -110,26 +77,16 @@ namespace Kephas.Injection.Conventions
         /// Gets the registration builder.
         /// </summary>
         /// <param name="builder">The builder.</param>
-        /// <param name="parts">The convention types.</param>
         /// <param name="buildContext">The registration context.</param>
         /// <returns>
         /// The registration builder.
         /// </returns>
-        private static IConventionsBuilder RegisterConventionsCore(this IConventionsBuilder builder, IList<Type> parts, IInjectionBuildContext buildContext)
+        private static IConventionsBuilder RegisterConventionsCore(this IConventionsBuilder builder, IInjectionBuildContext buildContext)
         {
             Requires.NotNull(builder, nameof(builder));
 
-            if (buildContext.Parts != null)
-            {
-                parts.AddRange(buildContext.Parts.Where(IsPartCandidate));
-            }
-
             var conventionRegistrars = buildContext.AmbientServices.GetService<IEnumerable<IConventionsRegistrar>>();
             var registrars = conventionRegistrars?.ToList() ?? new List<IConventionsRegistrar>();
-            if (buildContext.Registrars != null)
-            {
-                registrars.AddRange(buildContext.Registrars);
-            }
 
             var logger = typeof(ConventionsBuilderExtensions).GetLogger(buildContext);
 
@@ -141,7 +98,7 @@ namespace Kephas.Injection.Conventions
                     logger.Debug("Registering conventions from '{conventionsRegistrar}...", registrar.GetType());
                 }
 
-                registrar.RegisterConventions(builder, parts, buildContext);
+                registrar.RegisterConventions(builder, buildContext);
             }
 
             return builder;
---
+++ src/Kephas.Core/Injection/Hosting/PartsAppServiceInfosProvider.cs
@@ -0,0 +1,90 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PartsAppServiceInfosProvider.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Injection.Hosting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Kephas.Injection.AttributedModel;
+    using Kephas.Services;
+
+    /// <summary>
+    /// Implementation of <see cref="IAppServiceInfosProvider"/> for an enumeration of types.
+    /// </summary>
+    internal class PartsAppServiceInfosProvider : IAppServiceInfosProvider, IAppServiceTypesProvider
+    {
+        private readonly IList<Type> parts;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PartsAppServiceInfosProvider"/> class.
+        /// </summary>
+        /// <param name="parts">The parts.</param>
+        public PartsAppServiceInfosProvider(IEnumerable<Type> parts)
+        {
+            this.parts = parts.ToList();
+        }
+
+        /// <summary>
+        /// Gets the contract declaration types.
+        /// </summary>
+        /// <param name="context">Optional. The context in which the service types are requested.</param>
+        /// <returns>
+        /// The contract declaration types.
+        /// </returns>
+        public IEnumerable<Type>? GetContractDeclarationTypes(dynamic? context = null) => this.parts;
+
+        /// <summary>
+        /// Gets an enumeration of tuples containing the service type and the contract declaration type which it implements.
+        /// </summary>
+        /// <param name="context">Optional. The context in which the service types are requested.</param>
+        /// <returns>
+        /// An enumeration of tuples containing the service type and the contract declaration type which it implements.
+        /// </returns>
+        public IEnumerable<(Type serviceType, Type contractDeclarationType)> GetAppServiceTypes(dynamic? context = null)
+        {
+            return
+                from part in this.parts
+                where part.IsClass && !part.IsAbstract && part.GetCustomAttribute<ExcludeFromInjectionAttribute>() == null
+                let contractType = this.TryGetAppServiceContract(part)
+                where contractType != null
+                select (part, contractType!);
+        }
+
+        private Type? TryGetAppServiceContract(Type part)
+        {
+            if (this.IsAppServiceContract(part))
+            {
+                return part;
+            }
+
+            var contract = part.GetInterfaces().FirstOrDefault(this.IsAppServiceContract);
+            if (contract != null)
+            {
+                return contract;
+            }
+
+            var baseType = part.BaseType;
+            while (baseType != null)
+            {
+                if (this.IsAppServiceContract(baseType))
+                {
+                    return baseType;
+                }
+
+                baseType = baseType.BaseType;
+            }
+
+            return null;
+        }
+
+        private bool IsAppServiceContract(Type type) =>
+            ((IAppServiceInfosProvider)this).TryGetAppServiceInfo(type) != null;
+    }
+}
