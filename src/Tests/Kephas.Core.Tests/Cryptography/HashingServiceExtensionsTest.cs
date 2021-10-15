589426dd928793aa4943130c7756a1a8cb339c92
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-10-15 16:50:56 +0300

    Fix HashingServiceExtensionsTest.

--- src/Tests/Kephas.Core.Tests/Cryptography/HashingServiceExtensionsTest.cs
+++ src/Tests/Kephas.Core.Tests/Cryptography/HashingServiceExtensionsTest.cs
@@ -25,15 +25,15 @@ namespace Kephas.Core.Tests.Cryptography
         [Test]
         public void Hash_string()
         {
-            var hashingService = Substitute.For<IHashingService>();
             var valueString = "123";
             Action<IHashingContext> optionsConfig = ctx => { };
             var hashBytes = Encoding.UTF8.GetBytes("8910");
-            hashingService.Hash(Arg.Any<byte[]>(), Arg.Any<Action<IHashingContext>>()).Returns(ci =>
-                {
-                    var value = Encoding.UTF8.GetString(ci.ArgAt<byte[]>(0));
-                    return value == valueString ? hashBytes : null;
-                });
+
+            IHashingService hashingService = new TestHashingService((value, options) =>
+            {
+                var actualStringValue = Encoding.UTF8.GetString(value);
+                return actualStringValue == valueString ? hashBytes : null;
+            });
 
             var hash = hashingService.Hash(valueString, optionsConfig);
             var hashString = Encoding.UTF8.GetString(hash);
@@ -43,22 +43,35 @@ namespace Kephas.Core.Tests.Cryptography
         [Test]
         public void Hash_string_salt()
         {
-            var hashingService = Substitute.For<IHashingService>();
             var valueString = "123";
             var salt = new byte[] { 1, 2, 3 };
             var hashBytes = Encoding.UTF8.GetBytes("8910");
-            hashingService.Hash(Arg.Any<byte[]>(), Arg.Any<Action<IHashingContext>>()).Returns(ci =>
+
+            IHashingService hashingService = new TestHashingService((value, options) =>
             {
                 var ctx = Substitute.For<IHashingContext>();
-                ci.Arg<Action<IHashingContext>>()(ctx);
+                options?.Invoke(ctx);
                 ctx.Received(1).Salt = salt;
-                var value = Encoding.UTF8.GetString(ci.ArgAt<byte[]>(0));
-                return value == valueString ? hashBytes : null;
+                var actualValue = Encoding.UTF8.GetString(value);
+                return actualValue == valueString ? hashBytes : Array.Empty<byte>();
             });
 
             var hash = hashingService.Hash(valueString, salt);
             var hashString = Encoding.UTF8.GetString(hash);
             Assert.AreEqual("8910", hashString);
+        }
+
+        private class TestHashingService : IHashingService
+        {
+            private readonly Func<byte[], Action<IHashingContext>?, byte[]> callback;
+
+            public TestHashingService(Func<byte[], Action<IHashingContext>?, byte[]> callback)
+            {
+                this.callback = callback;
+            }
+
+            public byte[] Hash(byte[] value, Action<IHashingContext>? optionsConfig = null)
+                => this.callback(value, optionsConfig);
         }
     }
 }
