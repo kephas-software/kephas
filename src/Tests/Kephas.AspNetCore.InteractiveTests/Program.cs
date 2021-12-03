487fcc5019206e77fd5e94c0b9d7f2a70ce76d21
Ioan Crișan <ioan.crisan@kephas-software.com>
2021-12-03 11:24:15 +0200

    Fixed the AspNetCore interactive tests (NET 5)

--- src/Tests/Kephas.AspNetCore.InteractiveTests/Program.cs
+++ src/Tests/Kephas.AspNetCore.InteractiveTests/Program.cs
@@ -5,39 +5,12 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System;
-
 using Kephas;
 using Kephas.Application;
 using Kephas.AspNetCore.InteractiveTests;
 using Kephas.AspNetCore.InteractiveTests.Extensions;
 using Kephas.Cryptography;
 using Kephas.Extensions.Hosting;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-
-#if NET6_0_OR_GREATER
-
-var appArgs = new AppArgs(args);
-var ambientServices = new AmbientServices();
-await new WebApp(
-        new AppArgs(args),
-        builder =>
-        {
-            builder.Host
-                .ConfigureAmbientServices(
-                    ambientServices,
-                    args,
-                    ambient => ambient.BuildWithAutofac(),
-                    (services, ambient) =>
-                        ambient.SetupAmbientServices(
-                            CreateEncryptionService,
-                            services.TryGetStartupService<IConfiguration>()));
-        })
-    .RunAsync();
-
-#else
 
 await CreateHostBuilder(args).Build().RunAsync();
 
@@ -57,8 +30,6 @@ static IHostBuilder CreateHostBuilder(string[] args)
                 .UseStartup<Startup>()
                 .UseKestrel());
 }
-
-#endif
 
 static IEncryptionService CreateEncryptionService(IAmbientServices ambientServices)
 {
