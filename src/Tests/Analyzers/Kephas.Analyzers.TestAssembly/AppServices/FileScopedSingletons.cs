a916d6b4952cd61ca9c074dfbfb4a7ff3a34ccb3
Ioan Crisan <ioan.crisan@kephas-software.com>
2022-01-12 12:36:03 +0200

    try fix the generation of app services when file scoped namespaces are present

---
+++ src/Tests/Analyzers/Kephas.Analyzers.TestAssembly/AppServices/FileScopedSingletons.cs
@@ -0,0 +1,12 @@
+﻿using Kephas.Services;
+
+namespace Kephas.Analyzers.TestAssembly;
+
+[SingletonAppServiceContract]
+public interface IFileScopedNsSingletonServiceContract
+{
+}
+
+public class FileScopedNsSingletonService : IFileScopedNsSingletonServiceContract
+{
+}
