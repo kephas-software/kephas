55f5fe1548a46be6a6a2390750c7cf8f25d9891c
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-02-17 19:50:56 +0200

    NEW: Added MailKitImapConnection and ConnectionFactory.

---
+++ src/Kephas.Mail.MailKit/Connectivity/MailKitImapConnectionFactory.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MailKitImapConnection.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Kephas.Connectivity;
+using Kephas.Connectivity.AttributedModel;
+using Kephas.Cryptography;
+
+namespace Kephas.Mail.Connectivity;
+
+/// <summary>
+/// Connection factory for <see cref="MailKitImapConnection"/>.
+/// </summary>
+/// <seealso cref="IConnectionFactory" />
+[ConnectionKind("imap")]
+public class MailKitImapConnectionFactory : IConnectionFactory
+{
+    private readonly IEncryptionService encryptionService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MailKitImapConnectionFactory"/> class.
+    /// </summary>
+    /// <param name="encryptionService">The encryption service.</param>
+    public MailKitImapConnectionFactory(IEncryptionService encryptionService)
+    {
+        this.encryptionService = encryptionService ?? throw new ArgumentNullException(nameof(encryptionService));
+    }
+
+    /// <summary>
+    /// Creates the connection configured through the connection context.
+    /// </summary>
+    /// <param name="context">The connection creation context.</param>
+    /// <returns>
+    /// The newly created connection.
+    /// </returns>
+    public IConnection CreateConnection(IConnectionContext context)
+    {
+        return new MailKitImapConnection(this.encryptionService);
+    }
+}
