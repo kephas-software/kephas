bb63e377f4fba4b503b7a5f5254629393cfa916a
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-02-18 09:44:11 +0200

    Soft breaking change: Moved Priority, IHasOverridePriority and IHasProcessingPriority to Abstractions

--- src/Kephas.Injection/Services/AppServicesAttribute.cs
+++ src/Kephas.Injection/Services/AppServicesAttribute.cs
@@ -5,145 +5,144 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace Kephas.Services
+namespace Kephas.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+using Kephas.Logging;
+
+/// <summary>
+/// Assembly attribute decorating an assembly and collecting the application services, both contract types and implementation types.
+/// </summary>
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+public sealed class AppServicesAttribute : Attribute, IAppServiceInfosProvider, IHasProcessingPriority
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Linq;
-    using Kephas.Logging;
-    using Kephas.Services.Reflection;
+    private readonly Lazy<ILogger> lazyLogger = new(() => LoggingHelper.DefaultLogManager.GetLogger(typeof(AppServicesAttribute)));
 
     /// <summary>
-    /// Assembly attribute decorating an assembly and collecting the application services, both contract types and implementation types.
+    /// Initializes a new instance of the <see cref="AppServicesAttribute"/> class.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
-    public sealed class AppServicesAttribute : Attribute, IAppServiceInfosProvider, IHasProcessingPriority
+    /// <param name="providerType">
+    /// The type providing the application services.
+    /// Should implement the <see cref="IAppServiceInfosProvider"/> interface.
+    /// </param>
+    /// <param name="processingPriority">Adds the processing priority in which the attributes will be processed.</param>
+    public AppServicesAttribute([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] Type providerType, Priority processingPriority = Priority.Normal)
     {
-        private readonly Lazy<ILogger> lazyLogger = new (() => LoggingHelper.DefaultLogManager.GetLogger(typeof(AppServicesAttribute)));
+        this.ProviderType = providerType ?? throw new ArgumentNullException(nameof(providerType));
+        this.ProcessingPriority = processingPriority;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AppServicesAttribute"/> class.
-        /// </summary>
-        /// <param name="providerType">
-        /// The type providing the application services.
-        /// Should implement the <see cref="IAppServiceInfosProvider"/> interface.
-        /// </param>
-        /// <param name="processingPriority">Adds the processing priority in which the attributes will be processed.</param>
-        public AppServicesAttribute([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] Type providerType, Priority processingPriority = Priority.Normal)
+        if (!typeof(IAppServiceInfosProvider).IsAssignableFrom(this.ProviderType))
         {
-            this.ProviderType = providerType ?? throw new ArgumentNullException(nameof(providerType));
-            this.ProcessingPriority = processingPriority;
+            throw new ArgumentException($"The provider type must implement {nameof(IAppServiceInfosProvider)}", nameof(providerType));
+        }
+    }
 
-            if (!typeof(IAppServiceInfosProvider).IsAssignableFrom(this.ProviderType))
-            {
-                throw new ArgumentException($"The provider type must implement {nameof(IAppServiceInfosProvider)}", nameof(providerType));
-            }
+    /// <summary>
+    /// Gets the provider type.
+    /// </summary>
+    public Type ProviderType { get; }
+
+    /// <summary>
+    /// Gets the processing priority.
+    /// </summary>
+    public Priority ProcessingPriority { get; }
+
+    /// <summary>
+    /// Gets the logger.
+    /// </summary>
+    private ILogger Logger => this.lazyLogger.Value;
+
+    /// <summary>
+    /// Gets an enumeration of application service information objects and their contract declaration type.
+    /// The contract declaration type is the type declaring the contract: if the <see cref="AppServiceContractAttribute.ContractType"/>
+    /// is not provided, the contract declaration type is also the contract type.
+    /// </summary>
+    /// <param name="context">Optional. The context in which the service types are requested.</param>
+    /// <returns>
+    /// An enumeration of application service information objects and their contract declaration type.
+    /// </returns>
+    public IEnumerable<ContractDeclaration> GetAppServiceContracts(IContext? context = null)
+    {
+        if (this.Logger.IsTraceEnabled())
+        {
+            this.Logger.Trace("Creating instance of {providerType} in {operation}...", this.ProviderType, nameof(this.GetAppServiceContracts));
         }
 
-        /// <summary>
-        /// Gets the provider type.
-        /// </summary>
-        public Type ProviderType { get; }
-
-        /// <summary>
-        /// Gets the processing priority.
-        /// </summary>
-        public Priority ProcessingPriority { get; }
-
-        /// <summary>
-        /// Gets the logger.
-        /// </summary>
-        private ILogger Logger => this.lazyLogger.Value;
-
-        /// <summary>
-        /// Gets an enumeration of application service information objects and their contract declaration type.
-        /// The contract declaration type is the type declaring the contract: if the <see cref="AppServiceContractAttribute.ContractType"/>
-        /// is not provided, the contract declaration type is also the contract type.
-        /// </summary>
-        /// <param name="context">Optional. The context in which the service types are requested.</param>
-        /// <returns>
-        /// An enumeration of application service information objects and their contract declaration type.
-        /// </returns>
-        public IEnumerable<ContractDeclaration> GetAppServiceContracts(IContext? context = null)
+        if (Activator.CreateInstance(this.ProviderType) is not IAppServiceInfosProvider provider)
         {
-            if (this.Logger.IsTraceEnabled())
-            {
-                this.Logger.Trace("Creating instance of {providerType} in {operation}...", this.ProviderType, nameof(this.GetAppServiceContracts));
-            }
-
-            if (Activator.CreateInstance(this.ProviderType) is not IAppServiceInfosProvider provider)
-            {
-                this.Logger.Warn($"Instance of {{providerType}} cannot be converted to {nameof(IAppServiceInfosProvider)}.", this.ProviderType);
-                return Enumerable.Empty<ContractDeclaration>();
-            }
-
-            if (this.Logger.IsTraceEnabled())
-            {
-                this.Logger.Trace("Instance {provider} of {providerType} created successfully in {operation}.", provider, this.ProviderType, nameof(this.GetAppServiceContracts));
-            }
-
-            try
-            {
-                var contracts = provider.GetAppServiceContracts(context) ?? Array.Empty<ContractDeclaration>();
-                return contracts.ToArray();
-            }
-            catch (Exception ex)
-            {
-                this.Logger.Error(ex, "Error while getting the service contracts from {provider}.", provider);
-                return Enumerable.Empty<ContractDeclaration>();
-            }
+            this.Logger.Warn($"Instance of {{providerType}} cannot be converted to {nameof(IAppServiceInfosProvider)}.", this.ProviderType);
+            return Enumerable.Empty<ContractDeclaration>();
         }
 
-        /// <summary>
-        /// Gets an enumeration of tuples containing the service type and the contract declaration type which it implements.
-        /// </summary>
-        /// <param name="context">Optional. The context in which the contracts are requested.</param>
-        /// <returns>
-        /// An enumeration of tuples containing the service type and the contract declaration type which it implements.
-        /// </returns>
-        public IEnumerable<ServiceDeclaration> GetAppServices(IContext? context = null)
+        if (this.Logger.IsTraceEnabled())
         {
-            if (this.Logger.IsTraceEnabled())
-            {
-                this.Logger.Trace("Creating instance of {providerType} in {operation}...", this.ProviderType, nameof(this.GetAppServices));
-            }
-
-            if (Activator.CreateInstance(this.ProviderType) is not IAppServiceInfosProvider provider)
-            {
-                this.Logger.Warn($"Instance of {{providerType}} cannot be converted to {nameof(IAppServiceInfosProvider)}.", this.ProviderType);
-                return Enumerable.Empty<ServiceDeclaration>();
-            }
-
-            if (this.Logger.IsTraceEnabled())
-            {
-                this.Logger.Trace("Instance {provider} of {providerType} created successfully in {operation}.", provider, this.ProviderType, nameof(this.GetAppServices));
-            }
-
-            try
-            {
-                var services = provider.GetAppServices(context) ?? Enumerable.Empty<ServiceDeclaration>();
-                if (this.Logger.IsTraceEnabled() && !services.Any())
-                {
-                    this.Logger.Trace("Instance {provider} did not provide any services in {operation}.", provider, nameof(this.GetAppServices));
-                }
-
-                return services.ToArray();
-            }
-            catch (Exception ex)
-            {
-                this.Logger.Error(ex, "Error while getting the service declarations from {provider}.", provider);
-                return Enumerable.Empty<ServiceDeclaration>();
-            }
+            this.Logger.Trace("Instance {provider} of {providerType} created successfully in {operation}.", provider, this.ProviderType, nameof(this.GetAppServiceContracts));
         }
 
-
-        /// <summary>Returns a string that represents the current object.</summary>
-        /// <returns>A string that represents the current object.</returns>
-        /// <footer><a href="https://docs.microsoft.com/en-us/dotnet/api/System.Object.ToString?view=netstandard-2.1">`Object.ToString` on docs.microsoft.com</a></footer>
-        public override string ToString()
+        try
         {
-            return $"AppServices/{this.ProviderType}/{this.ProcessingPriority}";
+            var contracts = provider.GetAppServiceContracts(context) ?? Array.Empty<ContractDeclaration>();
+            return contracts.ToArray();
         }
+        catch (Exception ex)
+        {
+            this.Logger.Error(ex, "Error while getting the service contracts from {provider}.", provider);
+            return Enumerable.Empty<ContractDeclaration>();
+        }
+    }
+
+    /// <summary>
+    /// Gets an enumeration of tuples containing the service type and the contract declaration type which it implements.
+    /// </summary>
+    /// <param name="context">Optional. The context in which the contracts are requested.</param>
+    /// <returns>
+    /// An enumeration of tuples containing the service type and the contract declaration type which it implements.
+    /// </returns>
+    public IEnumerable<ServiceDeclaration> GetAppServices(IContext? context = null)
+    {
+        if (this.Logger.IsTraceEnabled())
+        {
+            this.Logger.Trace("Creating instance of {providerType} in {operation}...", this.ProviderType, nameof(this.GetAppServices));
+        }
+
+        if (Activator.CreateInstance(this.ProviderType) is not IAppServiceInfosProvider provider)
+        {
+            this.Logger.Warn($"Instance of {{providerType}} cannot be converted to {nameof(IAppServiceInfosProvider)}.", this.ProviderType);
+            return Enumerable.Empty<ServiceDeclaration>();
+        }
+
+        if (this.Logger.IsTraceEnabled())
+        {
+            this.Logger.Trace("Instance {provider} of {providerType} created successfully in {operation}.", provider, this.ProviderType, nameof(this.GetAppServices));
+        }
+
+        try
+        {
+            var services = provider.GetAppServices(context) ?? Enumerable.Empty<ServiceDeclaration>();
+            if (this.Logger.IsTraceEnabled() && !services.Any())
+            {
+                this.Logger.Trace("Instance {provider} did not provide any services in {operation}.", provider, nameof(this.GetAppServices));
+            }
+
+            return services.ToArray();
+        }
+        catch (Exception ex)
+        {
+            this.Logger.Error(ex, "Error while getting the service declarations from {provider}.", provider);
+            return Enumerable.Empty<ServiceDeclaration>();
+        }
+    }
+
+
+    /// <summary>Returns a string that represents the current object.</summary>
+    /// <returns>A string that represents the current object.</returns>
+    /// <footer><a href="https://docs.microsoft.com/en-us/dotnet/api/System.Object.ToString?view=netstandard-2.1">`Object.ToString` on docs.microsoft.com</a></footer>
+    public override string ToString()
+    {
+        return $"AppServices/{this.ProviderType}/{this.ProcessingPriority}";
     }
 }
