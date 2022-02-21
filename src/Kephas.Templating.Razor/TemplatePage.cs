ad620a7a8afdbb8a7ad0aaecc6ceab6ea6c21ce9
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-02-21 15:16:22 +0200

    Added the template page.

---
+++ src/Kephas.Templating.Razor/TemplatePage.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TemplatePage.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Templating.Razor
+{
+    using Microsoft.AspNetCore.Mvc.Razor;
+
+    /// <summary>
+    /// The base template page class.
+    /// </summary>
+    /// <typeparam name="T">The model type.</typeparam>
+    public abstract class TemplatePage<T> : RazorPage<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TemplatePage{T}"/> class.
+        /// </summary>
+        protected TemplatePage()
+        {
+            this.HtmlEncoder = System.Text.Encodings.Web.HtmlEncoder.Default;
+        }
+    }
+}
