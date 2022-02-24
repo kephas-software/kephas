e67394723454b0d0e66807c2ec3ba99ba8fa2d36
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-02-24 12:47:26 +0200

    Made scripting behaviors instance based, not singleton.

--- src/Kephas.Scripting/IScriptingBehavior.cs
+++ src/Kephas.Scripting/IScriptingBehavior.cs
@@ -13,37 +13,35 @@ namespace Kephas.Scripting
     using System.Threading;
     using System.Threading.Tasks;
 
-    using Kephas.Scripting.AttributedModel;
     using Kephas.Services;
 
     /// <summary>
-    /// Singleton application service contract responsible for adding behavior to script execution for a specified language.
+    /// Contract for application services responsible for adding behavior to script execution for a specified language.
     /// </summary>
-    [SingletonAppServiceContract(AllowMultiple = true)]
+    [AppServiceContract(AllowMultiple = true)]
     public interface IScriptingBehavior
     {
         /// <summary>
-        /// Interception called before invoking the language service to execute the script.
+        /// Interception invoked before the language service executes the script.
         /// </summary>
         /// <param name="executionContext">Information describing the execution.</param>
         /// <param name="token">The cancellation token.</param>
         /// <returns>
-        /// A task.
+        /// The asynchronous result.
         /// </returns>
-        Task BeforeExecuteAsync(IScriptingContext executionContext, CancellationToken token);
+        Task BeforeExecuteAsync(IScriptingContext executionContext, CancellationToken token) => Task.CompletedTask;
 
         /// <summary>
-        /// Interception called after invoking the language service to execute the script.
+        /// Interception invoked after the language service executed the script.
         /// </summary>
         /// <remarks>
-        /// The execution data contains the execution result. 
         /// The interceptor may change the result or even replace it with another one.
         /// </remarks>
         /// <param name="executionContext">Information describing the execution.</param>
         /// <param name="token">The cancellation token.</param>
         /// <returns>
-        /// A task.
+        /// The asynchronous result.
         /// </returns>
-        Task AfterExecuteAsync(IScriptingContext executionContext, CancellationToken token);
+        Task AfterExecuteAsync(IScriptingContext executionContext, CancellationToken token) => Task.CompletedTask;
     }
 }
