d4cb7b63761d68ae36c9a79568712f45831f6bde
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-03-23 15:41:04 +0200

    Breaking change: HostBuilderExtensions.ConfigureAmbientServices must be provided an AmbientServices.

--- src/Kephas.Extensions.Hosting/HostBuilderExtensions.cs
+++ src/Kephas.Extensions.Hosting/HostBuilderExtensions.cs
@@ -5,84 +5,52 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace Kephas.Extensions.Hosting
+namespace Kephas.Extensions.Hosting;
+
+using System;
+using System.Collections.Generic;
+
+using Kephas.Application;
+using Kephas.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+/// <summary>
+/// A host builder factory.
+/// </summary>
+public static class HostBuilderExtensions
 {
-    using System;
-    using System.Collections.Generic;
-
-    using Kephas.Application;
-    using Kephas.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-
     /// <summary>
-    /// A host builder factory.
+    /// Integrates the ambient services into the host builder and configures it.
     /// </summary>
-    public static class HostBuilderExtensions
+    /// <remarks>
+    /// For ASP.NET Core applications, do not create the container here, but instead in the Startup.ConfigureAmbientServices method.
+    /// </remarks>
+    /// <param name="hostBuilder">The host builder.</param>
+    /// <param name="ambientServices">The ambient services.</param>
+    /// <param name="appArgs">The application arguments.</param>
+    /// <param name="containerBuilder">The container builder.</param>
+    /// <param name="setupAction">Optional. Callback to setup the ambient services.</param>
+    /// <returns>The provided host builder.</returns>
+    public static IHostBuilder ConfigureAmbientServices(this IHostBuilder hostBuilder, IAmbientServices ambientServices, IAppArgs appArgs, Action<IAmbientServices> containerBuilder, Action<IServiceCollection, IAmbientServices>? setupAction = null)
     {
-        /// <summary>
-        /// Integrates a new instance of the ambient services into the host builder and configures it.
-        /// </summary>
-        /// <remarks>
-        /// For ASP.NET Core applications, do not create the container here, but instead in the Startup.ConfigureAmbientServices method.
-        /// </remarks>
-        /// <param name="hostBuilder">The host builder.</param>
-        /// <param name="containerBuilder">The container builder.</param>
-        /// <param name="setupAction">Optional. Callback to setup the ambient services.</param>
-        /// <returns>The provided host builder.</returns>
-        public static IHostBuilder ConfigureAmbientServices(this IHostBuilder hostBuilder, Action<IAmbientServices> containerBuilder, Action<IServiceCollection, IAmbientServices>? setupAction = null)
-        {
-            return ConfigureAmbientServices(hostBuilder, new AmbientServices(), null, containerBuilder, setupAction);
-        }
+        hostBuilder = hostBuilder ?? throw new ArgumentNullException(nameof(hostBuilder));
+        ambientServices = ambientServices ?? throw new ArgumentNullException(nameof(ambientServices));
 
-        /// <summary>
-        /// Integrates a new instance of the ambient services into the host builder and configures it.
-        /// </summary>
-        /// <remarks>
-        /// For ASP.NET Core applications, do not create the container here, but instead in the Startup.ConfigureAmbientServices method.
-        /// </remarks>
-        /// <param name="hostBuilder">The host builder.</param>
-        /// <param name="args">The application arguments.</param>
-        /// <param name="containerBuilder">The container builder.</param>
-        /// <param name="setupAction">Optional. Callback to setup the ambient services.</param>
-        /// <returns>The provided host builder.</returns>
-        public static IHostBuilder ConfigureAmbientServices(this IHostBuilder hostBuilder, IEnumerable<string>? args, Action<IAmbientServices> containerBuilder, Action<IServiceCollection, IAmbientServices>? setupAction = null)
-        {
-            return ConfigureAmbientServices(hostBuilder, new AmbientServices(), args, containerBuilder, setupAction);
-        }
-
-        /// <summary>
-        /// Integrates the ambient services into the host builder and configures it.
-        /// </summary>
-        /// <remarks>
-        /// For ASP.NET Core applications, do not create the container here, but instead in the Startup.ConfigureAmbientServices method.
-        /// </remarks>
-        /// <param name="hostBuilder">The host builder.</param>
-        /// <param name="ambientServices">The ambient services.</param>
-        /// <param name="args">The application arguments.</param>
-        /// <param name="containerBuilder">The container builder.</param>
-        /// <param name="setupAction">Optional. Callback to setup the ambient services.</param>
-        /// <returns>The provided host builder.</returns>
-        public static IHostBuilder ConfigureAmbientServices(this IHostBuilder hostBuilder, IAmbientServices ambientServices, IEnumerable<string>? args, Action<IAmbientServices> containerBuilder, Action<IServiceCollection, IAmbientServices>? setupAction = null)
-        {
-            hostBuilder = hostBuilder ?? throw new System.ArgumentNullException(nameof(hostBuilder));
-            ambientServices = ambientServices ?? throw new ArgumentNullException(nameof(ambientServices));
-
-            hostBuilder
-                .UseServiceProviderFactory(new InjectionServiceProviderFactory(ambientServices, containerBuilder))
-                .ConfigureServices(services =>
+        hostBuilder
+            .UseServiceProviderFactory(new InjectionServiceProviderFactory(ambientServices, containerBuilder))
+            .ConfigureServices(services =>
+            {
+                services.AddAmbientServices(ambientServices);
+                setupAction?.Invoke(services, ambientServices);
+            })
+            .ConfigureAppConfiguration(
+                (ctx, cfg) =>
                 {
-                    services.AddAmbientServices(ambientServices);
-                    setupAction?.Invoke(services, ambientServices);
-                })
-                .ConfigureAppConfiguration(
-                    (ctx, cfg) =>
-                    {
-                        ambientServices.RegisterAppArgs(args);
-                    });
+                    ambientServices.RegisterAppArgs(appArgs);
+                });
 
-            hostBuilder.Properties[nameof(IAmbientServices)] = ambientServices;
-            return hostBuilder;
-        }
+        hostBuilder.Properties[nameof(IAmbientServices)] = ambientServices;
+        return hostBuilder;
     }
 }
