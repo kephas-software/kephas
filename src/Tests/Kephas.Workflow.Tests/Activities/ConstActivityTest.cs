e00f866e50af530a5c334f311a2459043875a115
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-04-04 18:22:49 +0300

    Added Const and Set activity tests

---
+++ src/Tests/Kephas.Workflow.Tests/Activities/ConstActivityTest.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConstActivityTest.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Workflow.Tests.Activities;
+
+using Kephas.Workflow.Activities;
+using NSubstitute;
+using NUnit.Framework;
+
+[TestFixture]
+public class ConstActivityTest : FlowActivityTestBase
+{
+    [Test]
+    public async Task ExecuteAsync()
+    {
+        var context = this.CreateActivityContext();
+        var activity = new ConstActivity { Value = 12 };
+        var result = await activity.ExecuteAsync(context);
+
+        Assert.AreEqual(12, result);
+    }
+
+    [Test]
+    public void Clone()
+    {
+        var a1 = Substitute.For<IActivity>();
+        var a2 = Substitute.For<IActivity, ICloneable>();
+        var a2clone = Substitute.For<IActivity>();
+        ((ICloneable)a2).Clone().Returns(a2clone);
+
+        var activity = new ConstActivity { Value = 12};
+        var clone = activity.Clone() as ConstActivity;
+
+        Assert.AreNotSame(activity, clone);
+        Assert.AreEqual(12, clone.Value);
+    }
+}
---
+++ src/Tests/Kephas.Workflow.Tests/Activities/SetActivityTest.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SetActivityTest.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Workflow.Tests.Activities;
+
+using Kephas.Workflow.Activities;
+using NSubstitute;
+using NUnit.Framework;
+
+[TestFixture]
+public class SetActivityTest : FlowActivityTestBase
+{
+    [Test]
+    public async Task ExecuteAsync()
+    {
+        var context = this.CreateActivityContext();
+        var activity = new SetActivity
+        {
+            Variable = "myvar",
+            Expression = new ConstActivity { Value = "hi there" },
+        };
+        var result = await activity.ExecuteAsync(context);
+
+        Assert.AreEqual("hi there", result);
+        Assert.AreEqual("hi there", context.Scope["myvar"]);
+    }
+
+    [Test]
+    public void Clone()
+    {
+        var activity = new SetActivity
+        {
+            Variable = "myvar",
+            Expression = new ConstActivity { Value = "hi there" },
+        };
+        var clone = activity.Clone() as SetActivity;
+
+        Assert.AreNotSame(activity, clone);
+        Assert.AreEqual("myvar", clone.Variable);
+        Assert.IsInstanceOf<ConstActivity>(clone.Expression);
+        Assert.AreEqual("hi there", clone.Expression.Value);
+    }
+}
