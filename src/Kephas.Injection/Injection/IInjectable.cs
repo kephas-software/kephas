e8f5396d2543574caa3c0b5d5b526ae3b6dd1b91
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-04-07 01:06:43 +0300

    NEW: Injection: Added IInjectable marker interface to signal injectable instances. Also, added Type.IsInjectable() extension method to indicate whether a Type is injectable or not.

---
+++ src/Kephas.Injection/Injection/IInjectable.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IInjectable.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Injection;
+
+using Kephas.Services;
+
+/// <summary>
+/// Marker interface for classes instantiable through injection, for example using the <see cref="IInjectableFactory"/> service.
+/// </summary>
+public interface IInjectable
+{
+}
