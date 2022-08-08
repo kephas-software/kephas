a341b48a23a2e398d21bc14452caf0c6666219eb
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-08-08 10:29:09 +0300

    Update IMongoConventionsProvider.cs

--- src/Kephas.MongoDB/Conventions/IMongoConventionsProvider.cs
+++ src/Kephas.MongoDB/Conventions/IMongoConventionsProvider.cs
@@ -1,9 +1,19 @@
-﻿namespace Kephas.MongoDB.Conventions;
+﻿using MongoDB.Bson.Serialization.Conventions;
+
+namespace Kephas.MongoDB.Conventions;
 
 using Kephas.Services;
+
+/// <summary>
+/// Provides conventions for MongoDB client.
+/// </summary>
+/// <param name="Name">The registration name.</param>
+/// <param name="ConventionPack">The conventions pack.</param>
+/// <param name="Filter">Filter function for entity types.</param>
+public record MongoConventions(string Name, ConventionPack ConventionPack, Func<Type, bool>? Filter);
 
 [AppServiceContract(AllowMultiple = true)]
 public interface IMongoConventionsProvider
 {
-    // TODO
+    MongoConventions GetConventions();
 }
