e2f41eabe40fbc699ad87867edfa04348c297c80
Ioan Crisan <ioan.crisan@kephas-software.com>
2022-08-08 15:14:27 +0300

    Added IServiceSelector service

---
+++ src/Kephas.Injection/Injection/IServiceSelector.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IServiceSelector.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Injection;
+
+using Kephas.Services;
+
+[AppServiceContract(AsOpenGeneric = true)]
+public interface IServiceSelector<T>
+{
+    
+}
---
+++ src/Kephas.Injection/Injection/ServiceSelector.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ServiceSelector.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Injection;
+
+using Kephas.Services;
+
+[OverridePriority(Priority.Low)]
+public class ServiceSelector<T> : IServiceSelector<T>
+{
+    
+}
