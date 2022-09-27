1c3c8731961ff3df15deec4662ccf63149f3cf6c
Ioan Crișan <ioan.crisan@kephas-software.com>
2022-09-27 13:40:09 +0300

    Followup

---
+++ src/Kephas.Injection.Autofac/Injection/Autofac/AutofacServiceProviderBase.cs
@@ -0,0 +1,130 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AutofacServiceProviderBase.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the autofac injector base class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Injection.Autofac
+{
+    using System;
+    using System.Collections.Generic;
+
+    using global::Autofac;
+    using Kephas.Injection;
+    using Kephas.Injection.Autofac.Resources;
+    using Kephas.Logging;
+
+    /// <summary>
+    /// An Autofac injector base.
+    /// </summary>
+    public abstract class AutofacServiceProviderBase : ILoggable, IServiceProvider, IAdapter<ILifetimeScope>
+    {
+        private readonly IServiceProviderContainer? root;
+
+        private ILifetimeScope? innerContainer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutofacServiceProviderBase"/> class.
+        /// </summary>
+        /// <param name="root">The root.</param>
+        /// <param name="logger">The logger.</param>
+        internal AutofacServiceProviderBase(IServiceProviderContainer? root, ILogger? logger)
+        {
+            this.root = root;
+            this.Logger = logger;
+        }
+
+        /// <summary>
+        /// Gets the logger.
+        /// </summary>
+        /// <value>
+        /// The logger.
+        /// </value>
+        public ILogger? Logger { get; }
+
+        /// <summary>
+        /// Gets the object the current instance adapts.
+        /// </summary>
+        /// <value>
+        /// The object the current instance adapts.
+        /// </value>
+        ILifetimeScope IAdapter<ILifetimeScope>.Of => this.innerContainer!;
+
+        /// <summary>
+        /// Creates a new scoped injector.
+        /// </summary>
+        /// <returns>
+        /// The new scoped context.
+        /// </returns>
+        public IInjectionScope CreateScope()
+        {
+            this.AssertNotDisposed();
+
+            var scope = this.innerContainer!.BeginLifetimeScope();
+            return new InjectionScope((this.root ?? (IServiceProviderContainer)this).GetServiceProvider(scope));
+        }
+
+        /// <summary>Gets the service object of the specified type.</summary>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <returns>A service object of type <paramref name="serviceType" />.
+        /// -or-
+        /// <see langword="null" /> if there is no service object of type <paramref name="serviceType" />.</returns>
+        /// <footer><a href="https://docs.microsoft.com/en-us/dotnet/api/System.IServiceProvider.GetService?view=netstandard-2.1">`IServiceProvider.GetService` on docs.microsoft.com</a></footer>
+        object? System.IServiceProvider.GetService(Type serviceType)
+        {
+            return this.TryResolve(serviceType);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.innerContainer == null)
+            {
+                return;
+            }
+
+            (this.root ?? (IServiceProviderContainer)this).HandleDispose(this.innerContainer);
+            this.innerContainer.Dispose();
+            this.innerContainer = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutofacServiceProviderBase"/> class.
+        /// </summary>
+        /// <param name="container">The inner container.</param>
+        protected void Initialize(ILifetimeScope container)
+        {
+            container = container ?? throw new ArgumentNullException(nameof(container));
+
+            this.innerContainer = container;
+        }
+
+        /// <summary>
+        /// Asserts that the container is not disposed.
+        /// </summary>
+        protected void AssertNotDisposed()
+        {
+            if (this.innerContainer == null)
+            {
+                this.Logger.Error(Strings.AutofacInjector_Disposed_Exception);
+                throw new ObjectDisposedException(Strings.AutofacInjector_Disposed_Exception);
+            }
+        }
+    }
+}
