fa24a84c6ed60cc933172cd81c953bb814988865
Ioan Crișan <ioan.crisan@kephas-software.com>
2022-12-20 15:08:03 +0200

    Added ConcurrentCollection to Kephas.Core

---
+++ src/Kephas.Core/Collections/Concurrent/ConcurrentCollection.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConcurrentCollection.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Collections.Concurrent;
+
+using System.Collections.Concurrent;
+
+/// <summary>
+/// Internal implementation of a concurrent collection.
+/// </summary>
+/// <typeparam name="T">The item type.</typeparam>
+public class ConcurrentCollection<T> : ConcurrentQueue<T>, ICollection<T>
+{
+    /// <summary>Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1"></see> is read-only.</summary>
+    /// <returns>true if the <see cref="T:System.Collections.Generic.ICollection`1"></see> is read-only; otherwise, false.</returns>
+    public bool IsReadOnly => false;
+
+    /// <summary>Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1"></see>.</summary>
+    /// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1"></see>.</param>
+    /// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1"></see> is read-only.</exception>
+    public void Add(T item)
+    {
+        this.Enqueue(item);
+    }
+
+    /// <summary>Determines whether the <see cref="T:System.Collections.Generic.ICollection`1"></see> contains a specific value.</summary>
+    /// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1"></see>.</param>
+    /// <returns>true if <paramref name="item">item</paramref> is found in the <see cref="T:System.Collections.Generic.ICollection`1"></see>; otherwise, false.</returns>
+    public bool Contains(T item)
+    {
+        return ((IEnumerable<T>)this).Contains(item);
+    }
+
+    /// <summary>Removes the first occurrence of a specific object from the <see cref="T:System.Collections.Generic.ICollection`1"></see>.</summary>
+    /// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1"></see>.</param>
+    /// <returns>true if <paramref name="item">item</paramref> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1"></see>; otherwise, false. This method also returns false if <paramref name="item">item</paramref> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1"></see>.</returns>
+    /// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1"></see> is read-only.</exception>
+    bool ICollection<T>.Remove(T item)
+    {
+        throw new NotSupportedException();
+    }
+}
