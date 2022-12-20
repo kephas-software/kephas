43103b8a903f26008923b495f53c9b8d7ff6e2fc
Ioan Crișan <ioan.crisan@kephas-software.com>
2022-12-20 15:21:31 +0200

    Cosmetics

--- src/Analyzers/Kephas.Analyzers/AnalyzerHelper.cs
+++ src/Analyzers/Kephas.Analyzers/AnalyzerHelper.cs
@@ -1,4 +1,11 @@
-﻿namespace Kephas.Analyzers;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AnalyzerHelper.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Analyzers;
 
 using System.Text;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
--- src/Analyzers/Kephas.Analyzers/Application/ModuleInitializerSourceGenerator.cs
+++ src/Analyzers/Kephas.Analyzers/Application/ModuleInitializerSourceGenerator.cs
@@ -1,4 +1,11 @@
-﻿namespace Kephas.Analyzers.Application;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ModuleInitializerSourceGenerator.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.Analyzers.Application;
 
 using System.Reflection;
 using System.Text;
