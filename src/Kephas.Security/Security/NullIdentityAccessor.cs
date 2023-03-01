fcaeb81935e82cbc3ed93fc7a12a36262f6b855e
Ioan Crișan <ioan.crisan@kephas-software.com>
2023-03-01 14:09:42 +0200

    Added the IIdentityAccessor service.

---
+++ src/Kephas.Security/Security/NullIdentityAccessor.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NullIdentityAccessor.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Security.Principal;
+using Kephas.Services;
+
+namespace Kephas.Security;
+
+/// <summary>
+/// <see cref="IIdentityAccessor"/> getting a <c>null</c> identity.
+/// </summary>
+[OverridePriority(Priority.Lowest)]
+public class NullIdentityAccessor : IIdentityAccessor
+{
+    /// <summary>
+    /// Gets a <c>null</c> identity.
+    /// </summary>
+    public IIdentity? Identity => null;
+}
